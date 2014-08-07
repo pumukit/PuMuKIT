b1eda9eb12abf2d52e5ceaed3152793782be8917
Ruben Gonzalez <rubenrua@gmail.com>
2014-08-07 18:10:10 +0200

    Update PersonType from with fos_user

--- src/Pumukit/SchemaBundle/Form/PersonType.php
+++ src/Pumukit/SchemaBundle/Form/PersonType.php
@@ -8,25 +8,32 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PersonType extends AbstractType
 {
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('login')
+            ->add('username')
             ->add('password', 'repeated', array(
                'first_name'  => 'password',
                'second_name' => 'confirm',
                'type'        => 'password'))
             ->add('email')
-             ->add('honorific')
             ->add('name')
             ->add('web')
             ->add('phone')
+            ->add('honorific')
             ->add('firm')
             ->add('post')
             ->add('bio', 'textarea')
         ;
     }
-
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
@@ -34,6 +41,9 @@ class PersonType extends AbstractType
         ));
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'pumukit_schemabundle_persontype';
