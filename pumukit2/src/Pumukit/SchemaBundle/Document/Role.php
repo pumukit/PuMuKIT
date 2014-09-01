a01ce586d8a158f029147001f28b8d5373b029ae
Mercedes Rey <mrey@teltek.es>
2014-09-01 16:10:49 +0200

    Add MongoDB document Role

---
+++ pumukit2/src/Pumukit/SchemaBundle/Document/Role.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Document;
+
+use Doctrime\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class Role
+{
+
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @var string $cod
+     *
+     * @MongoDB\String
+     */
+    private $cod = 0;
+
+    /**
+     * @var integer $rank
+     *
+     * @MongoDB\Integer
+     */
+    private $rank;
+
+    /**
+     * See European Broadcasting Union Role Codes
+     * @var string $xml
+     *
+     * @MongoDB\Column
+     */
+    private $xml;
+
+    /**
+     * @var boolean $display
+     *
+     * @MongoDB\Column
+     */
+    private $display = true;
+
+    /**
+     * @var string $name
+     *
+     * @MongoDB\Column
+     */
+    private $name;
+
+    /**
+     * @var string $text
+     *
+     * @Gedmo\Translatable
+     * @MongoDB\Column
+     */
+    private $text;
+
+    /**
+     * @var ArrayCollection $person_in_multimedia_object
+     *
+     * @MongoDB\OneToMany
+     */
+    private $people_in_multimedia_object;
+
+    /**
+     * @Gedmo\Locale
+     * Used locale to override Translation listener`s locale
+     * this is not a mapped field of entity metadata, just a simple property
+     */
+    private $locale;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set cod
+     *
+     * @param string $cod
+     */
+    public function setCod($cod)
+    {
+        $this->cod = $cod;
+    }
+
+     /**
+      * Get cod
+      *
+      * @return string
+      */
+     public function getCod()
+     {
+         return $this->cod;
+     }
+
+     /**
+      * Set rank
+      *
+      * @param integer $rank
+      */
+     public function setRank($rank)
+     {
+         $this->rank = $rank;
+     }
+
+     /**
+      * Get rank
+      *
+      * @return integer
+      */
+     public function getRank()
+     {
+         return $this->rank;
+     }
+
+     /**
+      * Set xml
+      *
+      * @param string $xml
+      */
+     public function setXml($xml)
+     {
+         $this->xml = $xml;
+     }
+
+     /**
+      * Get xml
+      *
+      * @return string
+      */
+     public function getXml()
+     {
+         return $this->xml;
+     }
+
+     /**
+      * Set display
+      *
+      * @param boolean $display
+      */
+     public function setDisplay($display)
+     {
+         $this->display = $display;
+     }
+
+     /**
+      * Get display
+      *
+      * @return boolean
+      */
+     public function getDisplay()
+     {
+         return $this->display;
+     }
+
+     /**
+      * Set name
+      *
+      * @param string $name
+      */
+     public function setName($name)
+     {
+         $this->name = $name;
+     }
+
+     /**
+      * Get name
+      *
+      * @return string
+      */
+     public function getName()
+     {
+         return $this->name;
+     }
+
+     /**
+      * Set text
+      *
+      * @param string $text
+      */
+     public function setText($text)
+     {
+         $this->text = $text;
+     }
+
+     /**
+      * Get text
+      *
+      * @return string
+      */
+     public function getText()
+     {
+         return $this->text;
+     }
+}
---
+++ pumukit2/src/Pumukit/SchemaBundle/Tests/Document/RoleTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Tests\Document;
+
+use Pumukit\SchemaBundle\Document\Role;
+
+class RoleTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testDefaults()
+  {
+    $role = new Role();
+
+    $this->assertEquals(0,$role->getCod());
+    $this->assertTrue($role->getDisplay());
+  }
+
+  public function testGetterAndSetter()
+  {
+    $role = new Role();
+
+    $cod = 12345; //String - max length = 5
+    $rank = 123;
+    $xml = 'string <xml>';
+    $display = true;
+    $name = 'Alonso de Lanzós';
+    $text = 'The Irmandiño Wars were two revolts that took place in 15th-century Kingdom of Galicia against attempts by the regional nobility to maintain their rights over the peasantry and the bourgeoisie. The revolts were also part of the larger phenomenon of popular revolts in late medieval Europe caused by the general economic and demographic crises in Europe during the fourteenth and fifteenth centuries.[1] Similar rebellions broke out in the Iberian Kingdoms, including the War of the Remences in Catalonia and the foráneo revolts in the Balearic Islands.[2]';
+
+    $role->setCod($cod);
+    $role->setRank($rank);
+    $role->setXml($xml);
+    $role->setDisplay($display);
+    $role->setName($name);
+    $role->setText($text);
+
+    $this->assertEquals($cod, $role->getCod($cod));
+    $this->assertEquals($rank, $role->getRank($rank));
+    $this->assertEquals($xml, $role->getXml($xml));
+    $this->assertEquals($display, $role->getDisplay($display));
+    $this->assertEquals($name, $role->getName($name));
+    $this->assertEquals($text, $role->getText($text));
+
+  }
+}
