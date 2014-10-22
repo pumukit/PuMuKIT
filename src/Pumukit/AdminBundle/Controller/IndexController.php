353bed98260db9423a479f51b06f70fe3b443e4e
rubenrua <rubenrua@teltek.es>
2014-10-22 15:25:27 +0200

    Create a IndexController

--- src/Pumukit/AdminBundle/Controller/IndexController.php
+++ src/Pumukit/AdminBundle/Controller/IndexController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+      return array();
     }
 }
