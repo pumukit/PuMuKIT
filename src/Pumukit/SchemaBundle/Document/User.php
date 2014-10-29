995517463875ccfbff2d18ad878196c7b5f497ea
Mercedes Rey <mrey@teltek.es>
2014-10-29 19:45:22 +0100

    Añadidas variables a User

--- src/Pumukit/SchemaBundle/Document/User.php
+++ src/Pumukit/SchemaBundle/Document/User.php
@@ -14,6 +14,10 @@ use FOS\UserBundle\Document\User as BaseUser;
  */
 class User extends BaseUser
 {
+  const USER_TYPE_ADMIN = 'Administrator';
+  const USER_TYPE_PUB = 'Publisher';
+  const USER_TYPE_FTP = 'FTP';
+  
   /**
    * @var int $id
    *
@@ -21,8 +25,144 @@ class User extends BaseUser
    */
   protected $id;
 
+  /**
+   * @var string $name
+   *
+   * @MongoDB\String
+   */
+  protected $name;
+
+  /**
+   * @var string $type
+   *
+   * @MongoDB\Raw
+   */
+  protected $user_type = array('en'=>self::USER_TYPE_ADMIN);
+
+  /**
+   * @var boolean $root
+   *
+   * @MongoDB\Boolean
+   */
+  protected $root;  
+
+  /**
+   * @var locale $locale
+   */
+  private $locale = 'en';
+  
   public function __construct()
   {
     parent::__construct();
   }
+
+  /**
+   * Set name
+   *
+   * @param string $name
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  
+  /**
+   * Get name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Set user_type
+   *
+   * @param string $user_type
+   */
+  public function setUserType($user_type, $locale = null)
+  {
+    if ($locale == null) {
+      $locale = $this->locale;
+    }
+    $this->user_type[$locale] = $user_type;
+  }
+  
+  /**
+   * Get user_type
+   *
+   * @return string
+   */
+  public function getUserType($locale = null)
+  {
+    if ($locale == null) {
+      $locale = $this->locale;
+    }
+    if (!isset($this->user_type[$locale])){
+      return null;
+    }
+    return $this->user_type[$locale];
+  }
+
+  /**
+   * Set I18n user_type
+   *
+   * @param array $user_type
+   */
+  public function setI18nName(array $user_type)
+  {
+    $this->user_type = $user_type;
+  }
+  
+  /**
+   * Get i18n user_type
+   *
+   * @return array
+   */
+  public function getI18nUserType()
+  {
+    return $this->user_type;
+  }
+
+  /**
+   * Set locale
+   *
+   * @param string $locale
+   */
+  public function setLocale($locale)
+  {
+    $this->locale = $locale;
+  }
+
+  /**
+   * Set root
+   *
+   * @param boolean $root
+   */
+  public function setRoot($root)
+  {
+    $this->root = $root;
+  }
+  
+  /**
+   * Get root
+   *
+   * @return boolean
+   */
+  public function getRoot()
+  {
+    return $this->root;
+  }
+  
+  /**
+   * Get locale
+   *
+   * @return string
+   */
+  public function getLocale()
+  {
+    return $this->locale;
+  }
+
 }
