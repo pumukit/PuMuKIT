8e508edf0194fffb1b07c3ece4a53253973ae6e5
rubenrua <rubenrua@teltek.es>
2014-10-30 13:09:21 +0100

    Add listener to init mongo document locale

---
+++ src/Pumukit/SchemaBundle/Listener/LocaleListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Listener;
+
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Init the locale of the i18n Documents when loaded.
+ * Use default locale in console commands and current request locale in web request.
+ */
+class LocaleListener {
+
+  private $requestStack;
+  private $defaultLocale;
+
+  public function __construct(RequestStack $requestStack, $defaultLocale = 'en')
+  {
+    $this->requestStack = $requestStack;
+    $this->defaultLocale = $defaultLocale;
+  }
+
+
+  public function postLoad(LifecycleEventArgs $args)
+    {
+      $document = $args->getDocument();
+      
+      if (method_exists($document, 'setLocale')) {
+	if ($request = $this->requestStack->getCurrentRequest()) {
+	  $document->setLocale($request->getLocale());
+	} else {
+	  $document->setLocale($this->defaultLocale);
+	}
+      }
+    }
+}
