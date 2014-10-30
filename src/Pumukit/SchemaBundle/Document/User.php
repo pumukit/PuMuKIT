219c2224721ade60694e32d479989a920fe0bb66
Mercedes Rey <mrey@teltek.es>
2014-10-30 18:50:11 +0100

    Añadido los Roles al formulario. Eliminado Root

--- src/Pumukit/SchemaBundle/Document/User.php
+++ src/Pumukit/SchemaBundle/Document/User.php
@@ -14,10 +14,7 @@ use FOS\UserBundle\Document\User as BaseUser;
  */
 class User extends BaseUser
 {
-  const USER_TYPE_ADMIN = 'Administrator';
-  const USER_TYPE_PUB = 'Publisher';
-  const USER_TYPE_FTP = 'FTP';
-  
+
   /**
    * @var int $id
    *
@@ -31,25 +28,6 @@ class User extends BaseUser
    * @MongoDB\String
    */
   protected $fullname;
-
-  /**
-   * @var string $type
-   *
-   * @MongoDB\Raw
-   */
-  protected $user_type = array('en'=>self::USER_TYPE_ADMIN);
-
-  /**
-   * @var boolean $root
-   *
-   * @MongoDB\Boolean
-   */
-  protected $root;  
-
-  /**
-   * @var locale $locale
-   */
-  private $locale = 'en';
   
   public function __construct()
   {
@@ -74,95 +52,6 @@ class User extends BaseUser
   public function getFullname()
   {
     return $this->fullname;
-  }
-
-  /**
-   * Set user_type
-   *
-   * @param string $user_type
-   */
-  public function setUserType($user_type, $locale = null)
-  {
-    if ($locale == null) {
-      $locale = $this->locale;
-    }
-    $this->user_type[$locale] = $user_type;
-  }
-  
-  /**
-   * Get user_type
-   *
-   * @return string
-   */
-  public function getUserType($locale = null)
-  {
-    if ($locale == null) {
-      $locale = $this->locale;
-    }
-    if (!isset($this->user_type[$locale])){
-      return null;
-    }
-    return $this->user_type[$locale];
-  }
-
-  /**
-   * Set I18n user_type
-   *
-   * @param array $user_type
-   */
-  public function setI18nUserType(array $user_type)
-  {
-    $this->user_type = $user_type;
-  }
-  
-  /**
-   * Get i18n user_type
-   *
-   * @return array
-   */
-  public function getI18nUserType()
-  {
-    return $this->user_type;
-  }
-
-  /**
-   * Set root
-   *
-   * @param boolean $root
-   */
-  public function setRoot($root)
-  {
-    $this->root = $root;
-  }
-  
-  /**
-   * Get root
-   *
-   * @return boolean
-   */
-  public function getRoot()
-  {
-    return $this->root;
-  }
-
-  /**
-   * Set locale
-   *
-   * @param string $locale
-   */
-  public function setLocale($locale)
-  {
-    $this->locale = $locale;
-  }
-  
-  /**
-   * Get locale
-   *
-   * @return string
-   */
-  public function getLocale()
-  {
-    return $this->locale;
   }
 
 }
