c436f1aa5d81fcf95114a412335bc6b4e2c1da03
rubenrua <rubenrua@teltek.es>
2014-11-03 16:55:02 +0100

    Fix Tag test

--- src/Pumukit/SchemaBundle/Tests/Document/TagTest.php
+++ src/Pumukit/SchemaBundle/Tests/Document/TagTest.php
@@ -16,10 +16,6 @@ class TagTest extends \PHPUnit_Framework_TestCase
 		$slug = 'slug';
 		$cod = 23;
 		$metatag = true;
-		$left = 5;
-		$right = 15;
-		$root = 3;
-		$level = 2;
 		$created = new \DateTime("now");
 		$updated = new \DateTime("now");
 
@@ -30,19 +26,8 @@ class TagTest extends \PHPUnit_Framework_TestCase
 		$tag->setSlug($slug);
 		$tag->setCod($cod);
 		$tag->setMetatag($metatag);
-		$tag->setLeft($left);
-		$tag->setRight($right);
-		$tag->setRoot($root);
-		$tag->setLevel($level);
 		$tag->setCreated($created);
 		$tag->setUpdated($updated);
-
-		$tag_child1 = new Tag("child1");
-		$tag->addChildren($tag_child1);
-		$tag_child2 = new Tag("child2");
-		$tag->addChildren($tag_child2);
-		$children_array = [$tag_child1, $tag_child2];
-		$children = new ArrayCollection($children_array);
 
 		$tag_parent = new Tag("parent");
 		$tag->setParent($tag_parent);
@@ -52,13 +37,8 @@ class TagTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($slug, $tag->getSlug());
 		$this->assertEquals($cod, $tag->getCod());
 		$this->assertEquals($metatag, $tag->getMetatag());
-		$this->assertEquals($left, $tag->getLeft());
-		$this->assertEquals($right, $tag->getRight());
-		$this->assertEquals($root, $tag->getRoot());
-		$this->assertEquals($level, $tag->getLevel());
 		$this->assertEquals($created, $tag->getCreated());
 		$this->assertEquals($updated, $tag->getUpdated());
-		$this->assertEquals($children, $tag->getChildren());
 		$this->assertEquals($tag_parent, $tag->getParent());
 	}
 }
