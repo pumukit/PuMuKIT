8b9e66840865d32aaf15366ac3e4e0a1f6b36478
Ruben Gonzalez <rubenrua@gmail.com>
2014-11-10 21:14:12 +0100

    Improve command loading time

--- src/Pumukit/SchemaBundle/Command/PumukitTestCommand.php
+++ src/Pumukit/SchemaBundle/Command/PumukitTestCommand.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
- * This file is part of the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace Pumukit\SchemaBundle\Command;
 
-use Assetic\Util\VarUtils;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -22,11 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 use Pumukit\SchemaBundle\Document\Tag;
 
-/**
- * Dumps assets as their source files are modified.
- *
- * @author Kris Wallsmith <kris@symfony.com>
- */
+
 class PumukitTestCommand extends ContainerAwareCommand
 {
 
@@ -39,57 +25,87 @@ class PumukitTestCommand extends ContainerAwareCommand
             ->setName('pumukit:test')
             ->setDescription('Pumukit test command')
             ->addArgument('file', InputArgument::REQUIRED, 'Input CSV path')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Set this parameter to execute this action')
+            ->setHelp(<<<EOT
+TODO
 
-        ;
+The --force parameter has to be used to actually drop the database.
+
+EOT
+	      );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $stdout)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // capture error output
-        $stderr = $stdout instanceof ConsoleOutputInterface
-            ? $stdout->getErrorOutput()
-            : $stdout;
-
-
 	$this->dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
 	$this->repo = $this->getContainer()->get('doctrine_mongodb')->getRepository("PumukitSchemaBundle:Tag");
 
+
+	if ($input->getOption('force')) {
+	  $file = $input->getArgument('file');
+	  $this->removeTags();
+	  $root = $this->createRoot();
+	  $this->createFromFile($file, $root, $output);
+	} else {
+	  $output->writeln('<error>ATTENTION:</error> This operation should not be executed in a production environment.');
+	  $output->writeln('');
+	  $output->writeln('<info>Would drop the database</info>');
+	  $output->writeln('Please run the operation with --force to execute');
+	  $output->writeln('<error>All data will be lost!</error>');
+
+	  return -1;
+	}
 	
-	$file = $input->getArgument('file');
+    }
 
 
+    protected function removeTags()
+    {
+      $this->dm->getDocumentCollection('PumukitSchemaBundle:Tag')->remove(array());
+    }
+
+    protected function createRoot()
+    {
+      $root = $this->createTagFromCsvArray(array(null, "ROOT", 1, 0 , "ROOT", "ROOT", "ROOT"));
+      $this->dm->flush();
+      return $root;
+    }
+
+    protected function createFromFile($file, $root, OutputInterface $output)
+    {
 	if (!file_exists($file)) {
 	  $stderr->writeln("<error>Error stating " . $file . "</error>");
 	  return -1;
 	}
 
-
 	$idCodMapping = array();
 
 	$row = 1;
-	if (($file = fopen($file, "r")) !== FALSE) {
-	  while ( ($currentRow = fgetcsv($file, 300, ";")) !== FALSE)  {
+	if (($file = fopen($file, "r")) !== false) {
+	  while ( ($currentRow = fgetcsv($file, 300, ";")) !== false)  {
             $number = count($currentRow);
             if ($number == 6 || $number == 8){
-	      if (trim($currentRow[0]) == "id") { // header row 
+	      //Check header rows
+	      if (trim($currentRow[0]) == "id") { 
 		continue;
 	      }
+	      
+	      $parent = isset($idCodMapping[$currentRow[2]])
+		? $idCodMapping[$currentRow[2]]
+		: $root;
 
-	      /*
-	      if (!isset($imported_id_category_id[$currentRow[2]])){
-		echo "\n\nCurrent csv row: " . $row . "\n";
-		print_r($currentRow);
-		throw new Exception ("Parent category not defined");
+	      //Necesary to create the tree
+	      if (!$parent->getId()) {
+		$this->dm->flush();
 	      }
 
-	      $parent_cat = CategoryPeer::retrieveByPk($imported_id_category_id[$currentRow[2]]);
-	      $imported_id_category_id[$currentRow[0]] = $category->getId();
-	      */
-
-	      
-	      $parentCat = isset($idCodMapping[$currentRow[2]])?$this->repo->findOneBy(array("cod" => $idCodMapping[$currentRow[2]])):null;
-	      $category = $this->createCategoryFromCsvArray($currentRow, $parentCat);
-	      $idCodMapping[$currentRow[0]] = $currentRow[1];
+	      try {
+		$tag = $this->createTagFromCsvArray($currentRow, $parent);
+		$idCodMapping[$currentRow[0]] = $tag;
+		$output->writeln("Tag persisted - new id: " . $tag->getId() . " cod: " . $tag->getCod());
+	      } catch (Exception $e) {
+		$output->writeln("<error>" . $e->getMessage() . "</error>");
+	      }
 
             } else {
 	      $stderr->writeln("Last valid row = ...");
@@ -99,9 +115,10 @@ class PumukitTestCommand extends ContainerAwareCommand
             if ($row % 100 == 0 ) echo "Row " . $row . "\n";
             $previous_content = $currentRow;
             $row++;
-
 	  } 
 	  fclose($file);
+	  $this->dm->flush();
+
 	} else {
 	  $stderr->writeln("<error>Error opening " . $file . "</error>");
 	  return -1;
@@ -112,36 +129,30 @@ class PumukitTestCommand extends ContainerAwareCommand
     /**
      *
      */
-    private function createCategoryFromCsvArray($csv_array, $cat_parent=null)
+    private function createTagFromCsvArray($csv_array, $cat_parent=null)
     {
       $c = new Tag();
-      if (!$tag = $this->repo->findOneBy(array("cod" => $csv_array[1]))) {
-	$tag = new Tag();
-	$tag->setCod($csv_array[1]);
-	$tag->setMetatag($csv_array[3]);
-	$tag->setDisplay($csv_array[4]);
-	if ($cat_parent) {
-	  $tag->setParent($cat_parent);
-	}
-	$tag->setTitle($csv_array[5], 'es');
-	// TODO Take care of csv language order!
-	if (isset($csv_array[6])){
-	  $tag->setTitle($csv_array[6], 'gl');
-	}
-	if (isset($csv_array[7])){
-	  $tag->setTitle($csv_array[7], 'en');
-	}
+      if ($tag = $this->repo->findOneByCod($csv_array[1])) {
+	throw new \LengthException("Nothing done - Tag retrieved from DB id: " . $tag->getId() . " cod: " . $tag->getCod());
+      }
 
-        $this->dm->persist($tag);
-	$this->dm->flush();
-	echo "Tag persisted - new id: " . $tag->getId() . " cod: " . $tag->getCod() . " title: " . $tag->getTitle() . "\n";
-	
-      } else {
-        echo "\tNothing done - Tag retrieved from DB id: " . $tag->getId() . " cod: " . $tag->getCod() . " title: " . $tag->getTitle() . "\n";
-        // ¿Update with csv_array?
-        // ¿Check parent?
+      $tag = new Tag();
+      $tag->setCod($csv_array[1]);
+      $tag->setMetatag($csv_array[3]);
+      $tag->setDisplay($csv_array[4]);
+      if ($cat_parent) {
+	$tag->setParent($cat_parent);
+      }
+      $tag->setTitle($csv_array[5], 'es');
+      // TODO Take care of csv language order!
+      if (isset($csv_array[6])){
+	$tag->setTitle($csv_array[6], 'gl');
+      }
+      if (isset($csv_array[7])){
+	$tag->setTitle($csv_array[7], 'en');
       }
       
+      $this->dm->persist($tag);
       return $tag;
     }
 }
