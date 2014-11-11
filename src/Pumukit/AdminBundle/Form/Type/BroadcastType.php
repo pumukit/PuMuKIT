b6088819c797395dbc196b2ac5911f0a69964098
Mercedes Rey <mrey@teltek.es>
2014-11-11 16:48:32 +0100

    Select Broadcast type constants and representation

--- src/Pumukit/AdminBundle/Form/Type/BroadcastType.php
+++ src/Pumukit/AdminBundle/Form/Type/BroadcastType.php
@@ -5,6 +5,7 @@ namespace Pumukit\AdminBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Pumukit\SchemaBundle\Document\Broadcast;
 
 class BroadcastType extends AbstractType
 {
@@ -14,7 +15,12 @@ class BroadcastType extends AbstractType
     $builder
       ->add('name', 'text',
 	    array('attr' => array('style' => 'width: 420px'), 'label' => 'Nombre'))
-      ->add('a_broadcast_type', null, array('label' => 'Tipo'))
+      ->add('broadcast_type_id', 'choice', 
+	    array('choices' => array(
+                       Broadcast::BROADCAST_TYPE_PUB => Broadcast::BROADCAST_TYPE_PUB,
+                       Broadcast::BROADCAST_TYPE_PRI => Broadcast::BROADCAST_TYPE_PRI,
+                       Broadcast::BROADCAST_TYPE_COR => Broadcast::BROADCAST_TYPE_COR),
+		  'label' => 'Tipo'))
       ->add('passwd', 'password',
 	    array('attr' => array('style' => 'width: 420px'), 'label' => 'Passwd'))
       ->add('i18n_description', 'textareai18n',
@@ -25,7 +31,7 @@ class BroadcastType extends AbstractType
   public function setDefaultOptions(OptionsResolverInterface $resolver)
   {
     $resolver->setDefaults(array(
-        'data_class' => 'Pumukit\AdminBundle\Document\Broadcast'
+        'data_class' => 'Pumukit\SchemaBundle\Document\Broadcast'
     ));
   }
 
