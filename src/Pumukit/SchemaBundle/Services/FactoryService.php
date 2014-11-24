03fdff77bc6d5c88d86080b32a30ea0e3d3ba60b
rubenrua <rubenrua@teltek.es>
2014-11-24 09:50:10 +0100

    Inject DocumentManager in service. The controller is not necesary in this service (KISS).

--- src/Pumukit/SchemaBundle/Services/FactoryService.php
+++ src/Pumukit/SchemaBundle/Services/FactoryService.php
@@ -2,43 +2,49 @@
 
 namespace Pumukit\SchemaBundle\Services;
 
+use Doctrine\ODM\MongoDB\DocumentManager;
+
 use Pumukit\SchemaBundle\Document\Series;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 
 class FactoryService
 {
+  private $dm;
+
+
+  public function __construct(DocumentManager $documentManager)
+  {
+    $this->dm = $documentManager;
+  }
+
   /**
    * Create a new series with default values
    */
-  public function createSeries($controller)
+  public function createSeries()
   {
-      $dm = $controller->get('doctrine_mongodb')->getManager();
-
       $series = new Series();
 
       $series->setPublicDate(new \DateTime("now"));
       $series->setTitle('New');
       $series->setCopyright('UdN-TV');
 
-      $dm->persist($series);
-      $dm->flush();
+      $this->dm->persist($series);
+      $this->dm->flush();
   }
 
   /**
    * Create a new series with default values
    */
-  public function createMultimediaObject($controller)
+  public function createMultimediaObject()
   {
-      $dm = $controller->get('doctrine_mongodb')->getManager();
-
       $mm = new MultimediaObject();
 
       $mm->setPublicDate(new \DateTime("now"));
       $mm->setRecordDate($mm->getPublicDate());
       $mm->setTitle('New');
 
-      $dm->persist($mm);
-      $dm->flush();
+      $this->dm->persist($mm);
+      $this->dm->flush();
   }
 
 }
