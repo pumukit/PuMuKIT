c4d3771f64677ad9f3ccb245f60c29fe9eefa774
Mercedes Rey <mrey@teltek.es>
2014-11-25 19:47:33 +0100

    Show update tabs of Multimedia Object

--- src/Pumukit/AdminBundle/Controller/MultimediaObjectController.php
+++ src/Pumukit/AdminBundle/Controller/MultimediaObjectController.php
@@ -22,9 +22,7 @@ class MultimediaObjectController extends SortableAdminController
 
       $pluralName = $config->getPluralResourceName();
 
-      $dm = $this->get('doctrine_mongodb.odm.document_manager');
-      $repository = $dm->getRepository('PumukitSchemaBundle:Series');
-      $series = $repository->find($request->get('id'));
+      $series = $this->getSeries($request);
 
       $page = $this->get('session')->get('admin/mms/page', 1);
 
@@ -60,9 +58,7 @@ class MultimediaObjectController extends SortableAdminController
       $config = $this->getConfiguration();
       $pluralName = $config->getPluralResourceName();
 
-      $dm = $this->get('doctrine_mongodb.odm.document_manager');
-      $repository = $dm->getRepository('PumukitSchemaBundle:Series');
-      $series = $repository->find($request->attributes->get('id'));
+      $series = $this->getSeries($request);
 
       $factory = $this->get('pumukitschema.factory');
       $factory->createMultimediaObject($series);
@@ -92,9 +88,7 @@ class MultimediaObjectController extends SortableAdminController
 
     $this->get('session')->set('admin/mms/id', $data->getId());
 
-    $dm = $this->get('doctrine_mongodb.odm.document_manager');
-    $repository = $dm->getRepository('PumukitSchemaBundle:Role');
-    $roles = $repository->findAll();
+    $roles = $this->getRoles();
 
     $view = $this
       ->view()
@@ -108,5 +102,88 @@ class MultimediaObjectController extends SortableAdminController
     return $this->handleView($view);
   }
 
+  // TODO
+  /**
+   * Display the form for editing or update the resource.
+   */
+  public function updateAction(Request $request)
+  {
+      $config = $this->getConfiguration();
+      // TODO VALIDATE SERIES and roles
+      $series = $this->getSeries($request);
+      $roles = $this->getRoles();
+
+      $resource = $this->findOr404();
+      $form = $this->getForm($resource);
+      
+      if (($request->isMethod('PUT') || $request->isMethod('POST') || $request->isMethod('DELETE')) && $form->bind($request)->isValid()) {
+          $event = $this->update($resource);
+          if (!$event->isStopped()) {
+              $this->setFlash('success', 'update');
+
+	      $criteria = $this->getCriteria($config);
+	      $resources = $this->getResources($request, $config, $criteria);	      
+
+	      $pluralName = $config->getPluralResourceName();
+	      
+	      $view = $this
+		->view()
+		->setTemplate($config->getTemplate('list.html'))
+		->setTemplateVar($pluralName)
+		->setData($resources)
+		;
+	      
+	      return $this->handleView($view);
+          }
+
+          $this->setFlash($event->getMessageType(), $event->getMessage(), $event->getMessageParams());
+      }
+
+      if ($config->isApiRequest()) {
+          return $this->handleView($this->view($form));
+      }
+
+      $view = $this
+      ->view()
+      ->setTemplate($config->getTemplate('update.html'))
+      ->setData(array(
+              'mm'     => $resource,
+              'form'   => $form->createView(),
+	      'series' => $series,
+	      'roles'  => $roles
+              ))
+      ;
+
+      return $this->handleView($view);
+  }
   
+  /**
+   * Get series
+   */
+  private function getSeries(Request $request)
+  {
+      $dm = $this->get('doctrine_mongodb.odm.document_manager');
+      $repository = $dm->getRepository('PumukitSchemaBundle:Series');
+
+      if ($this->get('session')->get('admin/series/id', null)){
+	$series = $repository->find($this->get('session')->get('admin/series/id'));
+      }else{
+	$series = $repository->find($request->get('id'));
+	$this->get('session')->set('admin/series/id', $request->get('id'));
+      }
+
+      return $series;
+  }
+
+  /**
+   * Get all roles
+   */
+  private function getRoles()
+  {
+    $dm = $this->get('doctrine_mongodb.odm.document_manager');
+    $repository = $dm->getRepository('PumukitSchemaBundle:Role');
+    $roles = $repository->findAll();
+
+    return $roles;
+  }
 }
