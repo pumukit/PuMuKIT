fd027cf3c4b41fa3208fb324a1ffd183579aafcf
rubenrua <rubenrua@teltek.es>
2014-12-05 15:24:59 +0100

    Add new service SeriesPicService
    Mi idea es tener una clase para el servicio de SeriesPicService o otra para el servicio de MultimediaObjectPicService. Posiblemente pueda existir una clase abstracta donde se ponga el codigo comun. Esta idea es mas sencilla que tener una unica clase para los dos servicios.
    
    Pero cuando esté implementado valoraremos que solucion es mejor.

---
+++ src/Pumukit/SchemaBundle/Services/SeriesPicService.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Services;
+
+use Symfony\Component\HttpFoundation\File\File;
+
+use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\SchemaBundle\Document\SeriesPic;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+
+/**
+   TODO:
+   
+   [ ] Resize images?
+   [x] Configure paths
+   [ ] Global configuration paths
+
+ */
+
+class SeriesPicService
+{
+  private $dm;
+  private $targetPath;
+  private $targetUrl;
+
+  public function __construct(DocumentManager $documentManager, $targetPath, $targetUrl)
+  {
+      $this->dm = $documentManager;
+      $this->targetPath = $targetPath;
+      $this->targetUrl = $targetUrl;
+  }
+
+
+  /**
+   * Get pics from series or multimedia object
+   */
+  public function getRecomendedPics($series, $page, $limit)
+  {
+      $offset = ($page - 1) * $limit;
+      $total = 0;
+
+      //TODO paginate..
+      //TODO Execute RAW mongo query.
+
+      $list = new ArrayCollection();
+      foreach ($series->getMultimediaObjects() as $mmobj) {
+	foreach ($mmobj->getPics() as $pic) {
+	  $list[] = $pics;
+	}
+      }
+
+      return array($list, 0);
+  }
+
+
+  /**
+   * Set a pic from an url into the series
+   */
+  public function addPicUrl(Series $series, $picUrl)
+  {
+    //TODO check URL is valid and a image.
+    $pic = new SeriesPic();
+    $pic->setUrl($picUrl);    
+
+    $series->addPic($pic);
+    $this->dm->persist($series);
+    $this->dm->flush();
+
+    return $series;
+  }
+
+
+  /**
+   * Set a pic from an url into the series
+   */
+  public function addPicFile(Series $series, File $picFile)
+  {
+    //TODO check file is a image
+    //TODO delete double slash "//"
+    $path = $picFile->move($this->targetPath . "/" . $series->getId(), $picFile->getClientOriginalName());
+    
+    $pic = new SeriesPic();
+    $pic->setUrl(str_replace($this->targetPath, $this->targetUrl, $path));    
+
+    $series->addPic($pic);
+    $this->dm->persist($series);
+    $this->dm->flush();
+
+    return $series;
+  }
+}
