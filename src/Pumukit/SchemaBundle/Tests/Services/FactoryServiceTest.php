80969ac60e900fc9d2bffea015e202632c856937
Mercedes Rey <mrey@teltek.es>
2014-12-10 19:53:49 +0100

    Added test create multimedia object in factory

--- src/Pumukit/SchemaBundle/Tests/Services/FactoryServiceTest.php
+++ src/Pumukit/SchemaBundle/Tests/Services/FactoryServiceTest.php
@@ -3,7 +3,8 @@
 namespace Pumukit\SchemaBundle\Tests\Services;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Pumukit\SchemaBundle\Services\FactoryService;
+use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\SchemaBundle\Document\Broadcast;
 
 class FactoryServiceTest extends WebTestCase
 {
@@ -26,6 +27,8 @@ class FactoryServiceTest extends WebTestCase
       $this->factory = $kernel->getContainer()
 	  ->get('pumukitschema.factory');
 
+      $this->dm->getDocumentCollection('PumukitSchemaBundle:Broadcast')
+	  ->remove(array());
       $this->dm->getDocumentCollection('PumukitSchemaBundle:MultimediaObject')
 	  ->remove(array());
       $this->dm->getDocumentCollection('PumukitSchemaBundle:Series')
@@ -35,9 +38,49 @@ class FactoryServiceTest extends WebTestCase
 
     public function testCreateSeries()
     {
+      $this->createBroadcasts();
+
       $this->factory->createSeries();
 
       $this->assertEquals(1, count($this->dm->getRepository('PumukitSchemaBundle:Series')));
       $this->assertEquals(1, count($this->dm->getRepository('PumukitSchemaBundle:MultimediaObject')));
+    }
+
+    public function testCreateMultimediaObject()
+    {
+      $this->createBroadcasts();
+
+      $series = new Series();
+      $this->dm->persist($series);
+      $this->dm->flush();
+
+      $this->factory->createMultimediaObject($series);
+      
+      $this->assertEquals(1, count($this->dm->getRepository('PumukitSchemaBundle:Series')));
+      $this->assertEquals(1, count($this->dm->getRepository('PumukitSchemaBundle:MultimediaObject')));
+      $this->assertEquals($series, $this->dm->getRepository('PumukitSchemaBundle:Series')->findAll()[0]);
+    }
+
+    public function createBroadcasts()
+    {
+      $broadcastPrivate = new Broadcast();
+      $broadcastPrivate->setBroadcastTypeId(Broadcast::BROADCAST_TYPE_PRI);
+      $broadcastPrivate->setDefaultSel(true);
+      $broadcastPrivate->setName('Private');
+
+      $broadcastPublic = new Broadcast();
+      $broadcastPublic->setBroadcastTypeId(Broadcast::BROADCAST_TYPE_PUB);
+      $broadcastPublic->setDefaultSel(false);
+      $broadcastPublic->setName('Public');
+
+      $broadcastCorporative = new Broadcast();
+      $broadcastCorporative->setBroadcastTypeId(Broadcast::BROADCAST_TYPE_COR);
+      $broadcastCorporative->setDefaultSel(false);
+      $broadcastCorporative->setName('Corporative');
+
+      $this->dm->persist($broadcastPrivate);
+      $this->dm->persist($broadcastPublic);
+      $this->dm->persist($broadcastCorporative);
+      $this->dm->flush();
     }
 }
