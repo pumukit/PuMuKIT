64c99e66d5d833469c3e12616046a346ea211c65
Mercedes Rey <mrey@teltek.es>
2014-12-17 13:14:09 +0100

    Added Element Test

---
+++ src/Pumukit/SchemaBundle/Tests/Document/ElementTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Tests\Document;
+
+use Pumukit\SchemaBundle\Document\Element;
+
+class ElementTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetterAndSetter()
+    {
+        $tags = array('tag_a', 'tag_b');
+        $locale = 'en';
+        $url = '/mnt/video/123/23435.mp4';
+        $path = '/mnt/video/123/23435.mp4';
+        $mime = 'video/mpeg4';
+        $hide = false;
+	$format = 'formato';
+	$description = 'element description';
+
+        $element = new Element();
+        $element->setTags($tags);
+        $element->setLocale($locale);
+        $element->setUrl($url);
+        $element->setPath($path);
+        $element->setMimeType($mime);
+        $element->setHide($hide);
+	$element->setFormat($format);
+	$element->setDescription($description);
+
+        $this->assertEquals($tags, $element->getTags());
+        $this->assertEquals($locale, $element->getLocale());
+        $this->assertEquals($url, $element->getUrl());
+        $this->assertEquals($path, $element->getPath());
+        $this->assertEquals($mime, $element->getMimeType());
+        $this->assertFalse($hide, $element->getHide());
+        $this->assertEquals($format, $element->getFormat());
+	$this->assertEquals($description, $element->getDescription());
+    }
+
+    public function testMaxSize()
+    {
+        $size = 5368709120; // 5GB, integer types in 32 bits machines only admit 2GB
+
+        $element = new Element();
+        $element->setSize($size);
+        $this->assertEquals($size, $element->getSize());
+    }
+
+    public function testTagCollection()
+    {
+        $element = new Element();
+        $this->assertFalse($element->containsTag('t'));
+        $element->addTag('t');
+        $this->assertTrue($element->containsTag('t'));
+        $element->removeTag('t');
+        $this->assertFalse($element->containsTag('t'));
+
+        //Repeat Tag
+        $this->assertFalse($element->containsTag('t'));
+        $element->addTag('t');
+        $element->addTag('t');
+        $this->assertTrue($element->containsTag('t'));
+        $element->removeTag('t');
+        $this->assertFalse($element->containsTag('t'));
+        $this->assertFalse($element->removeTag('t'));
+
+        //containsAllTag and containsAnyTag
+        $element->addTag('t1');
+        $element->addTag('t2');
+        $element->addTag('t3');
+        $this->assertTrue($element->containsAnyTag(array('t0', 't2')));
+        $this->assertTrue($element->containsAnyTag(array('t2', 't3')));
+        $this->assertFalse($element->containsAnyTag(array('t0', 't4')));
+        $this->assertTrue($element->containsAllTags(array('t1', 't2')));
+        $this->assertTrue($element->containsAllTags(array('t1')));
+        $this->assertFalse($element->containsAllTags(array('t0', 't2')));
+        $this->assertFalse($element->containsAllTags(array('t0', 't1', 't2', 't3')));
+    }
+}
