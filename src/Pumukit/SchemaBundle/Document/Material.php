ba61b76e899ebdab9c5e69c1a2c900d0cc445840
Mercedes Rey <mrey@teltek.es>
2015-01-07 10:21:42 +0100

    Added name field to document Material

--- src/Pumukit/SchemaBundle/Document/Material.php
+++ src/Pumukit/SchemaBundle/Document/Material.php
@@ -11,4 +11,60 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
  */
 class Material extends Element
 {
+    /**
+     * @var string $name
+     *
+     * @MongoDB\Raw
+     */
+    private $name = array('en' => '');
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name, $locale = null)
+    {
+        if ($locale == null) {
+            $locale = $this->locale;
+        }
+        $this->name[$locale] = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    private function getName($locale = null)
+    {
+        if ($locale == null) {
+            $locale = $this->locale;
+        }
+        if (!isset($this->name[$locale])) {
+            return;
+        }
+        
+        return $this->name[$locale];
+    }
+    
+    /**
+     * Set I18n name
+     *
+     * @param array $name
+     */
+    public function setI18nName(array $name)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get I18n name
+     *
+     * @return array
+     */
+    public function getI18nName()
+    {
+        return $this->name;
+    }
 }
