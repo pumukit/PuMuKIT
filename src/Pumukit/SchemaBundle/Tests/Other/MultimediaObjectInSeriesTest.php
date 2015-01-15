6bc4a7726c6eba74a25a5dafb0c2be183d922283
Ruben Gonzalez <rubenrua@gmail.com>
2015-01-15 15:58:47 +0100

    Add test for bug in doctrine/mongo-odm

---
+++ src/Pumukit/SchemaBundle/Tests/Other/MultimediaObjectInSeriesTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This test signs a bug in 'doctrine/mongodb-odm'. The bug is similar to #981.
+ * Pumukit2 has the next workaround while the bug is not fixed:
+ *
+ * +      $mm->setSeries($series);
+ * -      $series->addMultimediaObject($mm);
+ *
+ */
+
+namespace Pumukit\SchemaBundle\Tests\Other;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+class MultimediaObjectInSeriesTest extends WebTestCase
+{
+    private $dm;
+    private $seriesRepo;
+    private $mmobjRepo;
+    private $factoryService;
+
+    public function setUp()
+    {
+        $options = array('environment' => 'test');
+        static::bootKernel($options);
+
+        $container = static::$kernel->getContainer();
+        $this->factoryService = $container->get('pumukitschema.factory');
+        $this->dm = $container->get('doctrine_mongodb')->getManager();
+        $this->seriesRepo = $this->dm->getRepository('PumukitSchemaBundle:Series');
+        $this->mmobjRepo = $this->dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+
+        $this->dm->getDocumentCollection('PumukitSchemaBundle:Series')->remove(array());
+        $this->dm->getDocumentCollection('PumukitSchemaBundle:MultimediaObject')->remove(array());
+    }
+
+    public function testCreateNewMultimediaObject()
+    {
+        $series = $this->factoryService->createSeries();
+        $id = $series->getId();
+        
+        $this->dm->clear();
+
+        $series = $this->seriesRepo->find($id);
+        $this->factoryService->createMultimediaObject($series);
+
+        $coll_mms = $series->getMultimediaObjects();
+
+        //echo "Assert\n";
+        $this->assertEquals(1, count($coll_mms));
+
+        //echo "Foreach\n";
+        $i = 0;
+        foreach($coll_mms as $mm) {
+          $i++;
+          //echo "\t - ", $mm->getId(), "\n";
+        }
+        $this->assertEquals(1, $i);
+    }
+
+}
