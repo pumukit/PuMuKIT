46e341cf95009bb1637b54b97d45a490e6da3caa
aaron <aaron.aballe@gmail.com>
2015-01-19 16:10:21 +0100

    Created bundle WebTVBundle

---
+++ src/Pumukit/WebTVBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pumukit\WebTVBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
---
+++ src/Pumukit/WebTVBundle/PumukitWebTVBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pumukit\WebTVBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PumukitWebTVBundle extends Bundle
+{
+}
