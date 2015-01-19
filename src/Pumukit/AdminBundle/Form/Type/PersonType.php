0b74b827bc05a281168741885b09ad3453a632fb
Mercedes Rey <mrey@teltek.es>
2015-01-19 19:51:39 +0100

    Added incomplete CRUD of Person

---
+++ src/Pumukit/AdminBundle/Form/Type/PersonType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pumukit\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Pumukit\SchemaBundle\Document\Person;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+      ->add('i18n_honorific', 'texti18n',
+        array('label' => 'Honorific'))
+      ->add('name', 'text',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Name'))
+      ->add('i18n_post', 'texti18n',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Post'))
+      ->add('i18n_firm', 'texti18n',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Firm'))
+      ->add('i18n_bio', 'textareai18n',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Bio'))
+      ->add('email', 'text',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Email'))
+      ->add('web', 'text',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Web'))
+      ->add('phone', 'text',
+        array('attr' => array('style' => 'width: 420px'), 'label' => 'Phone'));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+        'data_class' => 'Pumukit\SchemaBundle\Document\Person',
+    ));
+    }
+
+    public function getName()
+    {
+        return 'pumukitadmin_person';
+    }
+}
