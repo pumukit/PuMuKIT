356bf8f3fc4177971fe6c577e57c5334044ee5e3
Ruben Gonzalez <rubenrua@gmail.com>
2015-01-22 17:11:29 +0100

    Add more info about jobs

--- src/Pumukit/EncoderBundle/Command/PumukitEncoderListCommand.php
+++ src/Pumukit/EncoderBundle/Command/PumukitEncoderListCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Pumukit\SchemaBundle\Document\Tag;
+use Pumukit\EncoderBundle\Document\Job;
 
 class PumukitEncoderListCommand extends ContainerAwareCommand
 {
@@ -18,6 +18,7 @@ class PumukitEncoderListCommand extends ContainerAwareCommand
         $this
             ->setName('pumukit:encoder:list')
             ->setDescription('Pumukit list stats about encoder jobs')
+            ->addOption('all', null, InputOption::VALUE_NONE, 'Set this parameter to list jobs in all states')
             ->setHelp(<<<EOT
 TODO
 
@@ -28,7 +29,7 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->listCpus($output);
-        $this->listJobs($output);        
+        $this->listJobs($output, $input->getOption('all'));
     }
 
 
@@ -54,7 +55,7 @@ EOT
         $table->render();
     }
 
-    private function listJobs(OutputInterface $output)
+    private function listJobs(OutputInterface $output, $all = false)
     {
         $dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
         $jobRepo = $dm->getRepository('PumukitEncoderBundle:Job');
@@ -66,6 +67,33 @@ EOT
         $table = new Table($output);
         $table->setHeaders(array_keys($stats));
         $table->addRow(array_values($stats));
+        $table->render();
+
+        if ($all) {
+            $status = array_keys(Job::$statusTexts);
+        }else{
+            $status = array(Job::STATUS_EXECUTING);
+        }
+        $jobs = $jobRepo->findWithStatus($status);
+
+        $output->writeln("<info>JOBS:</info>");
+        $table = new Table($output);
+        $table->setHeaders(array('Id', 'Priority', 'MM', 'Profile', 'Cpu', 'Status',
+                                 'Timeini', 'Timestart', 'Timeend'));
+
+        foreach($jobs as $name => $job) {
+            $table->addRow(array(
+                $job->getId(),
+                Job::$statusTexts[$job->getStatus()],
+                $job->getMmId(),
+                $job->getProfile(),
+                $job->getCpu(),
+                $job->getPriority(),
+                $job->getTimeini('Y-m-d H:i:s'),
+                $job->getTimestart('Y-m-d H:i:s'),
+                $job->getTimeend('Y-m-d H:i:s'),
+            ));
+        }
         $table->render();
     }
 }
