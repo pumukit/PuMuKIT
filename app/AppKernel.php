275337022345a7d292c2e8ae3cede1a20f484e09
rubenrua <rubenrua@teltek.es>
2015-02-05 18:21:51 +0100

    new PumukitNewAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new Pumukit\EncoderBundle\PumukitEncoderBundle(),
             new Pumukit\InspectionBundle\PumukitInspectionBundle(),
             new Pumukit\WebTVBundle\PumukitWebTVBundle(),
+            new Pumukit\NewAdminBundle\PumukitNewAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
