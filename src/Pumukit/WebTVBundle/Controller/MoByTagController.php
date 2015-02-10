56b2ffc81b6ba226795c4db1bc23d5e55b3ae27b
Aaron Aballe <aaron.aballe@gmail.com>
2015-02-10 14:09:02 +0100

    Search by tag multimedia objects sorted by date

--- src/Pumukit/WebTVBundle/Controller/MoByTagController.php
+++ src/Pumukit/WebTVBundle/Controller/MoByTagController.php
@@ -11,26 +11,26 @@ use Pumukit\SchemaBundle\Document\Tag;
 class MoByTagController extends Controller
 {
     /**
-     * @Route("/multimediaobjects/tag/{id}/{sort}", defaults={"sort" = "date"}, requirements={"sort" = "alphabetically|date"})
+     * @Route("/multimediaobjects/tag/{id}")
      * @Template()
      */
-    public function indexAction(Tag $tag,$sort, Request $request)
+    public function indexAction(Tag $tag, Request $request)
     {
     	$repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
 
         dump($tag);
         dump($request->getLocale());
 
-        if($sort == "alphabetically"){
+        /*if($sort == "alphabetically"){
     		$mmobjs = $repo->findWithTag($tag, array('alphabetically.' . $request->getLocale() => +1));
         }
-        else{
-        	$mmobjs = $repo->findWithTag($tag, array('date' => +1));
+        else{*/
+        	$mmobjs = $repo->findWithTag($tag, array('record_date' => +1));
         	dump($mmobjs);
-        }
+       // }
 
         dump($mmobjs);
 
-        return array('mmobjs' => $mmobjs, 'sort' => $sort, 'tag' => $tag);
+        return array('mmobjs' => $mmobjs, 'tag' => $tag);
     }
 }
