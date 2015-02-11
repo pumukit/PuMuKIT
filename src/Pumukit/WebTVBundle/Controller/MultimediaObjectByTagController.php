1086a4831a982b3a5358d221da42e715d0e47813
Aaron Aballe <aaron.aballe@gmail.com>
2015-02-11 13:34:50 +0100

    Changed names to files

--- src/Pumukit/WebTVBundle/Controller/MultimediaObjectByTagController.php
+++ src/Pumukit/WebTVBundle/Controller/MultimediaObjectByTagController.php
@@ -6,9 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Adapter\DoctrineODMMongoDBAdapter;
+use Pagerfanta\Pagerfanta;
 use Pumukit\SchemaBundle\Document\Tag;
 
-class MoByTagController extends Controller
+class MultimediaObjectByTagController extends Controller
 {
     /**
      * @Route("/multimediaobjects/tag/{id}")
@@ -16,6 +18,11 @@ class MoByTagController extends Controller
      */
     public function indexAction(Tag $tag, Request $request)
     {
+        $limit = 4;
+        $page =  $request->get("page", 1);
+
+        dump($page);
+
     	$repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
 
         dump($tag);
@@ -25,12 +32,17 @@ class MoByTagController extends Controller
     		$mmobjs = $repo->findWithTag($tag, array('alphabetically.' . $request->getLocale() => +1));
         }
         else{*/
-        	$mmobjs = $repo->findWithTag($tag, array('record_date' => +1));
+        	$mmobjs = $repo->createBuilderWithTag($tag, array('record_date' => 1));
         	dump($mmobjs);
        // }
 
-        dump($mmobjs);
+        $adapter = new DoctrineODMMongoDBAdapter($mmobjs);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage($limit); // 10 by default
+        $pagerfanta->setCurrentPage($page); // 1 by default
 
-        return array('mmobjs' => $mmobjs, 'tag' => $tag);
+        dump($pagerfanta);
+
+        return array('mmobjs' => $pagerfanta, 'tag' => $tag);
     }
 }
