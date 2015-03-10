a34f13c6ea00194e84f866f0fa40852d33ec6f09
Aaron Aballe <aaron.aballe@gmail.com>
2015-03-10 16:48:42 +0100

    Added pager to series

--- src/Pumukit/WebTVBundle/Controller/SeriesController.php
+++ src/Pumukit/WebTVBundle/Controller/SeriesController.php
@@ -3,10 +3,14 @@
 namespace Pumukit\WebTVBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Adapter\DoctrineODMMongoDBAdapter;
+use Pagerfanta\Pagerfanta;
 use Pumukit\SchemaBundle\Document\Series;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Track;
 
 class SeriesController extends Controller
 {
@@ -14,10 +18,20 @@ class SeriesController extends Controller
      * @Route("/series/{id}")
      * @Template()
      */
-    public function indexAction(Series $series)
+    public function indexAction(Series $series, Request $request)
     {
-    	$mmobjs = $series->getMultimediaObjects();
+        $limit = 2;
+        $page =  $request->get("page", 1);
+
+    	$multimediaObjects = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $queryBuilder = $multimediaObjects->createStandardQueryBuilder();
+        $queryBuilder->field('series')->equals($series->getId());
+
+        $adapter = new DoctrineODMMongoDBAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage($limit);
+        $pagerfanta->setCurrentPage($page);
     	
-        return array('series' => $series, 'mmobjs' => $mmobjs);
+        return array('series' => $series, 'multimediaObjects' => $pagerfanta);
     }
 }
