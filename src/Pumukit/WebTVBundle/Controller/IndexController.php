d3e9a06edd51c0cee579a80654b2152f95a616d1
Aaron Aballe <aaron.aballe@gmail.com>
2015-03-11 17:17:11 +0100

    Added most viewed and recently added to index page

--- src/Pumukit/WebTVBundle/Controller/IndexController.php
+++ src/Pumukit/WebTVBundle/Controller/IndexController.php
@@ -5,6 +5,8 @@ namespace Pumukit\WebTVBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Series;
 
 class IndexController extends Controller
 {
@@ -14,6 +16,18 @@ class IndexController extends Controller
      */
     public function indexAction()
     {
-        return array();
+    	$series_repository = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Series');
+    	$num_series = count($series_repository->findall());
+
+        $repository = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+
+        $multimediaObjects_sorted_by_numview = $repository->findBy(array(), array('numview' => -1));
+        $multimediaObjects_sorted_by_numview = array_slice($multimediaObjects_sorted_by_numview, 0, 3);
+
+        $multimediaObjects_sorted_by_public_date = $repository->findBy(array(), array('public_date' => -1));
+        $multimediaObjects_sorted_by_public_date = array_slice($multimediaObjects_sorted_by_public_date, 0, 3);
+
+        return array('num_series' => $num_series, 'multimediaObjects_sorted_by_numview' => $multimediaObjects_sorted_by_numview, 
+            'multimediaObjects_sorted_by_public_date' => $multimediaObjects_sorted_by_public_date);
     }
 }
