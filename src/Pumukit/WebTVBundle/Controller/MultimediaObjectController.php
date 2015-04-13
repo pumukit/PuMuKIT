3146c3869891699ff0f2a9976778755cbe13b91b
rubenrua <rubenrua@teltek.es>
2015-04-13 17:30:29 +0200

    Start webtvbundle review

--- src/Pumukit/WebTVBundle/Controller/MultimediaObjectController.php
+++ src/Pumukit/WebTVBundle/Controller/MultimediaObjectController.php
@@ -3,22 +3,60 @@
 namespace Pumukit\WebTVBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\SchemaBundle\Document\Broadcast;
 
 class MultimediaObjectController extends Controller
 {
     /**
      * @Route("/video/{id}")
-     * @Template()
+     * @Template("PumukitWebTVBundle:MultimediaObject:index.html.twig")
      */
-    public function indexAction(MultimediaObject $multimediaObject)
+    public function indexByIdAction(MultimediaObject $multimediaObject, Request $request)
     {
+      if (MultimediaObject::STATUS_PUBLISHED !== $multimediaObject->getStatus())
+        throw $this->createNotFoundException();
+     
+      $track = $request->query->has('track_id') ?
+        $multimediaObject->getTrackById($request->query->get('track_id')) :
+        $multimediaObject->getTrackWithTag('display');
 
-    	$Serie = $multimediaObject->getSeries();
-    	$multimediaObjects = $Serie->getMultimediaObjects();
-        return array('multimediaObject' => $multimediaObject, 'multimediaObjects' => $multimediaObjects);
+      if (!$track)
+        throw $this->createNotFoundException();
+
+      if (($broadcast = $multimediaObject->getBroadcast()) && 
+          (Broadcast::BROADCAST_TYPE_PUB !== $broadcast->getBroadcastTypeId()))
+        //TODO.
+        throw $this->createNotFoundException();
+        
+      $serie = $multimediaObject->getSeries();
+      $multimediaObjects = $serie->getMultimediaObjects();
+      return array('multimediaObject' => $multimediaObject, 
+                   'track' => $track,
+                   'multimediaObjects' => $multimediaObjects);
+     
+    }
+
+
+    /**
+     * @Route("/video/magic/{secret}")
+     * @Template("PumukitWebTVBundle:MultimediaObject:index.html.twig")
+     */
+    public function indexByMagicIdAction(MultimediaObject $multimediaObject, Request $request)
+    {
+      $track = $request->query->has('track_id') ?
+        $multimediaObject->getTrackById($request->query->get('track_id')) :
+        $multimediaObject->getTrackWithTag('display');
+
+      $serie = $multimediaObject->getSeries();
+      $multimediaObjects = $serie->getMultimediaObjects();
+      return array('multimediaObject' => $multimediaObject, 
+                   'track' => $track,
+                   'multimediaObjects' => $multimediaObjects);
     }
 }
