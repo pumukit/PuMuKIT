866aa40c1587737f2db168237c97e83c1700926f
Mercedes Rey <mrey@teltek.es>
2015-04-15 19:49:37 +0200

    Completed templating WizardBundle

--- src/Pumukit/WizardBundle/Controller/DefaultController.php
+++ src/Pumukit/WizardBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Intl\Intl;
 
 class DefaultController extends Controller
 {
@@ -24,15 +25,10 @@ class DefaultController extends Controller
      */
     public function seriesAction(Request $request)
     {
-        $seriesData = array();
-
-        $typeForm = $request->get('pumukitwizard_type');
-        if ($typeForm){
-          $seriesData = $typeForm['series'];
-        }
+        $formData = $request->get('pumukitwizard_form_data');
 
         return array(
-                     'series_data' => $seriesData
+                     'form_data' => $formData
                      );
     }
 
@@ -41,19 +37,74 @@ class DefaultController extends Controller
      */
     public function typeAction($id, Request $request)
     {
-        $showPrevious = true;
-        // TODO id of existent series
-        if ('null' !== $id) $showPrevious = false;
+        $formData = $request->get('pumukitwizard_form_data');
 
-        $seriesForm = array();
-        if ($showPrevious){
-          $seriesForm = $request->get('pumukitwizard_series');
+        $showPrevious = true;
+        $seriesRepo = $this->get('doctrine_mongodb.odm.document_manager')
+            ->getRepository('PumukitSchemaBundle:Series');
+        $series = $seriesRepo->find($id);
+
+        if ($series){
+            $showPrevious = false;
+            if (!$formData){
+                $formData = array('series' => array(
+                                                   'i18n_title' => $series->getI18nTitle(),
+                                                   'i18n_description' => $series->getI18nDescription()
+                                                   ));
+            }
         }
 
         return array(
                      'series_id' => $id,
-                     'series_form' => $seriesForm,
+                     'form_data' => $formData,
                      'show_previous' => $showPrevious
+                     );
+    }
+
+    /**
+     * Option action
+     */
+    public function optionAction(Request $request)
+    {
+        $formData = $request->get('pumukitwizard_form_data');
+
+        if ('multiple' == $formData['type']['option']){
+            return $this->redirect($this->generateUrl('pumukitwizard_default_track', array('pumukitwizard_form_data' => $formData)));
+        }
+
+        return $this->redirect($this->generateUrl('pumukitwizard_default_multimediaobject', array('pumukitwizard_form_data' => $formData)));
+    }
+
+    /**
+     * @Template()
+     */
+    public function multimediaobjectAction(Request $request)
+    {
+        $formData = $request->get('pumukitwizard_form_data');
+
+        return array(
+                     'form_data' => $formData
+                     );
+    }
+
+    /**
+     * @Template()
+     */
+    public function trackAction(Request $request)
+    {
+        $formData = $request->get('pumukitwizard_form_data');
+
+        $masterProfiles = $this->get('pumukitencoder.profile')->getMasterProfiles(true);
+        $factoryService = $this->get('pumukitschema.factory');
+        $pubChannelsTags = $factoryService->getTagsByCod('PUBCHANNELS', true);
+
+        $languages = Intl::getLanguageBundle()->getLanguageNames();
+
+        return array(
+                     'form_data' => $formData,
+                     'master_profiles' => $masterProfiles,
+                     'pub_channels' => $pubChannelsTags,
+                     'languages' => $languages
                      );
     }
 }
