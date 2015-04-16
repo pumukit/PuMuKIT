eb4de93b9511704e48ba0ae8117fe3f6e36b490a
Mercedes Rey <mrey@teltek.es>
2015-04-16 17:59:04 +0200

    Added upload tracks with Wizard

--- src/Pumukit/WizardBundle/Controller/DefaultController.php
+++ src/Pumukit/WizardBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Intl\Intl;
+use Symfony\Component\Finder\Finder;
 
 class DefaultController extends Controller
 {
@@ -106,5 +107,184 @@ class DefaultController extends Controller
                      'pub_channels' => $pubChannelsTags,
                      'languages' => $languages
                      );
+    }
+
+    /**
+     * Upload action
+     * @Template()
+     */
+    public function uploadAction(Request $request)
+    {
+        $trackService = $this->get('pumukitschema.track');
+
+        $formData = $request->get('pumukitwizard_form_data');
+        if ($formData){
+            $seriesData = $this->getKeyData('series', $formData);
+            $typeData = $this->getKeyData('type', $formData);
+            $trackData = $this->getKeyData('track', $formData);
+
+            $profile = $this->getKeyData('profile', $trackData);
+            $priority = $this->getKeyData('priority', $trackData);
+            $language = $this->getKeyData('language', $trackData);
+            $description = $this->getKeyData('description', $trackData);
+
+            $pubchannel = $this->getKeyData('pubchannel', $trackData);
+
+            // TODO try catch
+            /* if (empty($_FILES) && empty($_POST)){ */
+            /*     throw new \Exception('PHP ERROR: File exceeds post_max_size ('.ini_get('post_max_size').')'); */
+            /* } */
+
+            $option = $this->getKeyData('option', $typeData);
+            if ('single' === $option){
+                $series = $this->getSeries($seriesData);
+                $multimediaobjectData = $this->getKeyData('multimediaobject', $formData);
+                $multimediaObject = $this->createMultimediaObject($multimediaobjectData, $series);
+
+                $filetype = $this->getKeyData('filetype', $trackData);
+                if ('file' === $filetype){
+                    $selectedPath = $request->get('resource');
+                    $multimediaObject = $trackService->createTrackFromLocalHardDrive($multimediaObject, $request->files->get('resource'), $profile, $priority, $language, $description);
+                }elseif ('inbox' === $filetype){
+                    $selectedPath = $request->get('file');
+                    $multimediaObject = $trackService->createTrackFromInboxOnServer($multimediaObject, $request->get('file'), $profile, $priority, $language, $description);
+                }
+                // TODO add pub channel if multimediaobject
+            }elseif ('multiple' === $option){
+                $series = $this->getSeries($seriesData);
+                $selectedPath = $request->get('file');
+                $finder = new Finder();
+                $finder->files()->in($selectedPath);
+                foreach ($finder as $f){
+                    $filePath = $f->getRealpath();
+                    $multimediaObject = $this->createMultimediaObject(array(), $series);
+                    if ($multimediaObject){
+                        $multimediaObject = $trackService->createTrackFromInboxOnServer($multimediaObject, $filePath, $profile, $priority, $language, $description);
+                        // TODO add pub channel
+                    }
+                }
+            }
+
+        }else{
+            // TODO THROW EXCEPTION OR RENDER SPECIFIC TEMPLATE WITH MESSAGE
+        }
+
+
+        return array();
+    }
+
+    /**
+     * @Template()
+     */
+    public function endAction(Request $request)
+    {
+        // TODO complete
+
+        return array();
+    }
+
+    /**
+     * Get key data
+     */
+    private function getKeyData($key='nonexistingkey', $formData=array())
+    {
+        $keyData = array();
+        if(array_key_exists($key, $formData)){
+            $keyData = $formData[$key];
+        }
+
+        return $keyData;
+    }
+
+    /**
+     * Get series (new or existing one)
+     */
+    private function getSeries($seriesData=array())
+    {
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+        $seriesRepo = $dm->getRepository('PumukitSchemaBundle:Series');
+
+        $seriesId = $this->getKeyData('id', $seriesData);
+        if ($seriesId && ('null' !== $seriesId)){
+            $series = $seriesRepo->find($seriesId);
+        }else{
+            $series = $this->createSeries($seriesData);
+        }
+
+        return $series;
+    }
+
+    /**
+     * Create Series
+     */
+    private function createSeries($seriesData=array())
+    {
+        if ($seriesData){
+            $factoryService = $this->get('pumukitschema.factory');
+            $series = $factoryService->createSeries();
+
+            $keys = array('i18n_title', 'i18n_description');
+            $series = $this->setData($series, $seriesData, $keys);
+
+            return $series;
+        }
+
+        return null;
+    }
+
+    /**
+     * Create Multimedia Object
+     */
+    private function createMultimediaObject($mmData, $series)
+    {
+        if ($series){
+            $factoryService = $this->get('pumukitschema.factory');
+            $multimediaObject = $factoryService->createMultimediaObject($series);
+
+            if ($mmData){
+                $keys = array('i18n_title', 'i18n_subtitle', 'i18n_description', 'i18n_line2');
+                $multimediaObject = $this->setData($multimediaObject, $mmData, $keys);
+            }
+
+            return $multimediaObject;
+        }
+
+        return null;
+    }
+
+    /**
+     * Set data
+     */
+    private function setData($resource, $resourceData, $keys)
+    {
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+
+        foreach ($keys as $key){
+            $value = $this->getKeyData($key, $resourceData);
+            if ($value){
+                $upperField = $this->getUpperFieldName($key);
+                $setField = 'set'.$upperField;
+                $resource->$setField($value);
+            }
+        }
+
+        $dm->persist($resource);
+        $dm->flush();
+
+        return $resource;
+    }
+
+    /**
+     * Get uppercase field name
+     * Converts something like 'i18n_title' into 'I18nTitle'
+     */
+    private function getUpperFieldName($key='')
+    {
+        $pattern = "/_[a-z]?/";
+        $aux = preg_replace_callback($pattern, function($matches){
+            return strtoupper(ltrim($matches[0], "_"));
+          }, $key);
+
+        return ucfirst($aux);
     }
 }
