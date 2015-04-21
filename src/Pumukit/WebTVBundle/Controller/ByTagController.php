75c236fe884068734bede0fea8ca5ee408d6e16e
rubenrua <rubenrua@teltek.es>
2015-04-21 12:04:56 +0200

    Join MultimediaObjectByTagController and SeriesByTagController in ByTagController

---
+++ src/Pumukit/WebTVBundle/Controller/ByTagController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Pumukit\WebTVBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Adapter\DoctrineODMMongoDBAdapter;
+use Pagerfanta\Pagerfanta;
+use Pumukit\SchemaBundle\Document\Tag;
+
+class ByTagController extends Controller
+{
+
+  private $limit = 3;
+
+  /**
+   * @Route("/multimediaobjects/tag/{cod}")
+   * @Template("PumukitWebTVBundle:ByTag:index.html.twig")
+   */
+  public function multimediaObjectsAction(Tag $tag, Request $request)
+  {
+    $repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+    $mmobjs = $repo->createBuilderWithTag($tag, array('record_date' => 1));
+    
+    $pagerfanta = $this->createPager($mmobjs, $request->query->get("page", 1));
+    
+    return array('objects' => $pagerfanta, 'tag' => $tag);
+  }
+  
+  /**
+   * @Route("/series/tag/{cod}")
+   * @Template("PumukitWebTVBundle:ByTag:index.html.twig")
+   */
+  public function seriesAction(Tag $tag, Request $request)
+  {  
+    $repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Series');
+    $series = $repo->createBuilderWithTag($tag, array('public_date' => +1));
+    
+    $pagerfanta = $this->createPager($series, $request->query->get("page", 1));
+    
+    return array('objects' => $pagerfanta, 'tag' => $tag);
+  }
+
+
+
+  private function createPager($objects, $page)
+  {
+    $adapter = new DoctrineODMMongoDBAdapter($objects);
+    $pagerfanta = new Pagerfanta($adapter);
+    $pagerfanta->setMaxPerPage($this->limit);
+    $pagerfanta->setCurrentPage($page);    
+
+    return $pagerfanta;
+  }
+}
