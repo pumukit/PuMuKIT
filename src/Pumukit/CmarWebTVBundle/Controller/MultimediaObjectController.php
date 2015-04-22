6103c626a37414ec0973a5f858f65aa3c1f0cf3d
rubenrua <rubenrua@teltek.es>
2015-04-22 18:15:14 +0200

    Clean CmarWebTVBundle

---
+++ src/Pumukit/CmarWebTVBundle/Controller/MultimediaObjectController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Pumukit\CmarWebTVBundle\Controller;
+
+use Pumukit\WebTVBundle\Controller\MultimediaObjectController as Base;
+
+class MultimediaObjectController extends Base
+{}
---
+++ src/Pumukit/CmarWebTVBundle/Controller/SeriesController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Pumukit\CmarWebTVBundle\Controller;
+
+use Pumukit\WebTVBundle\Controller\SeriesController as Base;
+
+class SeriesController extends Base
+{}
