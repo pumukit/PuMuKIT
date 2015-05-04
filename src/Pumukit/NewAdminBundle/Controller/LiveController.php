3bc599174b8cd8270aede929aab997edef87caaa
Mercedes Rey <mrey@teltek.es>
2015-05-04 12:57:50 +0200

    Fixed change page when new Live channel is created

---
+++ src/Pumukit/NewAdminBundle/Controller/LiveController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Pumukit\NewAdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LiveController extends AdminController
+{
+    /**
+     * Create Action
+     * Overwrite to return json response
+     * and update page
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse|Response
+     */
+    public function createAction(Request $request)
+    {
+        $config = $this->getConfiguration();
+
+        $resource = $this->createNew();
+        $form = $this->getForm($resource);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $resource = $this->domainManager->create($resource);
+
+            if ($this->config->isApiRequest()) {
+                return $this->handleView($this->view($resource, 201));
+            }
+
+            if (null === $resource) {
+                return new JsonResponse(array('liveId' => null));
+            }
+            $this->get('session')->set('admin/live/id', $resource->getId());
+
+            return new JsonResponse(array('liveId' => $resource->getId()));
+        }
+
+        if ($this->config->isApiRequest()) {
+            return $this->handleView($this->view($form));
+        }
+
+        return $this->render("PumukitNewAdminBundle:Live:create.html.twig",
+                             array(
+                                   'live' => $resource,
+                                   'form' => $form->createView()
+                                   ));
+    }
+
+    /**
+     * Gets the list of resources according to a criteria
+     */
+    public function getResources(Request $request, $config, $criteria)
+    {
+        $sorting = $config->getSorting();
+        $repository = $this->getRepository();
+        $session = $this->get('session');
+        $session_namespace = 'admin/live';
+
+        $newLiveId = $request->get('newLiveId');
+        $page = $session->get($session_namespace.'/page', 1);
+
+        if ($config->isPaginated()) {
+            $resources = $this
+                ->resourceResolver
+                ->getResource($repository, 'createPaginator', array($criteria, $sorting));
+
+            if ($request->get('page', null)) {
+                $session->set($session_namespace.'/page', $request->get('page', 1));
+            }
+
+            if ($request->get('paginate', null)) {
+                $session->set($session_namespace.'/paginate', $request->get('paginate', 10));
+            }
+
+            $resources
+                ->setMaxPerPage($session->get($session_namespace.'/paginate', 10))
+              ->setNormalizeOutOfRangePages(true);
+
+            if ($newLiveId && (($resources->getNbResults()/$resources->getMaxPerPage()) > $page)) {
+                $page = $resources->getNbPages();
+                $session->set($session_namespace.'/page', $page);
+            }
+            $resources->setCurrentPage($page);
+        } else {
+            $resources = $this
+                ->resourceResolver
+                ->getResource($repository, 'findBy', array($criteria, $sorting, $config->getLimit()));
+        }
+
+        return $resources;
+    }
+}
