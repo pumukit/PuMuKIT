9fa7c59cdb22b627a8caf978a95f381936067be5
mrey <mrey@teltek.es>
2015-05-21 11:56:34 +0200

    CmarWebTVBundle - MultimediaObjectController: added old browser verification

--- src/Pumukit/Cmar/WebTVBundle/Controller/MultimediaObjectController.php
+++ src/Pumukit/Cmar/WebTVBundle/Controller/MultimediaObjectController.php
@@ -12,8 +12,49 @@ class MultimediaObjectController extends Base
         if ($opencasturl = $multimediaObject->getProperty("opencasturl")) {
             $this->updateBreadcrumbs($multimediaObject);
             $this->incNumView($multimediaObject);
-            // TODO is_old_browser
-            return $this->render("PumukitCmarWebTVBundle:MultimediaObject:opencast.html.twig", array("multimediaObject" => $multimediaObject));
+            $userAgent = $this->getRequest()->headers->get('user-agent');
+            $isOldBrowser = $this->getIsOldBrowser($userAgent);
+            return $this->render("PumukitCmarWebTVBundle:MultimediaObject:opencast.html.twig",
+                                 array(
+                                       "multimediaObject" => $multimediaObject,
+                                       "is_old_browser" => $isOldBrowser
+                                       )
+                                 );
         }
+    }
+
+    private function getIsOldBrowser($userAgent)
+    {
+        $isOldBrowser = false;
+        $webExplorer = $this->getWebExplorer($userAgent);
+        $version = $this->getVersion($userAgent, $webExplorer);
+        if (($webExplorer == 'IE') || ($webExplorer == 'MSIE') || $webExplorer == 'Firefox' || $webExplorer == 'Opera' || ($webExplorer == 'Safari' && $version<4)){
+            $isOldBrowser = true;
+        }
+
+        return $isOldBrowser;
+    }
+
+    private function getWebExplorer($userAgent)
+    {
+        if (preg_match('/MSIE/i', $userAgent))         $webExplorer = "MSIE";
+        if (preg_match('/Opera/i', $userAgent))        $webExplorer = 'Opera';
+        if (preg_match('/Firefox/i', $userAgent))      $webExplorer = 'Firefox';
+        if (preg_match('/Safari/i', $userAgent))       $webExplorer = 'Safari';
+        if (preg_match('/Chrome/i', $userAgent))       $webExplorer = 'Chrome';
+
+        return $webExplorer;
+    }
+
+    private function getVersion($userAgent, $webExplorer)
+    {
+        $version = null;
+
+        if($webExplorer!=='Opera' && preg_match("#(".strtolower($webExplorer).")[/ ]?([0-9.]*)#", $userAgent, $match))
+            $version = floor($match[2]);
+        if($webExplorer=='Opera' || $webExplorer=='Safari' && preg_match("#(version)[/ ]?([0-9.]*)#", $userAgent, $match))
+            $version = floor($match[2]);
+
+        return $version;
     }
 }
