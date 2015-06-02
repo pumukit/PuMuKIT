2cd7adf41ffae76d34f451981d8196ab48986f36
mrey <mrey@teltek.es>
2015-06-02 10:42:30 +0200

    Renamed ArcaFilter to SchemaFilter

--- src/Pumukit/SchemaBundle/Filter/SchemaFilter.php
+++ src/Pumukit/SchemaBundle/Filter/SchemaFilter.php
@@ -5,7 +5,7 @@ namespace Pumukit\SchemaBundle\Filter;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetaData;
 use Doctrine\ODM\MongoDB\Query\Filter\BsonFilter;
 
-class ArcaFilter extends BsonFilter
+class SchemaFilter extends BsonFilter
 {
 
   public function addFilterCriteria(ClassMetadata $targetDocument)
