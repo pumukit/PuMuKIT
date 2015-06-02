2476bbcd1f85612c039b8e825e8ef1a321174727
mrey <mrey@teltek.es>
2015-06-02 10:46:14 +0200

    SchemaFilter: Changed MultimediaObject status to constant field defined in class

--- src/Pumukit/SchemaBundle/Filter/SchemaFilter.php
+++ src/Pumukit/SchemaBundle/Filter/SchemaFilter.php
@@ -4,6 +4,7 @@ namespace Pumukit\SchemaBundle\Filter;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetaData;
 use Doctrine\ODM\MongoDB\Query\Filter\BsonFilter;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
 
 class SchemaFilter extends BsonFilter
 {
@@ -11,7 +12,7 @@ class SchemaFilter extends BsonFilter
   public function addFilterCriteria(ClassMetadata $targetDocument)
   {
     if ("Pumukit\SchemaBundle\Document\MultimediaObject" === $targetDocument->reflClass->name) {
-      return array("status" => 0, "tags.cod" => $this->getParameter("pub_channel_tag"));
+      return array("status" => MultimediaObject::STATUS_PUBLISHED, "tags.cod" => $this->getParameter("pub_channel_tag"));
     }
   }
 }
