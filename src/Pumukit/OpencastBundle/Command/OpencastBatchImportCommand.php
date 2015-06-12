e67be14f6a4a2127321d3f5384b968e866d06226
Kristof Keppens <kkeppens@gmail.com>
2015-06-12 17:01:40 +0200

    skip recordings that are already in the database

--- src/Pumukit/OpencastBundle/Command/OpencastBatchImportCommand.php
+++ src/Pumukit/OpencastBundle/Command/OpencastBatchImportCommand.php
@@ -33,14 +33,21 @@ class OpencastBatchImportCommand extends ContainerAwareCommand
         while($batchPlace < $totalMediaPackages) {
             $output->writeln("Importing recordings " . $batchPlace . " to " . ($batchPlace + $batchSize));
             $mediaPackages = $opencastClientService->getMediaPackages('', $batchSize, $batchPlace);
+
+            $opencastImportService = $this->getContainer()->get('pumukit_opencast.import');
+            $repositoryMultimediaObjects = $this->getContainer()->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+
             foreach ($mediaPackages[1] as $mediaPackage) {
                 $output->writeln("Importing mediapackage: " . $mediaPackage['id']);
-                $opencastImportService = $this->getContainer()->get('pumukit_opencast.import');
-                $opencastImportService->importRecording($mediaPackage['id']);
+                if($repositoryMultimediaObjects->findOneBy(array("properties.opencast" => $mediaPackage["id"]))) {
+                    $output->writeln("Mediapackage " . $mediaPackage['id'] . " has already been imported, skipping to next mediapackage");
+                } else {
+                    $opencastImportService->importRecording($mediaPackage['id']);
+                }
             }
             $batchPlace = $batchPlace + $batchSize;
         }
         $stopTime = microtime(true);
-        $output->writeln("Finished importing " . $totalMediaPackages . " in " . ($stopTime - $startTime) . " seconds");
+        $output->writeln("Finished importing " . $totalMediaPackages . " recordings in " . ($stopTime - $startTime) . " seconds");
     }
 }
