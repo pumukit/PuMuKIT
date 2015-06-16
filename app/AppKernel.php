41cf9c3fc013061daa7a01e6bae9edbada07d4ee
mrey <mrey@teltek.es>
2015-06-16 13:44:55 +0200

    Disable PodcastBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,7 @@ class AppKernel extends Kernel
             new Pumukit\WebTVBundle\PumukitWebTVBundle(),
             //new Pumukit\Cmar\WebTVBundle\PumukitCmarWebTVBundle(),
             //new Pumukit\Cmar\SonarBundle\PumukitCmarSonarBundle(),
-            new Pumukit\Cmar\PodcastBundle\PumukitCmarPodcastBundle(),
+            //new Pumukit\Cmar\PodcastBundle\PumukitCmarPodcastBundle(),
             //new Pumukit\Cmar\LiveBundle\PumukitCmarLiveBundle(),
             //new Pumukit\NotificationBundle\PumukitNotificationBundle(),
         );
