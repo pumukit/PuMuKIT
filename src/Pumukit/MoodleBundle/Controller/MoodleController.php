e0c9eace0826675fd9e62b25185e2d9125e9514c
rubenrua <rubenrua@teltek.es>
2015-07-24 13:21:38 +0200

    PumukitMoodleBundle scaffolding

---
+++ src/Pumukit/MoodleBundle/Controller/MoodleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Pumukit\MoodleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+/**
+ * @Route("/pumoodle")
+ */
+class MoodleController extends Controller
+{
+  const ROLE_ID = 1;
+  const SECRET = '123_U?R';
+  const PERFIL_ID = 2;
+
+  public $embed_mini = false;
+
+  /**
+   * @Route("/index")
+   */
+  public function indexAction(Request $request)
+  {
+      $ticket  = $request->get('t');
+      $culture = $request->get('lang');
+      
+      $this->setCultureIfPresent($culture);
+      
+      if ($professor = $this->findProfessorEmailTicket($ticket)){
+          $series = $professor->getSeries(self::ROLE_ID);
+          $total_mm = 0;
+          $array_series_mm = array();
+          $out = array();
+          
+          foreach ($series as $s){
+              $serial_title = $s->getTitle();
+              $array_series_mm[$serial_title] = array();
+              $mms = $s->getMmsByPerson($professor->getId(), self::ROLE_ID);
+        
+              foreach ($mms as $mm) {
+
+                $mm_title = $mm->getRecordDate('Y-m-d') . ' ' . $mm->getTitle();
+                if ($mm->getSubtitle() != ''){
+                    $mm_title .= " - " . $mm->getSubtitle();
+                }
+                $array_series_mm[$serial_title][$mm_title] = url_for('pumoodle/embed?m=' . $mm->getId(), true) . '/lang/' . $this->vculture;
+                $total_mm++;
+              }
+          }
+      
+          $out['status']     = "OK";
+          $out['status_txt'] = $total_mm;
+          $out['out']        = $array_series_mm;
+
+          return new JsonResponse($out, 200);          
+      } 
+      
+      $out['status'] = "ERROR";
+      $out['status_txt'] = "Error de autenticación - profesor no encontrado en el servidor de vídeo pumukit";
+      $out['out'] = null;
+
+      return new JsonResponse($out, 404);
+  }
+    
+  /**
+   * @Route("/repository")
+   */
+  public function repositoryAction(Request $request)
+  {
+      
+  }
+
+
+  /**
+   * @Route("/embed")
+   */
+  public function embedAction(Request $request)
+  {
+      
+  }
+
+  private function checkIdTicket($id, $ticket, $mail = false)
+  {
+  }
+
+  private function pumukit_curl_action_parameters($action, array $parameters = null, $absoluteurl = false)
+  {
+  }
+
+  private function findProfessorEmailTicket($ticket)
+  {
+  }
+
+  private function setCultureIfPresent($culture)
+  {
+  }
+
+  private function getProfessors() 
+  {
+    $repo = $this->get('doctrine_mongodb.odm.document_manager')
+      ->getRepository('PumukitSchemaBundle:Series');
+
+  }
+}
