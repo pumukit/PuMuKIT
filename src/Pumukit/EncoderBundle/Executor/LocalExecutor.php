f488862d5844169e9cd8fae9b9dfdf7e078e0cce
rubenrua <rubenrua@teltek.es>
2015-08-04 17:45:34 +0200

    Using temporal directory for cwd of LocalExecutor

--- src/Pumukit/EncoderBundle/Executor/LocalExecutor.php
+++ src/Pumukit/EncoderBundle/Executor/LocalExecutor.php
@@ -2,18 +2,28 @@
 
 namespace Pumukit\EncoderBundle\Executor;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
+
 
 class LocalExecutor
 {
 
   public function execute($command, array $cpu=null)
   {
-      $process = new Process($command);
+      $fs = new Filesystem();
+
+      $tempfile = tempnam(sys_get_temp_dir(), '');
+      if (file_exists($tempfile)) { unlink($tempfile); }
+      $fs->mkdir($tempfile);
+
+      $process = new Process($command, $tempfile);
       $process->setTimeout(null);
       $process->setIdleTimeout(null);
       $process->run();
 
+      $fs->remove($tempfile);
+      
       if (!$process->isSuccessful()) {
           throw new \RuntimeException($process->getErrorOutput());
       }
