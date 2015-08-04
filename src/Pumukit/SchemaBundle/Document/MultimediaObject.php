ac5a2e0ed40ba37ff83a3daf77192a1c5ccfd857
mrey <mrey@teltek.es>
2015-08-04 10:03:48 +0200

    Fixed typo with tag codes

--- src/Pumukit/SchemaBundle/Document/MultimediaObject.php
+++ src/Pumukit/SchemaBundle/Document/MultimediaObject.php
@@ -890,16 +890,16 @@ class MultimediaObject
     }
     
     /**
-     * Contains all tags with code
+     * Contains all tags with codes
      * The original string tag logic used array_intersect and count to check it.
      * This function uses doctrine2 arrayCollection contains function instead.
      *
      * @param  array   $tags
      * @return boolean TRUE if this multimedia_object contained all tags, FALSE otherwise.
      */
-    public function containsAllTagsWithCod(array $tags)
+    public function containsAllTagsWithCodes(array $tagCodes)
     {
-        foreach ($tags as $tag) {
+        foreach ($tagCodes as $tag) {
             if (!($this->containsTagWithCod($tag))) {
                 return false;
             }
@@ -928,16 +928,16 @@ class MultimediaObject
     }
     
     /**
-     * Contains any tags with code
+     * Contains any tags with codes
      * The original string tag logic used array_intersect and count to check it.
      * This function uses doctrine2 arrayCollection contains function instead.
      *
      * @param  array   $tags
      * @return boolean TRUE if this multimedia_object contained any tag of the list, FALSE otherwise.
      */
-    public function containsAnyTagWithCod(array $tags)
+    public function containsAnyTagWithCodes(array $tagCodes)
     {
-        foreach ($tags as $tag) {
+        foreach ($tagCodes as $tag) {
             if ($this->containsTagWithCod($tag)) {
                 return true;
             }
