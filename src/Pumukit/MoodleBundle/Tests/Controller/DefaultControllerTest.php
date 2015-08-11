d16385021fd60519259e77e58482ae409eda89a0
mrey <mrey@teltek.es>
2015-08-11 11:18:28 +0200

    Skip Default Controller test in MoodleBundle

--- src/Pumukit/MoodleBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Pumukit/MoodleBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/hello/Fabien');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        /* $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0); */
     }
 }
