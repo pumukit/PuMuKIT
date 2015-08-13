1fc28fb2043497e4b42bc780acecfda70a1cf9e8
mrey <mrey@teltek.es>
2015-08-13 14:04:12 +0200

    Added repository action to MoodleBundle

--- src/Pumukit/MoodleBundle/Controller/MoodleController.php
+++ src/Pumukit/MoodleBundle/Controller/MoodleController.php
@@ -71,7 +71,59 @@ class MoodleController extends Controller
      */
     public function repositoryAction(Request $request)
     {
-    
+        $email = $request->get('professor_email');
+        $ticket  = $request->get('ticket');
+        $locale = $this->getLocale($request->get('lang'));
+
+        $roleCode = $this->container->getParameter('pumukit_moodle.role');
+        $seriesRepo = $this->get('doctrine_mongodb.odm.document_manager')
+        ->getRepository('PumukitSchemaBundle:Series');
+        $mmobjRepo = $this->get('doctrine_mongodb.odm.document_manager')
+        ->getRepository('PumukitSchemaBundle:MultimediaObject');
+
+        if ($professor = $this->findProfessorEmailTicket($email, $ticket, $roleCode)){
+            $series = $seriesRepo->findByPersonIdAndRoleCod($professor->getId(), $roleCode);
+            $numberMultimediaObjects = 0;
+            $multimediaObjectsArray = array();
+            $out = array();
+            $assetsHelper = $this->container->get('templating.helper.assets');
+            $defaultPicUrl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . $assetsHelper->getUrl('/images/folder.png');
+            foreach ($series as $oneseries){
+                $oneSeriesArray = array();
+                $oneSeriesArray["title"] = $oneseries->getTitle($locale);
+                $oneSeriesArray["url"]   = $this->generateUrl('pumukit_webtv_series_index', array('id' => $oneseries->getId()), true);
+                $oneSeriesArray["pic"]   = $oneseries->getFirstUrlPic($defaultPicUrl);
+                $oneSeriesArray["mms"]   = array();
+                $multimediaObjects = $mmobjRepo->findBySeriesAndPersonIdWithRoleCod($oneseries, $professor->getId(), $roleCode);
+                foreach ($multimediaObjects as $multimediaObject){
+                    $mmArray = array();
+                    $mmArray['title'] = $multimediaObject->getTitle($locale);
+                    $mmArray['date']  = $multimediaObject->getRecordDate()->format('Y-m-d');
+                    $mmArray['url']   = $this->generateUrl('pumukit_webtv_multimediaobject_index', array('id' => $multimediaObject->getId()), true);
+                    $mmArray['pic']   = $multimediaObject->getFirstUrlPic($defaultPicUrl);
+                    $mmArray['embed'] = $this->generateUrl('pumukit_moodle_moodle_embed',
+                                                           array(
+                                                                 'id' => $multimediaObject->getId(),
+                                                                 'lang' => $locale,
+                                                                 'opencast' => ($multimediaObject->getProperty('opencast') ? '1':'0')
+                                                                 ),
+                                                           true);
+                    $oneSeriesArray["mms"][] = $mmArray;
+                    $numberMultimediaObjects++;
+                }
+                $multimediaObjectsArray[] = $oneSeriesArray;
+            }
+            $out['status']     = "OK";
+            $out['status_txt'] = $numberMultimediaObjects;
+            $out['out']        = $multimediaObjectsArray;
+            return new JsonResponse($out, 200);
+        }
+        $out['status']     = "ERROR";
+        // TO DO - añadir host, más estados de error.
+        $out['status_txt'] = "Error de autenticación - profesor no encontrado en el servidor de vídeo pumukit";
+        $out['out']        = null;
+        //echo json_encode ($out);
+        return new JsonResponse($out, 404);
     }
 
 
@@ -147,10 +199,6 @@ class MoodleController extends Controller
         $password = $this->container->getParameter('pumukit_moodle.password');
         $check = md5($password . date("Y-m-d") . $id . $email);
         return ($check === $ticket);
-    }
-
-    private function pumukit_curl_action_parameters($action, array $parameters = null, $absoluteurl = false)
-    {
     }
 
     private function findProfessorEmailTicket($email, $ticket, $roleCode)
