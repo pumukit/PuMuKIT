c35f6c8d21d1879cf968c06c69759a9a79509cf4
mrey <mrey@teltek.es>
2015-08-28 09:59:24 +0200

    Fixed exception of render controller when LDAPBundle is not enabled

--- src/Pumukit/NewAdminBundle/Controller/PersonController.php
+++ src/Pumukit/NewAdminBundle/Controller/PersonController.php
@@ -180,12 +180,14 @@ class PersonController extends AdminController
         $resources = $this->getResources($request, $config, $criteria);
 
         $template = $multimediaObject->isPrototype() ? '_template' : '';
+        $ldapEnabled = $this->container->has('pumukit_ldap.ldap') ? true : false;
 
         return array(
                      'people' => $resources,
                      'mm' => $multimediaObject,
                      'role' => $role,
-                     'template' => $template
+                     'template' => $template,
+                     'ldap_enabled' => $ldapEnabled
                      );
     }
 
