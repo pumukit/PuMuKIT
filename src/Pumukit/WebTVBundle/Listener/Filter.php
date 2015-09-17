e5175c715203df0ab19e259ef6df4421e94f2325
mrey <mrey@teltek.es>
2015-09-17 14:57:08 +0200

    #7863 SchemaBundle: added filter by not private broadcasts in frontend

--- src/Pumukit/WebTVBundle/Listener/Filter.php
+++ src/Pumukit/WebTVBundle/Listener/Filter.php
@@ -5,6 +5,7 @@ namespace Pumukit\WebTVBundle\Listener;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Pumukit\SchemaBundle\Document\Broadcast;
 
 class Filter
 {
@@ -27,6 +28,26 @@ class Filter
       
       $filter = $this->dm->getFilterCollection()->enable("frontend");
       $filter->setParameter("pub_channel_tag", "PUCHWEBTV");
+      $filter->setParameter("private_broadcast", $this->getBroadcastCriteria());
     }
   }
+
+    private function getPrivateBroadcastIds()
+    {
+        $broadcastRepo = $this->dm->getRepository('PumukitSchemaBundle:Broadcast');
+        $privateBroadcastIds = $broadcastRepo->findDistinctIdsByBroadcastTypeId(Broadcast::BROADCAST_TYPE_PRI);
+        if (null != $privateBroadcastIds) {
+            return $privateBroadcastIds->toArray();
+        }
+        return array();
+    }
+
+    private function getBroadcastCriteria()
+    {
+        $privateBroadcastIds = $this->getPrivateBroadcastIds();
+        if (null != $privateBroadcastIds) {
+            return array('$nin' => $privateBroadcastIds);
+        }
+        return array();
+    }
 }
