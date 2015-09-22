1623523bd78ec42cfdd3e7ba6f381f03fbb4370e
mrey <mrey@teltek.es>
2015-09-22 15:39:50 +0200

    #6629 WebTVBundle: Fixed redirect response in checking broadcast

--- src/Pumukit/WebTVBundle/Controller/MultimediaObjectController.php
+++ src/Pumukit/WebTVBundle/Controller/MultimediaObjectController.php
@@ -5,6 +5,7 @@ namespace Pumukit\WebTVBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -212,13 +213,15 @@ class MultimediaObjectController extends Controller
 
     public function testBroadcast(MultimediaObject $multimediaObject, Request $request)
     {
-      $seriesUrl = $this->generateUrl('pumukit_webtv_series_index', array('id' => $multimediaObject->getSeries()->getId()), true);
       if (($broadcast = $multimediaObject->getBroadcast()) && 
           (Broadcast::BROADCAST_TYPE_PUB !== $broadcast->getBroadcastTypeId()) &&
           ((!($broadcastName = $request->headers->get('PHP_AUTH_USER', false))) ||
            ($request->headers->get('PHP_AUTH_PW') !== $broadcast->getPasswd() ) ||
-           ($broadcastName !== $broadcast->getName() )))
-        return new Response($this->redirect($seriesUrl), 401, array('WWW-Authenticate' => 'Basic realm="Resource not public."'));
+           ($broadcastName !== $broadcast->getName() ))) {
+        $seriesUrl = $this->generateUrl('pumukit_webtv_series_index', array('id' => $multimediaObject->getSeries()->getId()), true);
+        $redReq = new RedirectResponse($seriesUrl, 302);
+        return new Response($redReq->getContent(), 401, array('WWW-Authenticate' => 'Basic realm="Resource not public."'));
+      }
       if ($broadcast && (Broadcast::BROADCAST_TYPE_PRI === $broadcast->getBroadcastTypeId()))
         return new Response($this->render("PumukitWebTVBundle:Index:403forbidden.html.twig", array()), 403);
       return true;
