beef51ff4f1bc0cf546d861afe733e131044bbc6
Alfro <arodriguez@teltek.es>
2015-09-25 12:10:22 +0200

    #8088 Implemented changes on responsive webtv.

--- src/Pumukit/Responsive/WebTVBundle/Controller/MediaLibraryController.php
+++ src/Pumukit/Responsive/WebTVBundle/Controller/MediaLibraryController.php
@@ -19,6 +19,10 @@ class MediaLibraryController extends Controller
 
         $sortField = "alphabetically" == $sort ? 'title.' . $request->getLocale() : "public_date";
         $series = $repo->findBy(array(), array($sortField => 1));        
+        $criteria = $request->query->get('search', false) ?
+                    array('title.' . $request->getLocale() => new \MongoRegex(sprintf("/%s/", $request->query->get('search')))):
+                    array();
+        $series = $repo->findBy($criteria, array($sortField => 1));   
 
         $this->get('pumukit_responsive_web_tv.breadcrumbs')->addList("All", "pumukit_responsive_webtv_medialibrary_index");
 
