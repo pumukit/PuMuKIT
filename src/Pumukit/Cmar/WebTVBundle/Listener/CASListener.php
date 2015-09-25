ad57a9102091c644247149fb2a72e090f7e4eadc
mrey <mrey@teltek.es>
2015-09-25 11:08:21 +0200

    #7929 Fixed CAS exception to avoid echo messages and a not exit

--- src/Pumukit/Cmar/WebTVBundle/Listener/CASListener.php
+++ src/Pumukit/Cmar/WebTVBundle/Listener/CASListener.php
@@ -25,7 +25,9 @@ class CASListener
     public function onKernelRequest(GetResponseEvent $event)
     {
         if (($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) && ('test' !== $this->environment)){
+            ob_start();
             try {
+                \CAS_GracefullTerminationException::throwInsteadOfExiting();
                 \phpCAS::client(CAS_VERSION_2_0, $this->casUrl, $this->casPort, $this->casUri, false);
                 //\phpCAS::setDebug('/tmp/cas.log');
                 \phpCAS::setNoCasServerValidation();
@@ -34,6 +36,7 @@ class CASListener
                 \phpCAS::handleLogoutRequests(true, $this->casAllowedIpClients);
             } catch (\Exception $e) {
             }
+            ob_end_clean();
         }
     }
 }
