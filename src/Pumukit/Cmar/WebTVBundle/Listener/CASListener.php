f689670bf8211dae44993cf2c341e60527d463dd
rubenrua <rubenrua@teltek.es>
2015-09-25 14:28:08 +0200

    Revert "#7929 Fix CAS ill-formed ticket exception"
    This reverts commit 9eca05ced36dc7a659298123c9f7106ad94c12cb.

--- src/Pumukit/Cmar/WebTVBundle/Listener/CASListener.php
+++ src/Pumukit/Cmar/WebTVBundle/Listener/CASListener.php
@@ -25,15 +25,12 @@ class CASListener
     public function onKernelRequest(GetResponseEvent $event)
     {
         if (($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) && ('test' !== $this->environment)){
-            try {
-                \phpCAS::client(CAS_VERSION_2_0, $this->casUrl, $this->casPort, $this->casUri, false);
-                //\phpCAS::setDebug('/tmp/cas.log');
-                \phpCAS::setNoCasServerValidation();
-                //\phpCAS::setSingleSignoutCallback(array($this, 'casSingleSignOut'));
-                //\phpCAS::setPostAuthenticateCallback(array($this, 'casPostAuth'));
-                \phpCAS::handleLogoutRequests(true, $this->casAllowedIpClients);
-            } catch (\Exception $e) {
-            }
+            \phpCAS::client(CAS_VERSION_2_0, $this->casUrl, $this->casPort, $this->casUri, false);
+            //\phpCAS::setDebug('/tmp/cas.log');
+            \phpCAS::setNoCasServerValidation();
+            //\phpCAS::setSingleSignoutCallback(array($this, 'casSingleSignOut'));
+            //\phpCAS::setPostAuthenticateCallback(array($this, 'casPostAuth'));
+            \phpCAS::handleLogoutRequests(true, $this->casAllowedIpClients);
         }
     }
 }
