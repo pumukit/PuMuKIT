529610152aac61480dd9405e1ac332e68ccc608a
Alfro <arodriguez@teltek.es>
2015-09-29 17:58:38 +0200

    Executed php-cs-fixer

--- src/Pumukit/Responsive/WebTVBundle/Controller/MediaLibraryController.php
+++ src/Pumukit/Responsive/WebTVBundle/Controller/MediaLibraryController.php
@@ -15,35 +15,32 @@ class MediaLibraryController extends Controller
      */
     public function indexAction($sort, Request $request)
     {
-        $this->get('pumukit_responsive_web_tv.breadcrumbs')->addList('All', 'pumukit_responsive_webtv_medialibrary_index');        
+        $this->get('pumukit_responsive_web_tv.breadcrumbs')->addList('All', 'pumukit_responsive_webtv_medialibrary_index');
         $series_repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Series');
         $tags_repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Tag');
 
-        $array_tags = $this->container->getParameter('responsivewebtv.media_library.filter_tags');        
+        $array_tags = $this->container->getParameter('responsivewebtv.media_library.filter_tags');
         $selectionTags = array();
-        foreach( $array_tags as $cod ) {
-            $selectionTags[] = $tags_repo->findOneBy( array( 'cod' => $cod ) );         
+        foreach ($array_tags as $cod) {
+            $selectionTags[] = $tags_repo->findOneBy(array('cod' => $cod));
         }
 
         $criteria = $request->query->get('search', false) ?
                     array('title.'.$request->getLocale() => new \MongoRegex(sprintf('/%s/i', $request->query->get('search')))) :
                     array();
         $result = array();
-        
-        switch ( $sort )
-        {
+
+        switch ($sort) {
             case 'alphabetically':
             $sortField = 'title.'.$request->getLocale();
-            $series = $series_repo->findBy( $criteria, array($sortField => 1) );
-            foreach ( $series as $serie ) 
-            {
+            $series = $series_repo->findBy($criteria, array($sortField => 1));
+            foreach ($series as $serie) {
                 $num_mm = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject')->countInSeries($serie);
                 if ($num_mm < 1) {
                     continue;
                 }
-                $key = substr( $serie->getTitle(), 0, 1);
-                if( !isset( $result[ $key ] ) )
-                {
+                $key = substr($serie->getTitle(), 0, 1);
+                if (!isset($result[ $key ])) {
                     $result[$key] = array();
                 }
                 $result[$key][] = $serie;
@@ -51,16 +48,14 @@ class MediaLibraryController extends Controller
             break;
             case 'date':
             $sortField = 'public_date';
-            $series = $series_repo->findBy( $criteria, array($sortField => 1) );
-            foreach ( $series as $serie ) 
-            {
+            $series = $series_repo->findBy($criteria, array($sortField => 1));
+            foreach ($series as $serie) {
                 $num_mm = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject')->countInSeries($serie);
                 if ($num_mm < 1) {
                     continue;
                 }
-                $key = $serie->getPublicDate()->format("d/m/Y");
-                if( !isset( $result[ $key ] ) )
-                {
+                $key = $serie->getPublicDate()->format('d/m/Y');
+                if (!isset($result[ $key ])) {
                     $result[ $key ] = array();
                 }
                 $result[ $key ][] = $serie;
@@ -68,35 +63,32 @@ class MediaLibraryController extends Controller
             break;
             case 'tags':
             $p_cod = $request->query->get('p_tag', false);
-            $parentTag = $tags_repo->findOneBy( array('cod' => $p_cod) );
-            if( !isset( $parentTag ) )
-            {
+            $parentTag = $tags_repo->findOneBy(array('cod' => $p_cod));
+            if (!isset($parentTag)) {
                 break;
             }
             $tags = $parentTag->getChildren();
-            foreach( $tags as $tag )
-            {
-                
-                if( $tag->getNumberMultimediaObjects() < 1 ) {
+            foreach ($tags as $tag) {
+                if ($tag->getNumberMultimediaObjects() < 1) {
                     continue;
                 }
                 $key = $tag->getTitle();
-                if( !isset( $result[ $key ] ) )
-                {
+                if (!isset($result[ $key ])) {
                     $result[ $key ] = array();
                 }
-                
+
                 $series = $series_repo->findWithTag($tag, array('public_date' => +1));
-                foreach( $series as $serie ) {
+                foreach ($series as $serie) {
                     $num_mm = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject')->countInSeries($serie);
                     if ($num_mm < 1) {
                         continue;
-                    }                    
+                    }
                     $result[ $key ][] = $serie;
                 }
             }
             break;
         }
+
         return array('objects' => $result, 'sort' => $sort, 'tags' => $selectionTags);
     }
 }
