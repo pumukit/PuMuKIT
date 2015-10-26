b4bbdbf05e362dbcf1e0c8ca692c7f128b3875e5
Alfro <arodriguez@teltek.es>
2015-10-26 17:20:59 +0100

    #8430 WebTVBudle: Rewritten CategoriesController to reduce query usage.

--- src/Pumukit/WebTVBundle/Controller/CategoriesController.php
+++ src/Pumukit/WebTVBundle/Controller/CategoriesController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+//Used on countMmobjsInTags TODO Move to service
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
 class CategoriesController extends Controller
 {
     /**
@@ -16,7 +19,6 @@ class CategoriesController extends Controller
     public function indexAction($sort, Request $request)
     {
         $this->get('pumukit_web_tv.breadcrumbs')->addList('Videos by Category', 'pumukit_webtv_categories_index');
-
         $parentCod = $this->container->getParameter('categories_tag_cod');
 
         $groundsRoot = $this->getDoctrine()
@@ -33,43 +35,125 @@ class CategoriesController extends Controller
         }
 
         $allGrounds = array();
+        $tagsTree = $this->getDoctrine()
+                              ->getRepository('PumukitSchemaBundle:Tag')
+                              ->getTree($groundsRoot);
 
-        // Use getTree to optimize queries. Two level nesting 'getChildren' creates too many queries.
-        // The children/grandchildren tree should be a single query. (getDescendants()?)
-        foreach ( $groundsRoot->getChildren() as $id=>$parent ){
+        //Create array structure
+        //TODO Move this logic to a service.
+        $tagsArray = array();
+        $parentPathLength = strlen($groundsRoot->getPath());
+        foreach($tagsTree as $tag) {
+            $path = sprintf('%s__object', $tag->getPath());
+            $keys = explode('|', $path);
+            $ref =& $tagsArray;
+            foreach( $keys as $key ){
+                if(!array_key_exists($key, $ref)){
+                    $ref[$key] = array();
+                }
+                $ref =& $ref[$key];
+            }
+            $ref = $tag;
+        }
+        //Removes unnecessary parent nodes.
+        $parentKeys = explode('|', substr($groundsRoot->getPath(), 0 ,-1));
+        $ref =& $tagsArray;
+        foreach( $parentKeys as $key ){
+            $ref =& $ref[$key];
+        }
+        $tagsArray = $ref;
+        //End removes unnecessary parent nodes.
+
+        //Count number multimediaObjects
+        //TODO Move this logic into a service.
+        $counterMmobjs = $this->countMmobjInTags();
+        foreach ( $tagsArray as $id=>$parent ){if($id == '__object'){continue;}
             $allGrounds[$id] = array();
-            $allGrounds[$id]['title'] = $parent->getTitle();
-            $allGrounds[$id]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent->getCod()));
-            // This data is not correct. getNumberMultimediaObjects() Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
-            $allGrounds[$id]['num_mmobjs'] = $parent->getNumberMultimediaObjects();
+            $allGrounds[$id]['title'] = $parent['__object']->getTitle();
+            $allGrounds[$id]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent['__object']->getCod()));
+            $numMmobjs = 0;
+            $cod = $parent['__object']->getCod();
+            if(isset($counterMmobjs[$cod])){
+                $numMmobjs = $counterMmobjs[$cod];
+            }
+            $allGrounds[$id]['num_mmobjs'] = $numMmobjs;
             $allGrounds[$id]['children'] = array();
 
             //Add 'General' Tag
-            if( $listGeneralParam || true){
+            if( $listGeneralParam ){
                 $allGrounds[$id]['children']['general'] = array();
-                $allGrounds[$id]['children']['general']['title'] = $this->get('translator')->trans("General %title%", array('%title%' => $parent->getTitle()));
-                $allGrounds[$id]['children']['general']['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent->getCod(), 'list_only_general' => true));
-                //Dummy value
-                $allGrounds[$id]['children']['general']['num_mmobjs'] = 45610913;
+                $allGrounds[$id]['children']['general']['title'] = $this->get('translator')->trans("General %title%", array('%title%' => $parent['__object']->getTitle()));
+                $allGrounds[$id]['children']['general']['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent['__object']->getCod(), 'list_only_general' => true));
+
+                $numMmobjs = 0;
+                if(isset($counterGeneralMmobjs[$cod])){
+                    $numMmobjs = $counterMmobjs[$cod];
+                }
+                $allGrounds[$id]['children']['general']['num_mmobjs'] = $this->countGeneralMmobjsInTag($parent['__object']);
                 $allGrounds[$id]['children']['general']['children'] = array();
             }
-            foreach ($parent->getChildren() as $id2=>$child ) {
+            foreach ($parent as $id2=>$child ) {if($id2 == '__object'){continue;}
                 $allGrounds[$id]['children'][$id2] = array();
-                $allGrounds[$id]['children'][$id2]['title'] = $child->getTitle();
-                $allGrounds[$id]['children'][$id2]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $child->getCod()));
-                // This data is not correct. getNumberMultimediaObjects() Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
-                $allGrounds[$id]['children'][$id2]['num_mmobjs'] = $child->getNumberMultimediaObjects();
+                $allGrounds[$id]['children'][$id2]['title'] = $child['__object']->getTitle();
+                $allGrounds[$id]['children'][$id2]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $child['__object']->getCod()));
+
+                $numMmobjs = 0;
+                $cod = $child['__object']->getCod();
+                if(isset($counterMmobjs[$cod])){
+                    $numMmobjs = $counterMmobjs[$cod];
+                }
+                $allGrounds[$id]['children'][$id2]['num_mmobjs'] = $numMmobjs;
                 $allGrounds[$id]['children'][$id2]['children'] = array();
 
-                foreach($child->getChildren() as $id3=>$grandchild ){
+                foreach($child as $id3=>$grandchild ){if($id3 == '__object'){continue;}
                   $allGrounds[$id]['children'][$id2]['children'][$id3] = array();
-                  $allGrounds[$id]['children'][$id2]['children'][$id3]['title'] = $grandchild->getTitle();
-                  // This data is not correct. getNumberMultimediaObjects () Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
-                  $allGrounds[$id]['children'][$id2]['children'][$id3]['num_mmobjs'] = $grandchild->getNumberMultimediaObjects();
+                  $allGrounds[$id]['children'][$id2]['children'][$id3]['title'] = $grandchild['__object']->getTitle();
+                  $allGrounds[$id]['children'][$id2]['children'][$id3]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $grandchild['__object']->getCod()));
+                  $numMmobjs = 0;
+                  $cod = $grandchild['__object']->getCod();
+                  if(isset($counterMmobjs[$cod])){
+                      $numMmobjs = $counterMmobjs[$cod];
+                  }
+                  $allGrounds[$id]['children'][$id2]['children'][$id3]['num_mmobjs'] = $numMmobjs;
                 }
             }
-            dump($allGrounds[$id]['children']);
         }
         return array('allGrounds' => $allGrounds, 'title' => $groundsRoot->getTitle(), 'list_general_tags' => $listGeneralParam );
+    }
+
+
+    //TODO Move this function into a service.
+    private function countMmobjInTags(){
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+        $multimediaObjectsColl = $dm->getDocumentCollection('PumukitSchemaBundle:MultimediaObject');
+        $criteria = array('status' => MultimediaObject::STATUS_PUBLISHED, 'tags.cod' => 'PUCHWEBTV', 'tags.cod' =>'ITUNESU');
+        $criteria['$or'] = array(
+             array('tracks' => array('$elemMatch' => array('tags' => 'display', 'hide' => false)), 'properties.opencast' => array('$exists' => false)),
+             array('properties.opencast' => array('$exists' => true)),
+        );
+        $pipeline = array(
+            array('$match' => $criteria),
+            array('$unwind' => '$tags' ),
+            array('$group' => array('_id' => '$tags.cod', 'count' => array('$sum' => 1))),
+        );
+
+        $aggregation = $multimediaObjectsColl->aggregate($pipeline);
+        $mmobjCount = array();
+        foreach($aggregation as $a) {
+            $mmobjCount[(string)$a['_id']] = $a['count'];
+        }
+        return $mmobjCount;
+    }
+
+    //TODO Move this function into a service.
+    private function countGeneralMmobjsInTag($tag){
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+        $repo = $dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $qb = $repo->createBuilderWithGeneralTag($tag)
+            ->count()
+            ->getQuery()
+            ->execute();
+
+        return $qb;
     }
 }
