3f7aa20785d90bd7b3c4c651d7473bd9e7cbf42d
Alfro <arodriguez@teltek.es>
2015-10-26 11:23:18 +0100

    #8430 WebTVBundle: Generating categories url on controller.

--- src/Pumukit/WebTVBundle/Controller/CategoriesController.php
+++ src/Pumukit/WebTVBundle/Controller/CategoriesController.php
@@ -26,8 +26,8 @@ class CategoriesController extends Controller
         if(!isset($groundsRoot)){
             throw $this->createNotFoundException('The parent with cod: '.$parentCod.' was not found. Please add it to the Tags database or configure another categories_tag_cod in parameters.yml');
         }
-        
-        $listGeneralParam = null;
+
+        $listGeneralParam = false;
         if($this->container->hasParameter('categories.list_general_tags')) {
             $listGeneralParam = $this->container->getParameter('categories.list_general_tags');
         }
@@ -39,15 +39,24 @@ class CategoriesController extends Controller
         foreach ( $groundsRoot->getChildren() as $id=>$parent ){
             $allGrounds[$id] = array();
             $allGrounds[$id]['title'] = $parent->getTitle();
-            $allGrounds[$id]['cod'] = $parent->getCod();
+            $allGrounds[$id]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent->getCod()));
             // This data is not correct. getNumberMultimediaObjects() Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
             $allGrounds[$id]['num_mmobjs'] = $parent->getNumberMultimediaObjects();
             $allGrounds[$id]['children'] = array();
 
+            //Add 'General' Tag
+            if( $listGeneralParam || true){
+                $allGrounds[$id]['children']['general'] = array();
+                $allGrounds[$id]['children']['general']['title'] = $this->get('translator')->trans("General %title%", array('%title%' => $parent->getTitle()));
+                $allGrounds[$id]['children']['general']['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $parent->getCod(), 'list_only_general' => true));
+                //Dummy value
+                $allGrounds[$id]['children']['general']['num_mmobjs'] = 45610913;
+                $allGrounds[$id]['children']['general']['children'] = array();
+            }
             foreach ($parent->getChildren() as $id2=>$child ) {
                 $allGrounds[$id]['children'][$id2] = array();
                 $allGrounds[$id]['children'][$id2]['title'] = $child->getTitle();
-                $allGrounds[$id]['children'][$id2]['cod'] = $child->getCod();
+                $allGrounds[$id]['children'][$id2]['url'] = $this->generateUrl('pumukit_webtv_bytag_multimediaobjects', array('cod' => $child->getCod()));
                 // This data is not correct. getNumberMultimediaObjects() Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
                 $allGrounds[$id]['children'][$id2]['num_mmobjs'] = $child->getNumberMultimediaObjects();
                 $allGrounds[$id]['children'][$id2]['children'] = array();
@@ -55,11 +64,11 @@ class CategoriesController extends Controller
                 foreach($child->getChildren() as $id3=>$grandchild ){
                   $allGrounds[$id]['children'][$id2]['children'][$id3] = array();
                   $allGrounds[$id]['children'][$id2]['children'][$id3]['title'] = $grandchild->getTitle();
-                  $allGrounds[$id]['children'][$id2]['children'][$id3]['cod'] = $grandchild->getCod();
                   // This data is not correct. getNumberMultimediaObjects () Returns all mmobjs. Should return only published ones. Reimplement using Mongo aggregate.
                   $allGrounds[$id]['children'][$id2]['children'][$id3]['num_mmobjs'] = $grandchild->getNumberMultimediaObjects();
                 }
             }
+            dump($allGrounds[$id]['children']);
         }
         return array('allGrounds' => $allGrounds, 'title' => $groundsRoot->getTitle(), 'list_general_tags' => $listGeneralParam );
     }
