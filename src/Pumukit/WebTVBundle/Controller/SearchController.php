aa6560ddfeb1805d0f1e34484b24246dcdb0e428
Alfro <arodriguez@teltek.es>
2015-11-05 10:05:15 +0100

    #8539 WebTVBundle: Search. Removed form hiddens, using routing instead. Refactored var names and removed unnecessary vars.

--- src/Pumukit/WebTVBundle/Controller/SearchController.php
+++ src/Pumukit/WebTVBundle/Controller/SearchController.php
@@ -28,25 +28,25 @@ class SearchController extends Controller
 
       $this->get('pumukit_web_tv.breadcrumbs')->addList('Series search', 'pumukit_webtv_search_series');
 
-      $search_found = $request->query->get('search');
-      $start_found = $request->query->get('start');
-      $end_found = $request->query->get('end');
+      $searchFound = $request->query->get('search');
+      $startFound = $request->query->get('start');
+      $endFound = $request->query->get('end');
 
       $repository_series = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Series');
 
       $queryBuilder = $repository_series->createQueryBuilder();
 
-      if ($search_found != '') {
-          $queryBuilder->field('$text')->equals(array('$search' => $search_found));
+      if ($searchFound != '') {
+          $queryBuilder->field('$text')->equals(array('$search' => $searchFound));
       }
 
-      if ($start_found != 'All' && $start_found != '') {
-          $start = \DateTime::createFromFormat('d/m/Y', $start_found);
+      if ($startFound != 'All' && $startFound != '') {
+          $start = \DateTime::createFromFormat('d/m/Y', $startFound);
           $queryBuilder->field('public_date')->gt($start);
       }
 
-      if ($end_found != 'All' && $end_found != '') {
-          $end = \DateTime::createFromFormat('d/m/Y', $end_found);
+      if ($endFound != 'All' && $endFound != '') {
+          $end = \DateTime::createFromFormat('d/m/Y', $endFound);
           $queryBuilder->field('public_date')->lt($end);
       }
 
@@ -59,9 +59,11 @@ class SearchController extends Controller
 
   /**
    * @Route("/searchmultimediaobjects")
+   * @Route("/searchmultimediaobjects/{tagCod}")
+   * @Route("/searchmultimediaobjects/{tagCod}/general", defaults={"general" = true})
    * @Template("PumukitWebTVBundle:Search:index.html.twig")
    */
-  public function multimediaObjectsAction(Request $request)
+  public function multimediaObjectsAction($tagCod = null, $general = false, Request $request)
   {
       $numberCols = 2;
       if( $this->container->hasParameter('columns_objs_search')){
@@ -69,28 +71,25 @@ class SearchController extends Controller
       }
       $this->get('pumukit_web_tv.breadcrumbs')->addList('Multimedia object search', 'pumukit_webtv_search_multimediaobjects');
 
-      $tag_search = new Tag();
-
-      $blocked_tag = $request->query->get('blockedTag');
-      $blocked_tag_value = $request->query->get('blockedTagValue');
-      $useBlockedTagAsGeneral = $request->query->get('useBlockedTagAsGeneral');
-      $search_found = $request->query->get('search');
-      $tags_found = $request->query->get('tags');
-      $type_found = $request->query->get('type');
-      $duration_found = $request->query->get('duration');
-      $start_found = $request->query->get('start');
-      $end_found = $request->query->get('end');
-      $language_found = $request->query->get('language');
+      $blockedTagCod = $tagCod;
+      $useBlockedTagAsGeneral = $general;
+      $searchFound = $request->query->get('search');
+      $tagsFound = $request->query->get('tags');
+      $typeFound = $request->query->get('type');
+      $durationFound = $request->query->get('duration');
+      $startFound = $request->query->get('start');
+      $endFound = $request->query->get('end');
+      $languageFound = $request->query->get('language');
 
       $repository_multimediaObjects = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
       $repository_tags = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Tag');
 
       $searchByTagCod = 'ITUNESU';
-      if( $this->container->hasParameter('search.parent_tag.cod')) {
+      if ($this->container->hasParameter('search.parent_tag.cod')) {
           $searchByTagCod = $this->container->getParameter('search.parent_tag.cod');
       }
       $parentTag = $repository_tags->findOneByCod($searchByTagCod);
-      if( !isset($parentTag)) {
+      if (!isset($parentTag)) {
           throw new \Exception(sprintf('The parent Tag with COD:  \' %s  \' does not exist. Check if your tags are initialized and that you added the correct \'cod\' to parameters.yml (search.parent_tag.cod)',$searchByTagCod));
       }
 
@@ -102,66 +101,70 @@ class SearchController extends Controller
               throw new \Exception(sprintf('The parent Tag with COD:  \' %s  \' does not exist. Check if your tags are initialized and that you added the correct \'cod\' to parameters.yml (search.parent_tag.cod)',$searchByTagCod));
           }
       }
-      if( $blocked_tag_value !== null ) {
-          $tags_found[] = $blocked_tag_value;
+      if( $blockedTagCod !== null ) {
+          $tagsFound[] = $blockedTagCod;
       }
-      if($tags_found !== null) {
-          $tags_found = array_values(array_diff($tags_found, array('All')));
+      if($tagsFound !== null) {
+          $tagsFound = array_values(array_diff($tagsFound, array('All','')));
       }
 
       $searchLanguages = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject')->createStandardQueryBuilder()->distinct('tracks.language')->getQuery()->execute();
 
       $queryBuilder = $repository_multimediaObjects->createStandardQueryBuilder();
 
-      if ($search_found != '') {
-          $queryBuilder->field('$text')->equals(array('$search' => $search_found));
+      if ($searchFound != '') {
+          $queryBuilder->field('$text')->equals(array('$search' => $searchFound));
       }
       $blockedTag = null;
-      if($blocked_tag_value) {
-          $blockedTag = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Tag')->findOneByCod($blocked_tag_value);
+      if($blockedTagCod) {
+          $blockedTag = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Tag')->findOneByCod($blockedTagCod);
+          if(!isset($blockedTag)) {
+              throw $this->createNotFoundException(sprintf('The Tag with cod \'%s\ does not exist',$blockedTagCod));
+          }
           $this->get('pumukit_web_tv.breadcrumbs')->addList($blockedTag->getTitle(), 'pumukit_webtv_search_multimediaobjects');
       }
-      if (count($tags_found) > 0) {
-          $queryBuilder->field('tags.cod')->all($tags_found);
-          if($useBlockedTagAsGeneral !== null && $blockedTag !== null ) {
+      if (count($tagsFound) > 0) {
+          $queryBuilder->field('tags.cod')->all($tagsFound);
+
+          if($useBlockedTagAsGeneral && $blockedTag !== null ) {
               $queryBuilder->field('tags.path')->notIn(array(new \MongoRegex('/'.preg_quote($blockedTag->getPath()). '.*\|/')));
           }
       }
 
-      if ($type_found != 'All' && $type_found != '') {
-          $queryBuilder->field('tracks.only_audio')->equals($type_found == 'Audio');
+      if ($typeFound != '') {
+          $queryBuilder->field('tracks.only_audio')->equals($typeFound == 'Audio');
       }
 
-      if ($duration_found != 'All' && $duration_found != '') {
-          if ($duration_found == '-5') {
+      if ($durationFound != '') {
+          if ($durationFound == '-5') {
               $queryBuilder->field('tracks.duration')->lte(300);
           }
-          if ($duration_found == '-10') {
+          if ($durationFound == '-10') {
               $queryBuilder->field('tracks.duration')->lte(600);
           }
-          if ($duration_found == '-30') {
+          if ($durationFound == '-30') {
               $queryBuilder->field('tracks.duration')->lte(1800);
           }
-          if ($duration_found == '-60') {
+          if ($durationFound == '-60') {
               $queryBuilder->field('tracks.duration')->lte(3600);
           }
-          if ($duration_found == '+60') {
+          if ($durationFound == '+60') {
               $queryBuilder->field('tracks.duration')->gt(3600);
           }
       }
 
-      if ($start_found != 'All' && $start_found != '') {
-          $start = \DateTime::createFromFormat('d/m/Y', $start_found);
+      if ($startFound != '') {
+          $start = \DateTime::createFromFormat('d/m/Y', $startFound);
           $queryBuilder->field('record_date')->gt($start);
       }
 
-      if ($end_found != 'All' && $end_found != '') {
-          $end = \DateTime::createFromFormat('d/m/Y', $end_found);
+      if ($endFound != '') {
+          $end = \DateTime::createFromFormat('d/m/Y', $endFound);
           $queryBuilder->field('record_date')->lt($end);
       }
 
-      if($language_found != 'All' && $language_found != '') {
-          $queryBuilder->field('tracks.language')->equals($language_found);
+      if($languageFound != '') {
+          $queryBuilder->field('tracks.language')->equals($languageFound);
       }
 
       $pagerfanta = $this->createPager($queryBuilder, $request->query->get('page', 1));
@@ -170,16 +173,12 @@ class SearchController extends Controller
          'objects' => $pagerfanta,
          'parent_tag' => $parentTag,
          'parent_tag2' => $parentTag2,
-         'tags_found' => $tags_found,
-         'type_found' => $type_found,
-         'duration_found' => $duration_found,
+         'tags_found' => $tagsFound,
+         'type_found' => $typeFound,
          'number_cols' => $numberCols,
          'languages' => $searchLanguages,
-         'language_found' => $language_found,
-         'blocked_tag' => $blocked_tag,
-         'blocked_tag_value' => $blocked_tag_value,
-         'blockedTag' => $blockedTag,
-         'use_blocked_tag_as_general' => $useBlockedTagAsGeneral);
+         'language_found' => $languageFound,
+         'blocked_tag' => $blockedTag);
     }
 
     private function createPager($objects, $page)
