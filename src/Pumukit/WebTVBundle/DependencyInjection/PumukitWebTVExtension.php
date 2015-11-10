05a4f147e3b2aec1a864e861de64d05ecbb93afe
Alfro <arodriguez@teltek.es>
2015-11-10 10:33:05 +0100

    #8559 WebTVBundle: Added parameters.yml with default values

--- src/Pumukit/WebTVBundle/DependencyInjection/PumukitWebTVExtension.php
+++ src/Pumukit/WebTVBundle/DependencyInjection/PumukitWebTVExtension.php
@@ -24,5 +24,7 @@ class PumukitWebTVExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
     }
 }
