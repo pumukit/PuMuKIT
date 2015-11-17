9b5e82780ea138b674e4d7ae666b3d4281b57131
mrey <mrey@teltek.es>
2015-11-17 12:04:42 +0100

    #8678 SchemaBundle: added new events

---
+++ src/Pumukit/SchemaBundle/Event/PersonWithRoleEvent.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+class PersonEvent extends Event
+{
+    /**
+     * @var MultimediaObject
+     */
+    protected $multimediaObject;
+
+    /**
+     * @var Person
+     */
+    protected $person;
+
+    /**
+     * @var Role
+     */
+    protected $role;
+
+    /**
+     * @param MultimediaObject $multimediaObject
+     * @param Person|EmbeddedPerson $person
+     * @param Role|EmbeddedRole $role
+     */
+    public function __construct(MultimediaObject $multimediaObject, $person, $role)
+    {
+        $this->multimediaObject = $multimediaObject;
+        $this->person = $person;
+        $this->role = $role;
+    }
+
+    /**
+     * @return MultimediaObject
+     */
+    public function getMultimediaObject()
+    {
+        return $this->multimediaObject;
+    }
+
+    /**
+     * @return Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+
+    /**
+     * @return Role
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
