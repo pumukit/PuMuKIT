b50ab6bd2b8285e437541d651105bc6dbb4ea946
Alfro <arodriguez@teltek.es>
2015-11-18 13:27:37 +0100

    APIBundle: Added serializer\exclude to prevent chaining results.

--- src/Pumukit/SchemaBundle/Document/Broadcast.php
+++ src/Pumukit/SchemaBundle/Document/Broadcast.php
@@ -4,6 +4,7 @@ namespace Pumukit\SchemaBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Doctrine\Common\Collections\ArrayCollection;
+use JMS\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -28,6 +29,7 @@ class Broadcast
    * @var ArrayCollection $multimedia_objects
    *
    * @MongoDB\ReferenceMany(targetDocument="MultimediaObject", mappedBy="broadcast", simple=true, orphanRemoval=false)
+   * @Serializer\Exclude
    */
   private $multimedia_objects;
 
