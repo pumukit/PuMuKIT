0195237c5155f3acf6ae352b5dc10fe4105bf142
Alfro <arodriguez@teltek.es>
2015-12-03 12:17:10 +0100

    #8877 StatsBundle/Controller: Added mmobj/most_viewed API.

---
+++ src/Pumukit/StatsBundle/Controller/APIController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Pumukit\StatsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/api/media")
+ */
+class APIController extends Controller
+{
+
+    /**
+     * @Route("/mmobj/most_viewed.{_format}", defaults={"_format"="json"}, requirements={"_format": "json|xml"})
+     */
+    public function mmobjMostViewedAction(Request $request)
+    {
+        $viewsRepo = $this->get('doctrine_mongodb')->getRepository('PumukitStatsBundle:ViewsLog');
+        $serializer = $this->get('serializer');
+        $viewsService = $this->get('pumukit_stats.stats');
+
+        $limit = intval($request->get('limit'));
+        if(!$limit || $limit > 100 ) {
+            $limit = 100;
+        }
+
+        $criteria = $request->get('criteria')?:array();
+        $sort = intval($request->get('sort'));
+        if(!in_array($sort, array(1,-1))) {
+            $sort = -1;
+        }
+        
+        $fromDate = $request->get('from_date');
+        $toDate = $request->get('to_date');
+        if(!strpos($fromDate,'T')) {
+            $fromDate .= "T00:00:00";
+        }
+        if(!strpos($toDate,'T')) {
+            $toDate .= "T23:59:59";
+        }
+        $fromDate = \DateTime::createFromFormat('Y-m-d\TH:i:s', $fromDate);
+        $toDate = \DateTime::createFromFormat('Y-m-d\TH:i:s', $toDate);
+
+        if(!$fromDate) {
+            $fromDate = null;
+        }
+        if(!$toDate) {
+            $toDate = null;
+        }
+
+        $mmobjs = $viewsService->getMmobjsMostViewedByRange($fromDate, $toDate, $limit, $criteria, $sort);
+
+        $views = array(
+            'limit' => $limit,
+            'criteria' => $criteria, 
+            'sort'  => $sort,
+            'fromDate' => $fromDate,
+            'toDate' => $toDate,
+            'mmobjs' => $mmobjs
+        );
+
+        $data = $serializer->serialize($views, $request->getRequestFormat());
+        return new Response($data);
+    }
+
+    /**
+     * @Route("/series/most_viewed.{_format}", defaults={"_format"="json"}, requirements={"_format": "json|xml"})
+     */
+    public function seriesMostViewedAction(Request $request)
+    {
+        $viewsRepo = $this->get('doctrine_mongodb')->getRepository('PumukitStatsBundle:ViewsLog');
+        $serializer = $this->get('serializer');
+        $viewsService = $this->get('pumukit_stats.stats');
+
+        $limit = intval($request->get('limit'));
+        if(!$limit || $limit > 100 ) {
+            $limit = 100;
+        }
+
+        $criteria = $request->get('criteria')?:array();
+        $sort = intval($request->get('sort'));
+        if(!in_array($sort, array(1,-1))) {
+            $sort = -1;
+        }
+        
+        $fromDate = $request->get('from_date');
+        $toDate = $request->get('to_date');
+        if(!strpos($fromDate,'T')) {
+            $fromDate .= "T00:00:00";
+        }
+        if(!strpos($toDate,'T')) {
+            $toDate .= "T23:59:59";
+        }
+        $fromDate = \DateTime::createFromFormat('Y-m-d\TH:i:s', $fromDate);
+        $toDate = \DateTime::createFromFormat('Y-m-d\TH:i:s', $toDate);
+
+        if(!$fromDate) {
+            $fromDate = null;
+        }
+        if(!$toDate) {
+            $toDate = null;
+        }
+
+        $mmobjs = $viewsService->getMmobjsMostViewedByRange($fromDate, $toDate, $limit, $criteria, $sort);
+
+        $views = array(
+            'limit' => $limit,
+            'criteria' => $criteria, 
+            'sort'  => $sort,
+            'fromDate' => $fromDate,
+            'toDate' => $toDate,
+            'mmobjs' => $mmobjs
+        );
+
+        $data = $serializer->serialize($views, $request->getRequestFormat());
+        return new Response($data);
+    }
+
+}
