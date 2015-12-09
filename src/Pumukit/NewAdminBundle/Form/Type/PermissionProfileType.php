b73e9267e9e2309aac5e90d78100fd3018650774
mrey <mrey@teltek.es>
2015-12-09 12:10:42 +0100

    #8866 NewAdminBundle: reduced PermissionProfile form type to only have name

--- src/Pumukit/NewAdminBundle/Form/Type/PermissionProfileType.php
+++ src/Pumukit/NewAdminBundle/Form/Type/PermissionProfileType.php
@@ -22,16 +22,8 @@ class PermissionProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('default', 'checkbox',
-                  array('required' => false,
-                        'label' => $this->translator->trans('Default', array(), null, $this->locale)))
             ->add('name', 'text',
-                  array('label' => $this->translator->trans('Name', array(), null, $this->locale)))
-            ->add('scope', 'choice',
-                  array('choices' => PermissionProfile::$scopeDescription,
-                        'required' => false,
-                        'label' => $this->translator->trans('Scope', array(), null, $this->locale)))
-;
+                  array('label' => $this->translator->trans('Name', array(), null, $this->locale)));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
