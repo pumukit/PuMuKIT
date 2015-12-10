2a701eac0d895e96e632d72eea529956d95e3139
mrey <mrey@teltek.es>
2015-12-10 18:08:12 +0100

    #8868 SchemaBundle: added global scope to super admin role hierarchy

--- src/Pumukit/SchemaBundle/Security/RoleHierarchy.php
+++ src/Pumukit/SchemaBundle/Security/RoleHierarchy.php
@@ -3,12 +3,14 @@
 namespace Pumukit\SchemaBundle\Security;
 
 use Symfony\Component\Security\Core\Role\RoleHierarchy as SymfonyRoleHierarchy;
+use Pumukit\SchemaBundle\Document\PermissionProfile;
 
 class RoleHierarchy extends SymfonyRoleHierarchy
 {
     public function __construct(array $hierarchy)
     {
         if (isset($hierarchy['ROLE_SUPER_ADMIN'])) {
+            $hierarchy['ROLE_SUPER_ADMIN'][] = PermissionProfile::SCOPE_GLOBAL;
             foreach (Permission::$permissionDescription as $permission => $description) {
                 $hierarchy['ROLE_SUPER_ADMIN'][] = $permission;
             }
