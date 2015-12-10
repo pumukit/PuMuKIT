cedf83696b5b12bb3b86afcf572e71c3f5a32da2
mrey <mrey@teltek.es>
2015-12-10 19:29:33 +0100

    #8868 NewAdminBundle: changed permissions in menu builder

--- src/Pumukit/NewAdminBundle/Menu/Builder.php
+++ src/Pumukit/NewAdminBundle/Menu/Builder.php
@@ -4,6 +4,7 @@ namespace Pumukit\NewAdminBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Pumukit\SchemaBundle\Security\Permission;
 
 class Builder extends ContainerAware
 {
@@ -14,36 +15,60 @@ class Builder extends ContainerAware
 
         // Add translations in src/Pumukit/NewAdminBundle/Resource/translations/NewAdminBundle.locale.yml
         $authorizationChecker = $this->container->get('security.authorization_checker');
-        if ($authorizationChecker->isGranted('ROLE_ADMIN')) {
+        if (false !== $authorizationChecker->isGranted(Permission::ACCESS_DASHBOARD)) {
             $menu->addChild('Dashboard', array('route' => 'pumukit_newadmin_dashboard_index'))->setExtra('translation_domain', 'NewAdminBundle');
         }
 
-        $series = $menu->addChild('Multimedia Series', array('route' => 'pumukitnewadmin_series_index'))->setExtra('translation_domain', 'NewAdminBundle');
-        $series->addChild('Multimedia', array('route' => 'pumukitnewadmin_mms_index'))->setExtra('translation_domain', 'NewAdminBundle');
-        $series->setDisplayChildren(false);
+        if ($authorizationChecker->isGranted(Permission::ACCESS_MULTIMEDIA_SERIES)) {
+            $series = $menu->addChild('Multimedia Series', array('route' => 'pumukitnewadmin_series_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            $series->addChild('Multimedia', array('route' => 'pumukitnewadmin_mms_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            $series->setDisplayChildren(false);
+        }
 
-        if ($authorizationChecker->isGranted('ROLE_ADMIN')) {
+        if ($authorizationChecker->isGranted(Permission::ACCESS_LIVE_EVENTS) || $authorizationChecker->isGranted(Permission::ACCESS_LIVE_CHANNELS)) {
             $live = $menu->addChild('Live')->setExtra('translation_domain', 'NewAdminBundle');
-            $live->addChild('Live Channels', array('route' => 'pumukitnewadmin_live_index'))->setExtra('translation_domain', 'NewAdminBundle');
-            $live->addChild('Live Events', array('route' => 'pumukitnewadmin_event_index'))->setExtra('translation_domain', 'NewAdminBundle');
-
+            if ($authorizationChecker->isGranted(Permission::ACCESS_LIVE_CHANNELS)) {
+                $live->addChild('Live Channels', array('route' => 'pumukitnewadmin_live_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_LIVE_EVENTS)) {
+                $live->addChild('Live Events', array('route' => 'pumukitnewadmin_event_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+        }
+        if ($authorizationChecker->isGranted(Permission::ACCESS_JOBS)) {
             $menu->addChild('Encoder jobs', array('route' => 'pumukit_encoder_info'))->setExtra('translation_domain', 'NewAdminBundle');        
-
+        }
+        if ($authorizationChecker->isGranted(Permission::ACCESS_PEOPLE) || $authorizationChecker->isGranted(Permission::ACCESS_TAGS) ||
+            $authorizationChecker->isGranted(Permission::ACCESS_BROADCASTS) || $authorizationChecker->isGranted(Permission::ACCESS_SERIES_TYPES)) {
             $tables = $menu->addChild('Tables')->setExtra('translation_domain', 'NewAdminBundle');
-            $tables->addChild('People', array('route' => 'pumukitnewadmin_person_index'))->setExtra('translation_domain', 'NewAdminBundle');
-            $tables->addChild('Tags', array('route' => 'pumukitnewadmin_tag_index'))->setExtra('translation_domain', 'NewAdminBundle');
-            $tables->addChild('Access profiles', array('route' => 'pumukitnewadmin_broadcast_index'))->setExtra('translation_domain', 'NewAdminBundle');
-            $tables->addChild('Series types', array('route' => 'pumukitnewadmin_seriestype_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            if ($authorizationChecker->isGranted(Permission::ACCESS_PEOPLE)) {
+                $tables->addChild('People', array('route' => 'pumukitnewadmin_person_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_TAGS)) {
+                $tables->addChild('Tags', array('route' => 'pumukitnewadmin_tag_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_BROADCASTS)) {
+                $tables->addChild('Access profiles', array('route' => 'pumukitnewadmin_broadcast_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_SERIES_TYPES)) {
+                $tables->addChild('Series types', array('route' => 'pumukitnewadmin_seriestype_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
         }
 
-        if ($authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
-          $management = $menu->addChild('Management')->setExtra('translation_domain', 'NewAdminBundle');
-          $management->addChild('Admin users', array('route' => 'pumukitnewadmin_user_index'))->setExtra('translation_domain', 'NewAdminBundle');
-          $management->addChild('Permission profiles', array('route' => 'pumukitnewadmin_permissionprofile_index'))->setExtra('translation_domain', 'NewAdminBundle');
-          $management->addChild('Roles', array('route' => 'pumukitnewadmin_role_index'))->setExtra('translation_domain', 'NewAdminBundle');
+        if ($authorizationChecker->isGranted(Permission::ACCESS_ADMIN_USERS) || $authorizationChecker->isGranted(Permission::ACCESS_PERMISSION_PROFILES) ||
+            $authorizationChecker->isGranted(Permission::ACCESS_ROLES)) {
+            $management = $menu->addChild('Management')->setExtra('translation_domain', 'NewAdminBundle');
+            if ($authorizationChecker->isGranted(Permission::ACCESS_ADMIN_USERS)) {
+                $management->addChild('Admin users', array('route' => 'pumukitnewadmin_user_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_PERMISSION_PROFILES)) {
+                $management->addChild('Permission profiles', array('route' => 'pumukitnewadmin_permissionprofile_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
+            if ($authorizationChecker->isGranted(Permission::ACCESS_ROLES)) {
+                $management->addChild('Roles', array('route' => 'pumukitnewadmin_role_index'))->setExtra('translation_domain', 'NewAdminBundle');
+            }
         }
 
-        if ($authorizationChecker->isGranted('ROLE_ADMIN')) {
+        if ($authorizationChecker->isGranted(Permission::ACCESS_INGESTOR)) {
             if ($this->container->has("pumukit_opencast.client")) {
                 $ingester = $menu->addChild('Ingester')->setExtra('translation_domain', 'NewAdminBundle');
                 $ingester->addChild('Opencast Ingester', array('route' => 'pumukitopencast'))->setExtra('translation_domain', 'NewAdminBundle');
