bf936ccd14ebf2144f8275e8455b195cd8ac9e41
mrey <mrey@teltek.es>
2015-12-18 11:48:58 +0100

    #8965 OpencastBundle: added invert option to OpencastBatchImportCommand

--- src/Pumukit/OpencastBundle/Command/OpencastBatchImportCommand.php
+++ src/Pumukit/OpencastBundle/Command/OpencastBatchImportCommand.php
@@ -15,7 +15,9 @@ class OpencastBatchImportCommand extends ContainerAwareCommand
     {
         $this
             ->setName('pumukit:opencast:batchimport')
-            ->setDescription('Import the complete opencast repository');
+            ->setDescription('Import the complete opencast repository')
+            ->addOption('invert', 'i', InputOption::VALUE_OPTIONAL, 'Inverted recording (CAMERA <-> SCREEN)')
+          ;
     }
 
     protected function execute( InputInterface $input, OutputInterface $output)
@@ -23,6 +25,13 @@ class OpencastBatchImportCommand extends ContainerAwareCommand
         $startTime = microtime(true);
         $opencastClientService = $this->getContainer()->get('pumukit_opencast.client');
         $mediaPackages = $opencastClientService->getMediaPackages('', 1, 0);
+
+        $invert = $input->getOption('invert');
+        if (('0' === $invert) || ('1' === $invert)) {
+            $invert = ('1' === $invert);
+        } else {
+            $invert = $this->getContainer()->getParameter('pumukit_opencast.batchimport_inverted');
+        }
 
         $totalMediaPackages = $mediaPackages[0];
         $batchSize = 200;
@@ -42,7 +51,7 @@ class OpencastBatchImportCommand extends ContainerAwareCommand
                 if($repositoryMultimediaObjects->findOneBy(array("properties.opencast" => $mediaPackage["id"]))) {
                     $output->writeln("Mediapackage " . $mediaPackage['id'] . " has already been imported, skipping to next mediapackage");
                 } else {
-                    $opencastImportService->importRecording($mediaPackage['id']);
+                    $opencastImportService->importRecording($mediaPackage['id'], $invert);
                 }
             }
             $batchPlace = $batchPlace + $batchSize;
