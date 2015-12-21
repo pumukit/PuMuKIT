685dbfe527ac87a228017fcbcadc507c13b6c925
mrey <mrey@teltek.es>
2015-12-21 18:59:08 +0100

    #8967 OpencastBundle: renamed variables in ClientService

--- src/Pumukit/OpencastBundle/Services/ClientService.php
+++ src/Pumukit/OpencastBundle/Services/ClientService.php
@@ -28,50 +28,16 @@ class ClientService
         return ('/' === $this->player[0]) ? $this->url . $this->player : $this->player;
     }
 
-
-    private function request ($path, $useAdminUrl=false)
+    public function getMediaPackages($query, $limit, $offset)
     {
-        $sal = array();
-
-        if ($useAdminUrl && $this->adminUrl) {
-            $ch = curl_init($this->adminUrl . $path);
-        } else {
-            $ch = curl_init($this->url . $path);
-        }
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-
-        if ($this->user != "") {
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
-            curl_setopt($ch, CURLOPT_USERPWD, $this->user . ':' . $this->passwd);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Requested-Auth: Digest",
-                                                       "X-Opencast-Matterhorn-Authorization: true"));
-        }
-
-        $sal["var"] = curl_exec($ch);
-        $sal["error"] = curl_error($ch);
-        $sal["status"] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        curl_close($ch);
-
-        if (200 != $sal["status"]) {
-            throw new \Exception("Error Processing Request", 1);    
-        }
-
-        return $sal;
-    }
+        $output = $this->request("/search/episode.json?q=" . $query . "&limit=" . $limit . "&offset=" . $offset);
 
 
-    public function getMediaPackages($q, $limit, $offset)
-    {
-        $sal = $this->request("/search/episode.json?q=" . $q . "&limit=" . $limit . "&offset=" . $offset);
-
-
-        if ($sal["status"] !== 200) return false;
-        $decode = json_decode($sal["var"], true);
+        if ($output["status"] !== 200) return false;
+        $decode = json_decode($output["var"], true);
 
         if (!($decode)) {
-            throw new \Exception("Matterhorn communication error");
+            throw new \Exception("Opencast Matterhorn communication error");
         }
 
         $return = array(0, array());
@@ -89,16 +55,15 @@ class ClientService
         return $return;
     }
 
-
     public function getMediapackage($id)
     {
-        $sal = $this->request("/search/episode.json?id=" . $id);
+        $output = $this->request("/search/episode.json?id=" . $id);
 
-        if ($sal["status"] !== 200) return false;
-        $decode = json_decode($sal["var"], true);
+        if ($output["status"] !== 200) return false;
+        $decode = json_decode($output["var"], true);
 
         if (!($decode)) {
-            throw new \Exception("Matterhorn communication error");
+            throw new \Exception("Opencast Matterhorn communication error");
         }
 
         if ($decode["search-results"]["total"] == 0)
@@ -112,13 +77,13 @@ class ClientService
     public function getMediapackageFromArchive($id)
     {
         $this->adminUrl = $this->getAdminUrl();
-        $sal = $this->request("/episode/episode.json?id=" . $id, true);
+        $output = $this->request("/episode/episode.json?id=" . $id, true);
 
-        if ($sal["status"] !== 200) return false;
-        $decode = json_decode($sal["var"], true);
+        if ($output["status"] !== 200) return false;
+        $decode = json_decode($output["var"], true);
 
         if (!($decode)) {
-            throw new \Exception("Matterhorn communication error");
+            throw new \Exception("Opencast Matterhorn communication error");
         }
 
         if ($decode["search-results"]["total"] == 0)
@@ -129,13 +94,45 @@ class ClientService
             return $decode["search-results"]["result"]["mediapackage"];
     }
 
+    private function request ($path, $useAdminUrl=false)
+    {
+        $output = array();
+
+        if ($useAdminUrl && $this->adminUrl) {
+            $request = curl_init($this->adminUrl . $path);
+        } else {
+            $request = curl_init($this->url . $path);
+        }
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_FOLLOWLOCATION, false);
+
+        if ($this->user != "") {
+            curl_setopt($request, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+            curl_setopt($request, CURLOPT_USERPWD, $this->user . ':' . $this->passwd);
+            curl_setopt($request, CURLOPT_HTTPHEADER, array("X-Requested-Auth: Digest",
+                                                       "X-Opencast-Matterhorn-Authorization: true"));
+        }
+
+        $output["var"] = curl_exec($request);
+        $output["error"] = curl_error($request);
+        $output["status"] = curl_getinfo($request, CURLINFO_HTTP_CODE);
+
+        curl_close($request);
+
+        if (200 != $output["status"]) {
+            throw new \Exception("Error Processing Request", 1);
+        }
+
+        return $output;
+    }
+
     private function getAdminUrl()
     {
         $output = $this->request('/services/available.json?serviceType=org.opencastproject.episode');
         if ($output["status"] !== 200) return false;
         $decode = json_decode($output["var"], true);
         if (!($decode)) {
-            throw new \Exception("Matterhorn communication error");
+            throw new \Exception("Opencast Matterhorn communication error");
         }
         if (isset($decode['services'])) {
             if (isset($decode['services']['service'])) {
