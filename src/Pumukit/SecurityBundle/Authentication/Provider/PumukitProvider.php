9e37e3525922768e5e2330cc7d9385b45d27b0b3
rubenrua <rubenrua@teltek.es>
2016-01-18 17:29:17 +0100

    Add PumukitProvider::createUser

--- src/Pumukit/SecurityBundle/Authentication/Provider/PumukitProvider.php
+++ src/Pumukit/SecurityBundle/Authentication/Provider/PumukitProvider.php
@@ -2,6 +2,7 @@
 
 namespace Pumukit\SecurityBundle\Authentication\Provider;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
@@ -12,18 +13,24 @@ use Symfony\Component\Security\Core\Exception\NonceExpiredException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\Security\Core\Util\StringUtils;
+use Pumukit\SchemaBundle\Services\UserService;
+use Pumukit\SchemaBundle\Document\User;
 
 class PumukitProvider implements AuthenticationProviderInterface
 {
   private $userProvider;
   private $providerKey;
   private $userChecker;
+  private $container;
 
-  public function __construct(UserProviderInterface $userProvider, $providerKey, UserCheckerInterface $userChecker)
+
+  public function __construct(UserProviderInterface $userProvider, $providerKey, UserCheckerInterface $userChecker, ContainerInterface $container)
   {
     $this->userProvider = $userProvider;
     $this->providerKey = $providerKey;
     $this->userChecker = $userChecker;
+    //NOTE: using container instead of tag service to avoid ServiceCircularReferenceException.
+    $this->container = $container;
   }
 
   public function authenticate(TokenInterface $token)
@@ -39,6 +46,7 @@ class PumukitProvider implements AuthenticationProviderInterface
     try {
         $user = $this->userProvider->loadUserByUsername($user);
     } catch(UsernameNotFoundException $notFound){
+      $this->createUser($user);
       //TODO add use in ddbb.
         throw $notFound;
     } catch (\Exception $repositoryProblem) {
@@ -56,6 +64,22 @@ class PumukitProvider implements AuthenticationProviderInterface
 
     return $authenticatedToken;
   }
+
+
+  private function createUser($userName)
+  {
+    $userService = $this->container->get('pumukitschema.user');
+    if($userService) {
+      //TODO create createDefaultUser in UserService.
+      //$this->userService->createDefaultUser($user);
+      $user = new User();
+      $user->setUsername($userName);
+      $userService->create($user);
+    } else {
+      throw new AuthenticationServiceException("Not UserService to create a new user");
+    }
+  }
+
 
   public function supports(TokenInterface $token)
   {
