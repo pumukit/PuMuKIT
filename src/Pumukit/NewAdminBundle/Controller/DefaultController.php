ca91fc3d0d9c46cdc8f26a99a5b3d4c955a12e90
rubenrua <rubenrua@teltek.es>
2016-02-18 18:07:46 +0100

    Delete unused code

--- src/Pumukit/NewAdminBundle/Controller/DefaultController.php
+++ src/Pumukit/NewAdminBundle/Controller/DefaultController.php
@@ -4,14 +4,12 @@ namespace Pumukit\NewAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="pumukit_newadmin_index")
      * @Route("/default")
-     * @Template()
      */
     public function indexAction()
     {
