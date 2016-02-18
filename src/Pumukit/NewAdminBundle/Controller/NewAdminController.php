bb296590090cf5ec3a51656e18df170c2abf31e3
Alfro <arodriguez@teltek.es>
2016-02-18 18:52:54 +0100

    #9554 Added comments to interfaces
    A slight commenting to explain what these interfaces are for.

--- src/Pumukit/NewAdminBundle/Controller/NewAdminController.php
+++ src/Pumukit/NewAdminBundle/Controller/NewAdminController.php
@@ -3,5 +3,8 @@ namespace Pumukit\NewAdminBundle\Controller;
 
 interface NewAdminController
 {
-    //..
+    /** 
+     * Interface that can be added to any controller.
+     * When implemented, said controller will use the FilterListener from the NewAdminBundle.
+     */
 }
--- src/Pumukit/WebTVBundle/Controller/WebTVController.php
+++ src/Pumukit/WebTVBundle/Controller/WebTVController.php
@@ -3,5 +3,8 @@ namespace Pumukit\WebTVBundle\Controller;
 
 interface WebTVController
 {
-    // ...
+    /** 
+     * Interface that can be added to any controller.
+     * When implemented, said controller will use the FilterListener from the WebTVBundle.
+     */
 }
