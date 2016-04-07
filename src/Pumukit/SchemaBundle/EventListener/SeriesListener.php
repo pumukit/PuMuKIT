924a48063e6c460d2d35fa305f4b7d7ac3e198dd
mrey <mrey@teltek.es>
2016-04-07 11:50:30 +0200

    #10182 SchemaBundle: added SeriesListener

---
+++ src/Pumukit/SchemaBundle/EventListener/SeriesListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pumukit\SchemaBundle\EventListener;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Pumukit\SchemaBundle\Event\SeriesEvent;
+use Pumukit\SchemaBundle\Services\MultimediaObjectEventDispatcherService;
+
+class SeriesListener
+{
+    private $dm;
+    private $mmRepo;
+    private $mmDispatcher;
+
+    public function __construct(DocumentManager $dm, MultimediaObjectEventDispatcherService $mmDispatcher)
+    {
+        $this->dm = $dm;
+        $this->mmRepo = $dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $this->mmDispatcher = $mmDispatcher;
+    }
+
+    public function postUpdate(SeriesEvent $event)
+    {
+        $series = $event->getSeries();
+        $multimediaObjects = $this->mmRepo->findBySeries($series);
+        foreach ($multimediaObjects as $multimediaObject) {
+            $updateSeries = $multimediaObject->getSeries();
+            $multimediaObject->setSeries($updateSeries);
+            $this->dm->persist($multimediaObject);
+            $this->mmDispatcher->dispatchUpdate($multimediaObject);
+        }
+        $this->dm->flush();
+    }
+}
