bc2ac5d0c586578b98b2e261bb2cfac3cb6bdb1e
rubenrua <rubenrua@teltek.es>
2016-04-15 10:04:24 +0200

    Fix typo 3

--- Resources/public/js/controller.js
+++ Resources/public/js/controller.js
@@ -899,7 +899,7 @@ angular.module('app').controller("PMKController", function ($http, $q, $filter, 
         pmk.his[tab][scope].api.update();
         pmk.loading.his[tab][scope] = false;
 
-        var aux_total_views;
+        var aux_total_views = 0;
         angular.forEach(data.values, function(val){
             aux_total_views += val[1];
         })
