19b2dae2dc2ef274424219a269bdc8eaa85c6a25
mrey <mrey@teltek.es>
2016-04-28 19:17:03 +0200

    #10451 SchemaBundle: added groups to user

--- src/Pumukit/SchemaBundle/Tests/Repository/UserRepositoryTest.php
+++ src/Pumukit/SchemaBundle/Tests/Repository/UserRepositoryTest.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Pumukit\SchemaBundle\Document\Person;
 use Pumukit\SchemaBundle\Document\PermissionProfile;
 use Pumukit\SchemaBundle\Document\User;
+use Pumukit\SchemaBundle\Document\Group;
 
 class UserRepositoryTest extends WebTestCase
 {
     private $dm;
     private $repo;
+    private $groupRepo;
     private $factoryService;
 
     public function setUp()
@@ -20,6 +22,7 @@ class UserRepositoryTest extends WebTestCase
 
         $this->dm = static::$kernel->getContainer()->get('doctrine_mongodb')->getManager();
         $this->repo = $this->dm->getRepository('PumukitSchemaBundle:User');
+        $this->groupRepo = $this->dm->getRepository('PumukitSchemaBundle:Group');
         $this->factoryService = static::$kernel->getContainer()->get('pumukitschema.factory');
 
         //DELETE DATABASE
@@ -28,6 +31,8 @@ class UserRepositoryTest extends WebTestCase
         $this->dm->getDocumentCollection('PumukitSchemaBundle:PermissionProfile')
             ->remove(array());
         $this->dm->getDocumentCollection('PumukitSchemaBundle:User')
+            ->remove(array());
+        $this->dm->getDocumentCollection('PumukitSchemaBundle:Group')
             ->remove(array());
         $this->dm->flush();
     }
@@ -89,5 +94,104 @@ class UserRepositoryTest extends WebTestCase
         $user = $this->repo->find($user->getId());
 
         $this->assertEquals($permissionProfile, $user->getPermissionProfile());
+    }
+
+    public function testUserGroups()
+    {
+        $this->assertEquals(0, count($this->groupRepo->findAll()));
+
+        $key1 = 'Group1';
+        $name1 = 'Group 1';
+        $group1 = $this->createGroup($key1, $name1);
+
+        $this->assertEquals(1, count($this->groupRepo->findAll()));
+
+        $key2 = 'Group2';
+        $name2 = 'Group 2';
+        $group2 = $this->createGroup($key2, $name2);
+
+        $this->assertEquals(2, count($this->groupRepo->findAll()));
+
+        $user = new User();
+        $user->setEmail('testgroup@mail.com');
+        $user->setUsername('testgroup');
+        $user->addAdminGroup($group1);
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $this->assertTrue($user->containsAdminGroup($group1));
+        $this->assertFalse($user->containsAdminGroup($group2));
+        $this->assertFalse($user->containsMemberGroup($group1));
+        $this->assertFalse($user->containsMemberGroup($group2));
+        $this->assertEquals(1, $user->getAdminGroups()->count());
+        $this->assertEquals(0, $user->getMemberGroups()->count());
+
+        $user->addAdminGroup($group2);
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $this->assertTrue($user->containsAdminGroup($group1));
+        $this->assertTrue($user->containsAdminGroup($group2));
+        $this->assertFalse($user->containsMemberGroup($group1));
+        $this->assertFalse($user->containsMemberGroup($group2));
+        $this->assertEquals(2, $user->getAdminGroups()->count());
+        $this->assertEquals(0, $user->getMemberGroups()->count());
+
+        $user->removeAdminGroup($group1);
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $this->assertFalse($user->containsAdminGroup($group1));
+        $this->assertTrue($user->containsAdminGroup($group2));
+        $this->assertFalse($user->containsMemberGroup($group1));
+        $this->assertFalse($user->containsMemberGroup($group2));
+        $this->assertEquals(1, $user->getAdminGroups()->count());
+        $this->assertEquals(0, $user->getMemberGroups()->count());
+
+        $this->assertEquals(2, count($this->groupRepo->findAll()));
+
+        $user->addMemberGroup($group1);
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $this->assertFalse($user->containsAdminGroup($group1));
+        $this->assertTrue($user->containsAdminGroup($group2));
+        $this->assertTrue($user->containsMemberGroup($group1));
+        $this->assertFalse($user->containsMemberGroup($group2));
+        $this->assertEquals(1, $user->getAdminGroups()->count());
+        $this->assertEquals(1, $user->getMemberGroups()->count());
+
+        $this->assertEquals(2, count($this->groupRepo->findAll()));
+
+        $user->removeMemberGroup($group1);
+
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $this->assertFalse($user->containsAdminGroup($group1));
+        $this->assertTrue($user->containsAdminGroup($group2));
+        $this->assertFalse($user->containsMemberGroup($group1));
+        $this->assertFalse($user->containsMemberGroup($group2));
+        $this->assertEquals(1, $user->getAdminGroups()->count());
+        $this->assertEquals(0, $user->getMemberGroups()->count());
+
+        $this->assertEquals(2, count($this->groupRepo->findAll()));
+    }
+
+    private function createGroup($key='Group1', $name='Group 1')
+    {
+        $group = new Group();
+
+        $group->setKey($key);
+        $group->setName($name);
+
+        $this->dm->persist($group);
+        $this->dm->flush();
+
+        return $group;
     }
 }
