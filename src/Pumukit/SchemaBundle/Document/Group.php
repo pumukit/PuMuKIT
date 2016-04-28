bd17061bd6daaf13b83adba24c382c3581669796
mrey <mrey@teltek.es>
2016-04-28 18:25:18 +0200

    #10450 Added document of Group

---
+++ src/Pumukit/SchemaBundle/Document/Group.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Pumukit\SchemaBundle\Document\Group
+ *
+ * @MongoDB\Document
+ */
+class Group
+{
+    const GROUP_ORIGIN_LOCAL = 'local';
+    const GROUP_ORIGIN_LDAP = 'ldap';
+
+    /**
+     * @var int $id
+     *
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @var string $key
+     *
+     * @MongoDB\String
+     * @MongoDB\UniqueIndex(order="asc")
+     * @Assert\Regex("/^\w*$/")
+     */
+    protected $key;
+
+    /**
+     * @var string $name
+     *
+     * @MongoDB\String
+     * @MongoDB\UniqueIndex(order="asc")
+     */
+    protected $name;
+
+    /**
+     * @var string $origin
+     *
+     * @MongoDB\String
+     */
+    protected $origin = self::GROUP_ORIGIN_LOCAL;
+
+    /**
+     * @var date $createdAt
+     *
+     * @MongoDB\Date
+     */
+    private $createdAt;
+
+    /**
+     * @var date $updatedAt
+     *
+     * @MongoDB\Date
+     */
+    private $updatedAt;
+
+    /**
+     * Used locale to override Translation listener`s locale
+     * this is not a mapped field of entity metadata, just a simple property
+     * @var locale $locale
+     */
+    private $locale = 'en';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $createdAt = new \Datetime('now');
+        $updatedAt = new \Datetime('now');
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set origin
+     *
+     * @param string $origin
+     */
+    public function setOrigin($origin)
+    {
+        $this->origin = $origin;
+    }
+
+    /**
+     * Get origin
+     *
+     * @return string
+     */
+    public function getOrigin()
+    {
+        return $this->origin;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param string $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return string
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
