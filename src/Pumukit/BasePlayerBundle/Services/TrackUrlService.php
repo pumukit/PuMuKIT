73705a5515f8f15491485651d2d4fd3eb76ad29e
Alfro <arodriguez@teltek.es>
2016-05-27 17:43:28 +0200

    #10823 BasePlayerBundle: Generated correct route on trackurlservice.

--- src/Pumukit/BasePlayerBundle/Services/TrackUrlService.php
+++ src/Pumukit/BasePlayerBundle/Services/TrackUrlService.php
@@ -4,18 +4,27 @@ namespace Pumukit\BasePlayerBundle\Services;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Pumukit\SchemaBundle\Document\Track;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class TrackUrlService
 {
     private $dm;
+    private $router;
 
-    public function __construct(DocumentManager $dm)
+    public function __construct(DocumentManager $dm, UrlGeneratorInterface $router)
     {
         $this->dm = $dm;
+        $this->router = $router;
     }
 
     public function generateTrackFileUrl(Track $track, $absolute = false)
     {
-        return $track->getUrl($absolute);
+        $ext = pathinfo($track->getUrl(), PATHINFO_EXTENSION);
+        $params = array(
+            'id' => $track->getId(),
+            'ext' => $ext,
+        );
+        $url = $this->router->generate('pumukit_trackfile_index', $params, $absolute ? UrlGeneratorInterface::ABSOLUTE_PATH: UrlGeneratorInterface::RELATIVE_PATH);
+        return $url;
     }
 }
