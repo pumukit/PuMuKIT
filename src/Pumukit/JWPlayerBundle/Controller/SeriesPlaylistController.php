99ab12f7e307644a8f036767ab4868b63335244f
Alfro <arodriguez@teltek.es>
2016-05-31 11:01:48 +0200

    #10825 JWPlayerBundle: Added SeriesPlaylistController. * This controller will provide a route to retrieve a JWPlayer playlist

---
+++ src/Pumukit/JWPlayerBundle/Controller/SeriesPlaylistController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Pumukit\JWPlayerBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pumukit\SchemaBundle\Document\Series;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Pumukit\WebTVBundle\Controller\WebTVController;
+
+class SeriesPlaylistController extends Controller implements WebTVController
+{
+    /**
+     * @Route("/series_playlist/{id}", name="pumukit_seriesplaylist_index", defaults={"no_channels": true} )
+     * @Template("PumukitJWPlayerBundle:JWPlayer:player_playlist.html.twig")
+     */
+    public function indexAction(Series $series, Request $request)
+    {
+        $playlistService = $this->get('pumukit_baseplayer.seriesplaylist');
+        $mmobjs = $playlistService->getPlaylistMmobjs($series);
+        return array(
+            'series' => $series,
+            'playlist_mmobjs' => $mmobjs,
+            'multimediaObject' => $series,
+            'responsive' => true,
+        );
+    }
+
+    /**
+     * @Route("/series_playlist/magic/{secret}", name="pumukit_videoplayer_magicindex", defaults={"show_hide": true, "no_channels": true} )
+     * @Template("PumukitJWPlayerBundle:JWPlayer:index.html.twig")
+     */
+    public function magicIndexAction(Series $series, Request $request)
+    {
+        $playlistService = $this->get('pumukit_baseplayer.seriesplaylist');
+        $mmobjs = $playlistService->getPlaylistMmobjs($series);
+        return array(
+            'series' => $series,
+            'playlist_mmobjs' => $mmobjs,
+            'multimediaObject' => $series,
+            'responsive' => true,
+        );
+    }
+}
