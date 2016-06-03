766c1b614213621ed67393eb1390f06e85a49f85
Alfro <arodriguez@teltek.es>
2016-06-03 17:42:17 +0200

    For #130. JWPlayerBundle: Merged index and magicIndex.
    Also renamed the Controller and its route.

--- src/Pumukit/JWPlayerBundle/Controller/PlaylistController.php
+++ src/Pumukit/JWPlayerBundle/Controller/PlaylistController.php
@@ -9,28 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Pumukit\WebTVBundle\Controller\WebTVController;
 
-class SeriesPlaylistController extends Controller implements WebTVController
+class PlaylistController extends Controller implements WebTVController
 {
     /**
-     * @Route("/series_playlist/{id}", name="pumukit_seriesplaylist_index", defaults={"no_channels": true} )
+     * @Route("/playlist/{id}", name="pumukit_seriesplaylist_index", defaults={"no_channels": true} )
+     * @Route("/playlist/magic/{secret}", name="pumukit_videoplayer_magicindex", defaults={"show_hide": true, "no_channels": true} )
      * @Template("PumukitJWPlayerBundle:JWPlayer:player_playlist.html.twig")
      */
     public function indexAction(Series $series, Request $request)
-    {
-        $playlistService = $this->get('pumukit_baseplayer.seriesplaylist');
-        $mmobjs = $playlistService->getPlaylistMmobjs($series);
-        return array(
-            'playlist_mmobjs' => $mmobjs,
-            'object' => $series,
-            'responsive' => true,
-        );
-    }
-
-    /**
-     * @Route("/series_playlist/magic/{secret}", name="pumukit_videoplayer_magicindex", defaults={"show_hide": true, "no_channels": true} )
-     * @Template("PumukitJWPlayerBundle:JWPlayer:player_playlist.html.twig")
-     */
-    public function magicIndexAction(Series $series, Request $request)
     {
         $playlistService = $this->get('pumukit_baseplayer.seriesplaylist');
         $mmobjs = $playlistService->getPlaylistMmobjs($series);
