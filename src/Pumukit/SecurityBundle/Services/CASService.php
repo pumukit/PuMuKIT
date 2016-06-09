61307b8bab69fb8d48b6f4a3e0abb6cccb0c693f
Ruben Gonzalez <rubenrua@gmail.com>
2016-06-09 23:16:00 +0200

    #10929 Add CAS single sign out

--- src/Pumukit/SecurityBundle/Services/CASService.php
+++ src/Pumukit/SecurityBundle/Services/CASService.php
@@ -23,53 +23,72 @@ class CASService
     private function prepare()
     {
         $this->initialize = true;
-        \phpCAS::client(CAS_VERSION_2_0, $this->casUrl, $this->casPort, $this->casUri, false);
+        \phpCAS::client(CAS_VERSION_2_0, $this->casUrl, $this->casPort, $this->casUri, true);
         //\phpCAS::setDebug('/tmp/cas.log');
         \phpCAS::setNoCasServerValidation();
-        //\phpCAS::setSingleSignoutCallback(array($this, 'casSingleSignOut'));
-        //\phpCAS::setPostAuthenticateCallback(array($this, 'casPostAuth'));
-        \phpCAS::handleLogoutRequests(true, $this->casAllowedIpClients);
+        if ($this->casAllowedIpClients) {
+            \phpCAS::handleLogoutRequests(true, $this->casAllowedIpClients);
+        } else {
+            \phpCAS::handleLogoutRequests(false);
+        }
     }
 
     public function isAuthenticated()
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
+
         return \phpCAS::isAuthenticated();
     }
 
     public function getUser()
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
+
         return \phpCAS::getUser();
     }
 
     public function getAttributes()
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
+
         return \phpCAS::getAttributes();
     }
 
     public function setFixedServiceURL($url)
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
         \phpCAS::setFixedServiceURL($url);
     }
 
     public function forceAuthentication()
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
         \phpCAS::forceAuthentication();
     }
 
     public function logoutWithRedirectService($url)
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
         \phpCAS::logoutWithRedirectService($url);
     }
 
     public function logout()
     {
-        if(!$this->initialize) $this->prepare();
+        if (!$this->initialize) {
+            $this->prepare();
+        }
         \phpCAS::logout();
     }
 }
