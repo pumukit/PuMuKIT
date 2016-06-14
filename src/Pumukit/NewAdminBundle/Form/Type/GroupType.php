fd83277632be84fa5d36ba89e9b02ffa6c6e257f
mrey <mrey@teltek.es>
2016-06-14 18:12:30 +0200

    10982 NewAdminBundle: add oninvalid in Group key creation

--- src/Pumukit/NewAdminBundle/Form/Type/GroupType.php
+++ src/Pumukit/NewAdminBundle/Form/Type/GroupType.php
@@ -24,7 +24,12 @@ class GroupType extends AbstractType
     {
         $builder
             ->add('key', 'text',
-                  array('label' => $this->translator->trans('Key', array(), null, $this->locale)))
+                  array(
+                        'attr' => array(
+                                        'pattern' => "^\w*$",
+                                        'oninvalid' => "setCustomValidity('The key can not have blank spaces neither special characters')",
+                                        'oninput' => "setCustomValidity('')"),
+                        'label' => $this->translator->trans('Key', array(), null, $this->locale)))
             ->add('name', 'text',
                   array('label' => $this->translator->trans('Name', array(), null, $this->locale)))
             ->add('comments', 'textarea',
