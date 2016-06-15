304dbc413820cc745c645aa37dda36cd8e135a53
Alfro <arodriguez@teltek.es>
2016-06-15 11:42:08 +0200

    #10870 BasePlayerBundle: Added 'playlist' mmobjs to list.
    The service functions return an iterable of mmobjs, including both the objects belonging to the 'series' and the objects inside the 'playlist' property.

--- src/Pumukit/BasePlayerBundle/Services/SeriesPlaylistService.php
+++ src/Pumukit/BasePlayerBundle/Services/SeriesPlaylistService.php
@@ -7,6 +7,7 @@ use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Document\Track;
 use Pumukit\SchemaBundle\Document\Series;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Doctrine\Common\Collections\Criteria;
 
 class SeriesPlaylistService
 {
@@ -25,17 +26,25 @@ class SeriesPlaylistService
                    ->field('series')->references($series);
         $mmobjs = $qb->getQuery()->execute();
 
-        return $mmobjs;
+        $playlist = $series->getPlaylist()->getMultimediaObjects()->getIterator();
+
+        $iterable = new \AppendIterator();
+        $iterable->append($mmobjs);
+        $iterable->append($playlist);
+        return $iterable;
     }
 
     public function getPlaylistFirstMmobj(Series $series)
     {
-        $mmobjs = array();
+        $mmobj = array();
         $qb = $this->mmobjRepo->createStandardQueryBuilder()
                    ->field('series')->references($series);
-        $mmobjs = $qb->getQuery()->getSingleResult();
+        $mmobj = $qb->getQuery()->getSingleResult();
 
-        return $mmobjs;
+        if(!$mmobj)
+            $mmobj = $series->getPlaylist()->getMultimediaObjects()->first();
+
+        return $mmobj;
     }
 
     public function getMmobjFromIdAndPlaylist($mmobjId, Series $series)
@@ -44,6 +53,15 @@ class SeriesPlaylistService
                    ->field('series')->references($series)
                    ->field('id')->equals(new \MongoId($mmobjId));
         $mmobj = $qb->getQuery()->getSingleResult();
+
+        if(!$mmobj) {
+            $mmobj = $series->getPlaylist()->getMultimediaObjects()->filter(
+                function($mmobj) use($mmobjId) {
+                    return $mmobj->getId() == $mmobjId;
+                }
+            );
+            $mmobj = $mmobj->first();
+        }
 
         return $mmobj;
     }
