2c59f8c2f1d0e989283ce7f102180cd8c723ae6f
rubenrua <rubenrua@teltek.es>
2016-08-25 18:10:08 +0200

    #11204-2 new simple wizard for naked backoffice

---
+++ src/Pumukit/WizardBundle/Controller/SimpleController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Pumukit\WizardBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Pumukit\EncoderBundle\Services\JobService;
+use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\NewAdminBundle\Form\Type\Base\CustomLanguageType;
+
+/**
+ * @Security("is_granted('ROLE_ACCESS_WIZARD_UPLOAD')")
+ */
+class SimpleController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function indexAction(Series $series, Request $request)
+    {
+        $languages = CustomLanguageType::getLanguageNames($this->container->getParameter('pumukit2.customlanguages'), $this->get('translator'));
+
+        return array(
+            'series' => $series,
+            'languages' => $languages,
+        );
+    }
+
+    public function uploadAction(Series $series, Request $request)
+    {
+        $jobService = $this->get('pumukitencoder.job');
+        $inspectionService = $this->get('pumukit.inspection');
+
+        $priority = 2;
+        $profile = $this->get('pumukitencoder.profile')->getDefaultMasterProfile();
+        $description = array();
+        $language = $request->request->get('language', $request->getLocale());
+        $file = $request->files->get('resource');
+
+        try {
+            if (!$file) {
+                throw new \Exception('No file found');
+            }
+
+            if (!$file->isValid()) {
+                throw new \Exception($file->getErrorMessage());
+            }
+
+            $filePath = $file->getPathname();
+
+            try {
+                //exception if is not a mediafile (video or audio)
+                $duration = $inspectionService->getDuration($filePath);
+            } catch (\Exception $e) {
+                throw new \Exception('The file is not a valid video or audio file');
+            }
+
+            if (0 == $duration) {
+                throw new \Exception('The file is not a valid video or audio file (duration is zero)');
+            }
+
+            $multimediaObject = $this->createMultimediaObject($file->getClientOriginalName(), $series);
+            $multimediaObject->setDuration($duration);
+
+            $jobService->createTrackFromLocalHardDrive(
+                $multimediaObject, $file, $profile, $priority, $language, $description,
+                array(), $duration, JobService::ADD_JOB_NOT_CHECKS
+            );
+        } catch (\Exception $e) {
+            //TODO Hanle error.
+            throw $e;
+        }
+
+        return $this->redirect($this->generateUrl('pumukitnewadmin_mms_shortener', array('id' => $multimediaObject->getId())));
+    }
+
+    /**
+     * Create Multimedia Object.
+     */
+    private function createMultimediaObject($title, Series $series)
+    {
+        $factoryService = $this->get('pumukitschema.factory');
+        $multimediaObject = $factoryService->createMultimediaObject($series, true, $this->getUser());
+
+        foreach ($this->container->getParameter('pumukit2.locales') as $locale) {
+            $multimediaObject->setTitle($title, $locale);
+        }
+
+        return $multimediaObject;
+    }
+}
