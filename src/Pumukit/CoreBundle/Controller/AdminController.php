7a0897476ffd2cc88c3b22a328f52e18cc0e5c23
Alfro <arodriguez@teltek.es>
2016-09-19 17:44:53 +0200

    #11523 CoreBundle: Added filter interfaces.

---
+++ src/Pumukit/CoreBundle/Controller/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pumukit\CoreBundle\Controller;
+
+/**
+ * This interface adds the 'admin' filter to any controller implementing it.
+ *
+ * The 'admin' filter checks whether you have edit access to the mmobjs/series.
+ * These filters are used to avoid certain mmobjs/series from appearing when executing ODM calls. (check EventListener/AdminFilterListener.php)
+ * We use the implementation of before/after filters with an EventListener as in the Symfony cookbooks.
+ * Symfony docs: http://symfony.com/doc/current/cookbook/event_dispatcher/before_after_filters.html#tag-controllers-to-be-checked
+ *
+ */
+interface AdminController
+{
+    //...
+}
---
+++ src/Pumukit/CoreBundle/Controller/PersonalControler.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pumukit\CoreBundle\Controller;
+
+/**
+ * This interface adds the 'personal' filter to any controller implementing it.
+ *
+ * The 'personal' filter checks whether you have access to the mmobjs/series or not, an '$or' of the 'personal' and the 'admin' filters.
+ * These filters are used to avoid certain mmobjs/series from appearing when executing ODM calls. (check EventListener/PersonalFilterListener.php)
+ * We use the implementation of before/after filters with an EventListener as in the Symfony cookbooks.
+ * Symfony docs: http://symfony.com/doc/current/cookbook/event_dispatcher/before_after_filters.html#tag-controllers-to-be-checked
+ *
+ */
+interface PersonalController
+{
+    //...
+}
