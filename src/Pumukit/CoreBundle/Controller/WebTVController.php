91c13a3b87343c26b20e8c9f095fb0e666b08851
Alfonso Rodríguez <arodriguez@teltek.es>
2016-09-21 17:23:02 +0200

    #11523 CoreBundle: Fixed controllers syntax.

--- src/Pumukit/CoreBundle/Controller/WebTVController.php
+++ src/Pumukit/CoreBundle/Controller/WebTVController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Pumukit\CoreBundle\Controller;
 
 /**
@@ -8,7 +9,6 @@ namespace Pumukit\CoreBundle\Controller;
  * These filters are used to avoid certain mmobjs/series from appearing when executing ODM calls. (check EventListener/WebTVFilterListener.php)
  * We use the implementation of before/after filters with an EventListener as in the Symfony cookbooks.
  * Symfony docs: http://symfony.com/doc/current/cookbook/event_dispatcher/before_after_filters.html#tag-controllers-to-be-checked
- *
  */
 interface WebTVController
 {
