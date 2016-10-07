600c9a315b39c87c0f4e85308d2d128c18e2526d
Ruben Gonzalez <rubenrua@gmail.com>
2016-10-08 00:39:03 +0200

    Add controller to get the list of keywords using an aggregation

---
+++ src/Pumukit/SchemaBundle/Controller/APIKeywordsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/api/keywords")
+ */
+class APIKeywordsController extends Controller
+{
+    /**
+     * @Route("/series.{_format}", defaults={"_format"="json"}, requirements={"_format": "json|xml"})
+     */
+    public function seriesAction(Request $request)
+    {
+        return $this->base('PumukitSchemaBundle:Series', $request->getRequestFormat(), 10);
+    }
+
+    /**
+     * @Route("/mmobj.{_format}", defaults={"_format"="json"}, requirements={"_format": "json|xml"})
+     */
+    public function mmobjAction(Request $request)
+    {
+        return $this->base('PumukitSchemaBundle:MultimediaObject', $request->getRequestFormat(), 10);
+    }
+
+    private function base($collName, $format = 'json', $limit = null)
+    {
+        $coll = $this
+          ->get('doctrine_mongodb.odm.document_manager')
+          ->getDocumentCollection($collName);
+        $serializer = $this->get('serializer');
+
+        $pipeline = array(
+            array('$project' => array( 'k' => '$keywords.en', '_id' => false )),
+            array('$match' => array('k' => array('$ne' => ''))),
+            array('$unwind' => array('path' => '$k')),
+            array('$group' => array('_id' => '$k', 'count' => array( '$sum' => 1 ))),
+            array('$sort' => array('count' => -1)),
+        );
+
+        if ($limit) {
+            $pipeline[] = array('$limit' => $limit);
+        }
+
+        $kws = $coll->aggregate($pipeline);
+        $data = $serializer->serialize($kws->toArray(), $format);
+        return new Response($data);
+    }
+
+}
