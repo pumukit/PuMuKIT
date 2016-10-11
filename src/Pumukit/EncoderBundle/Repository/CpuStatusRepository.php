5b2c4c24fd30046ed9aafd4ea7f786d1d8071c45
Alfro <arodriguez@teltek.es>
2016-10-11 10:47:12 +0200

    #11784 EncoderBundle: Added 'cpuStatus' document to mongo.
    This document will be used to save those CPUS that are on Maintenance Mode (deactivated).

---
+++ src/Pumukit/EncoderBundle/Repository/CpuStatusRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pumukit\EncoderBundle\Repository;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Pumukit\EncoderBundle\Document\CpuStatus;
+
+/**
+ * CpuStatusRepository
+ */
+class CpuStatusRepository extends DocumentRepository
+{
+
+}
