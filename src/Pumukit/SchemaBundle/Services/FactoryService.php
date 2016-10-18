262dc991ac9eb2d42c5670683a2c374bfca6428e
Ruben Gonzalez <rubenrua@gmail.com>
2016-10-19 00:01:37 +0200

    Default status for new multimedia objected must be blocked

--- src/Pumukit/SchemaBundle/Services/FactoryService.php
+++ src/Pumukit/SchemaBundle/Services/FactoryService.php
@@ -167,13 +167,11 @@ class FactoryService
         $mm->setPublicDate(new \DateTime("now"));
         $mm->setRecordDate($mm->getPublicDate());
 
+        $mm->setStatus(MultimediaObject::STATUS_BLOQ);
         if ($loggedInUser) {
             if ($loggedInUser->hasRole(Permission::INIT_STATUS_PUBLISHED)) {
                 $mm->setStatus(MultimediaObject::STATUS_PUBLISHED);
-            }else{
-                $mm->setStatus(MultimediaObject::STATUS_BLOQ);
             }
-
             foreach ($loggedInUser->getRoles() as $role) {
                 if ($pubCh = Permission::getPubChannelForRoleTagDefaul($role)) {
                     $this->tagService->addTagByCodToMultimediaObject($mm, $pubCh, false);
