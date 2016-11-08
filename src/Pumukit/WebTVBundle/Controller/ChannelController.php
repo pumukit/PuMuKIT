cd063938010cccc5f17ad6d414a72486f12a772c
Ruben Gonzalez <rubenrua@gmail.com>
2016-11-08 20:23:18 +0100

    PHP CS FIXER fixer single_array_no_trailing_comma

--- src/Pumukit/WebTVBundle/Controller/ChannelController.php
+++ src/Pumukit/WebTVBundle/Controller/ChannelController.php
@@ -27,7 +27,7 @@ class ChannelController extends Controller implements WebTVController
                            5 => array('102', '104', '106', '107', '114', '115'), //"Humanities",
                            6 => array('103'), //"Health & Medicine",
                            4 => array('116'), //"Law",
-                           7 => array('110', '111', '112', '113'), //"Social Matters & Education",
+                           7 => array('110', '111', '112', '113')//"Social Matters & Education",
     );
     /**
      * @Route("/series/channel/{channelNumber}.html", name="pumukit_webtv_channel_series")
