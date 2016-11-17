135b943060893745ea19967987ce1e3ec5117fa2
rubenrua <rubenrua@teltek.es>
2016-11-17 11:06:18 +0100

    #12447 Don't show chat if disabled

--- src/Pumukit/Cmar/LiveBundle/Controller/DefaultController.php
+++ src/Pumukit/Cmar/LiveBundle/Controller/DefaultController.php
@@ -6,7 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pumukit\LiveBundle\Controller\DefaultController as Base;
+use Pumukit\LiveBundle\Document\Live;
 
 class DefaultController extends Base
 {
+
+    public function iframeAction(Live $live)
+    {
+        $parameters = parent::iframeAction($live);
+
+        $parameters['enable_chat'] = $this->container->getParameter('pumukit_cmar_live.chat.enable');
+
+        return $parameters;
+    }
 }
