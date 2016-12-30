3779e0a263ef95e404f359746e6d8daa3ed95d3a
rubenrua <rubenrua@teltek.es>
2016-12-30 14:19:05 +0100

    Fix typo

--- src/Pumukit/BasePlayerBundle/DependencyInjection/Configuration.php
+++ src/Pumukit/BasePlayerBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pumukit_expired_video');
+        $rootNode = $treeBuilder->root('pumukit_base_player');
 
         $rootNode
           ->children()
