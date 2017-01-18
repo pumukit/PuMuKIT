4dfa6959347587cbcb4b6495e4d15f06d33be4fd
rubenrua <rubenrua@teltek.es>
2017-01-18 16:23:07 +0100

    TTK-12965 Add breadcrumb

--- src/Pumukit/TemplateBundle/EventListener/RouteFoundHttpListener.php
+++ src/Pumukit/TemplateBundle/EventListener/RouteFoundHttpListener.php
@@ -7,18 +7,22 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Pumukit\SchemaBundle\EventListener\LocaleListener;
+use Pumukit\WebTVBundle\Services\BreadcrumbsService;
 
 class RouteFoundHttpListener
 {
     private $templatEngine;
     private $repository;
     private $requestFixer;
+    private $breadcrumbsService;
 
-    public function __construct(DocumentManager $dm, EngineInterface $templatEngine, LocaleListener $requestFixer)
+    public function __construct(DocumentManager $dm, EngineInterface $templatEngine,
+                                LocaleListener $requestFixer, BreadcrumbsService $breadcrumbsService)
     {
         $this->templatEngine = $templatEngine;
         $this->repository = $dm->getRepository('PumukitTemplateBundle:Template');
         $this->requestFixer = $requestFixer;
+        $this->breadcrumbsService = $breadcrumbsService;
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
@@ -35,6 +39,7 @@ class RouteFoundHttpListener
 
             $t = $this->repository->findOneBy(array('name' => $name, 'hide' => false));
             if ($t) {
+                $this->breadcrumbsService->addList($t->getName(), 'pumukit_webtv_index_index', array(), true);
                 $response = $this->templatEngine->renderResponse('PumukitTemplateBundle:List:index.html.twig', array('template' => $t));
                 $event->setResponse($response);
             }
