2de9cc2447050fd8c911079469fd61e6ef839abc
rubenrua <rubenrua@teltek.es>
2017-01-18 13:56:13 +0100

    TTK-12965 Fix TemplateBundle list

--- src/Pumukit/SchemaBundle/EventListener/LocaleListener.php
+++ src/Pumukit/SchemaBundle/EventListener/LocaleListener.php
@@ -3,6 +3,7 @@
 namespace Pumukit\SchemaBundle\EventListener;
 
 use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -28,40 +29,42 @@ class LocaleListener implements EventSubscriberInterface
 
     public function onKernelRequest(GetResponseEvent $event)
     {
-        /*
-      if ($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
-          return;
-      }
-      */
+        //if ($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
+        //    return
+        //}
 
-      $request = $event->getRequest();
+        $this->fixRequestLocale($event->getRequest());
+    }
+
+    public function fixRequestLocale(Request $request)
+    {
         $requestLocale = $request->attributes->get('_locale');
         $sessionLocale = $request->getSession()->get('_locale');
 
-      // try to see if the locale has been set as a _locale routing parameter
-      if ($requestLocale && in_array($requestLocale, $this->pumukit2Locales)) {
-          $request->getSession()->set('_locale', $requestLocale);
-      } else {
-          if (!$sessionLocale || !in_array($sessionLocale, $this->pumukit2Locales)) {
-              if (in_array($this->defaultLocale, $this->pumukit2Locales)) {
-                  $request->getSession()->set('_locale', $this->defaultLocale);
-              } elseif (!empty($this->pumukit2Locales)) {
-                  $request->getSession()->set('_locale', $this->pumukit2Locales[0]);
-              } else {
-                  throw new \Exception('Pumukit2.Locales is empty. You should define it in your parameters.');
-              }
-          }
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($requestLocale && in_array($requestLocale, $this->pumukit2Locales)) {
+            $request->getSession()->set('_locale', $requestLocale);
+        } else {
+            if (!$sessionLocale || !in_array($sessionLocale, $this->pumukit2Locales)) {
+                if (in_array($this->defaultLocale, $this->pumukit2Locales)) {
+                    $request->getSession()->set('_locale', $this->defaultLocale);
+                } elseif (!empty($this->pumukit2Locales)) {
+                    $request->getSession()->set('_locale', $this->pumukit2Locales[0]);
+                } else {
+                    throw new \Exception('Pumukit2.Locales is empty. You should define it in your parameters.');
+                }
+            }
 
-          $request->setLocale($request->getSession()->get('_locale'));
-      }
+            $request->setLocale($request->getSession()->get('_locale'));
+        }
     }
 
     public static function getSubscribedEvents()
     {
         return array(
-                   // must be registered before the default Locale listener
-                   KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
-                   );
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
     }
 
     public function postLoad(LifecycleEventArgs $args)
--- src/Pumukit/TemplateBundle/EventListener/RouteFoundHttpListener.php
+++ src/Pumukit/TemplateBundle/EventListener/RouteFoundHttpListener.php
@@ -6,16 +6,19 @@ use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Pumukit\SchemaBundle\EventListener\LocaleListener;
 
 class RouteFoundHttpListener
 {
     private $templatEngine;
     private $repository;
+    private $requestFixer;
 
-    public function __construct(DocumentManager $dm, EngineInterface $templatEngine)
+    public function __construct(DocumentManager $dm, EngineInterface $templatEngine, LocaleListener $requestFixer)
     {
         $this->templatEngine = $templatEngine;
         $this->repository = $dm->getRepository('PumukitTemplateBundle:Template');
+        $this->requestFixer = $requestFixer;
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
@@ -24,6 +27,8 @@ class RouteFoundHttpListener
 
         if ($exception instanceof NotFoundHttpException) {
             $request = $event->getRequest();
+            $this->requestFixer->fixRequestLocale($request);
+
             $requestUri = $request->getRequestUri();
             $pathInfo = $request->getPathInfo();
             $name = substr($pathInfo, 1);
