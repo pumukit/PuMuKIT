da0b4dbc506124f839f42176e8cf2777e06ee9df
Ruben Gonzalez <rubenrua@gmail.com>
2017-01-21 19:10:48 +0100

    Add manual controller

---
+++ src/Pumukit/NewAdminBundle/Controller/ManualController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pumukit\NewAdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/**
+ * @Security("is_granted('ROLE_ACCESS_MULTIMEDIA_SERIES')")
+ */
+class ManualController extends Controller implements NewAdminController
+{
+    /**
+     * @Route("/manual")
+     * @Template
+     */
+    public function indexAction(Request $request)
+    {
+        return array();
+    }
+}
