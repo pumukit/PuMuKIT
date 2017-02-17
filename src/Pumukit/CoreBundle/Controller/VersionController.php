08d6437c66bec9fe036320ab2001c23bcf1ad099
pogando <pablo.ogando@teltek.es>
2017-02-17 16:58:45 +0100

    hotfix filter with pumukit keyword

--- src/Pumukit/CoreBundle/Controller/VersionController.php
+++ src/Pumukit/CoreBundle/Controller/VersionController.php
@@ -23,7 +23,7 @@ class VersionController extends Controller implements AdminController
         $pumukit = array();
         $other = array();
         foreach ($composerLock->packages as $package) {
-            if ((strpos($package->name, '/pmk2-') !== false) || strpos(strtolower($package->name), 'pumukit') !== false || strpos(strtolower($package->name), 'teltek') !== false) {
+            if ((strpos($package->name, '/pmk2-') !== false) || strpos(strtolower($package->name), 'pumukit') !== false || in_array('teltek', $package->keywords) !== false) {
                 $pumukit[] = $package;
             } else {
                 $other[] = $package;
