d6a43458a482bb18c53e4350a26e34364572370e
mrey <mrey@teltek.es>
2017-02-17 16:01:39 +0100

    TTK-13384 NotificationBundle: php-cs-fixer

--- src/Pumukit/NotificationBundle/Services/JobNotificationService.php
+++ src/Pumukit/NotificationBundle/Services/JobNotificationService.php
@@ -133,7 +133,7 @@ class JobNotificationService
     /**
      * Get emails.
      *
-     * @param Job $job
+     * @param Job              $job
      * @param MultimediaObject $multimediaObject
      *
      * @return string|array
