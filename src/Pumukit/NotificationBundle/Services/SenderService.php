6592e89a8f4c3a6df76463ec8459882e85abd581
mrey <mrey@teltek.es>
2017-02-17 12:42:21 +0100

    TTK-13384 NotificationBundle: fix filterEmails and template in SenderService

--- src/Pumukit/NotificationBundle/Services/SenderService.php
+++ src/Pumukit/NotificationBundle/Services/SenderService.php
@@ -179,6 +179,7 @@ class SenderService
                 $errorEmails[] = $emailTo;
             }
         }
+
         $filterEmail = array(
             'verified' => $verifiedEmails,
             'error' => $errorEmails,
