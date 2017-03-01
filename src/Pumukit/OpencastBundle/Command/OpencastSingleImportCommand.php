5031d9135db4cd9fb626ba0873c192e4e24bcccd
rubenrua <rubenrua@teltek.es>
2017-03-01 11:54:53 +0100

    JIT

--- src/Pumukit/OpencastBundle/Command/OpencastSingleImportCommand.php
+++ src/Pumukit/OpencastBundle/Command/OpencastSingleImportCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
 
 class OpencastSingleImportCommand extends ContainerAwareCommand
 {
@@ -17,6 +18,7 @@ class OpencastSingleImportCommand extends ContainerAwareCommand
             ->setDescription('Import a single opencast recording')
             ->addArgument('id', InputArgument::REQUIRED, 'Opencast id to import')
             ->addOption('invert', 'i', InputOption::VALUE_NONE, 'Inverted recording (CAMERA <-> SCREEN)')
+            ->addOption('mmobjid', 'o', InputOption::VALUE_NONE, 'Use an existing multimedia object. Not create a new one')
         ;
     }
 
@@ -29,10 +31,48 @@ class OpencastSingleImportCommand extends ContainerAwareCommand
         $opencastImportService = $this->getContainer()->get('pumukit_opencast.import');
         $mmobjRepo = $this->getContainer()->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
 
-        if ($mmobjRepo->findOneBy(array('properties.opencast' => $opencastId))) {
-            $output->writeln('Mediapackage '.$opencastId.' has already been imported, skipping to next mediapackage');
+        if ($mmObjId = $input->getOption('mmobjid')) {
+            if ($mmobj = $mmobjRepo->find($mmObjId)) {
+                $this->completeMultimediaObject($mmobj);
+            } else {
+                $output->writeln('No multimedia object with id '.$mmObjId);
+            }
         } else {
-            $opencastImportService->importRecording($opencastId, $input->getOption('invert'));
+            if ($mmobjRepo->findOneBy(array('properties.opencast' => $opencastId))) {
+                $output->writeln('Mediapackage '.$opencastId.' has already been imported, skipping to next mediapackage');
+            } else {
+                $opencastImportService->importRecording($opencastId, $input->getOption('invert'));
+            }
         }
+    }
+
+    protected function completeMultimediaObject(MultimediaObject $multimediaObject)
+    {
+        $opencastImportService = $this->getContainer()->get('pumukit_opencast.import');
+        $opencastClient = $this->getContainer()->get('pumukit_opencast.client');
+        $mmsService = $this->getContainer()->get('pumukitschema.multimedia_object');
+
+        $mediaPackage = $opencastClient->getMediaPackage($opencastId);
+
+        $properties = $opencastImportService->getMediaPackageField($mediaPackage, 'id');
+        if ($properties) {
+            $multimediaObject->setProperty('opencast', $properties);
+            $multimediaObject->setProperty('opencasturl', $opencastClient->getPlayerUrl().'?id='.$properties);
+        }
+        $multimediaObject->setProperty('opencastinvert', boolval($invert));
+
+        $media = $opencastImportService->getMediaPackageField($mediaPackage, 'media');
+        $tracks = $opencastImportService->getMediaPackageField($media, 'track');
+        if (isset($tracks[0])) {
+            // NOTE: Multiple tracks
+            for ($i = 0; $i < count($tracks); ++$i) {
+                $track = $opencastImportService->createTrackFromMediaPackage($mediaPackage, $multimediaObject, $i);
+            }
+        } else {
+            // NOTE: Single track
+            $track = $opencastImportService->createTrackFromMediaPackage($mediaPackage, $multimediaObject);
+        }
+
+        //$mmsService->updateMultimediaObject($multimediaObject);
     }
 }
