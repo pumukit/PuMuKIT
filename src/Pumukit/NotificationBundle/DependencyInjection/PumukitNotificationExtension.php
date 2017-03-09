4dd8dc9dbb934f7e2fefbc829fd8b98a7309aa42
mrey <mrey@teltek.es>
2017-03-09 23:33:53 +0100

    TTK-13526 Fix notification bundle tests in case of being active

--- src/Pumukit/NotificationBundle/DependencyInjection/PumukitNotificationExtension.php
+++ src/Pumukit/NotificationBundle/DependencyInjection/PumukitNotificationExtension.php
@@ -24,16 +24,24 @@ class PumukitNotificationExtension extends Extension
 
         $container->setParameter('pumukit_notification.enable', $config['enable']);
         $container->setParameter('pumukit_notification.platform_name', $config['platform_name']);
-        $container->setParameter('pumukit_notification.sender_email', $config['sender_email']);
         $container->setParameter('pumukit_notification.sender_name', $config['sender_name']);
         $container->setParameter('pumukit_notification.enable_multi_lang', $config['enable_multi_lang']);
-        $container->setParameter('pumukit_notification.template', $config['template']);
         $container->setParameter('pumukit_notification.subject_success', $config['subject_success']);
         $container->setParameter('pumukit_notification.subject_fails', $config['subject_fails']);
         $container->setParameter('pumukit_notification.subject_success_trans', $config['subject_success_trans']);
         $container->setParameter('pumukit_notification.subject_fails_trans', $config['subject_fails_trans']);
         $container->setParameter('pumukit_notification.notificate_errors_to_admin', $config['notificate_errors_to_admin']);
-        $container->setParameter('pumukit_notification.admin_email', $config['admin_email']);
+
+        $env = $container->getParameter('kernel.environment');
+        if ($env === 'test') {
+            $container->setParameter('pumukit_notification.template', Configuration::TEMPLATE);
+            $container->setParameter('pumukit_notification.sender_email', Configuration::SENDER_EMAIL);
+            $container->setParameter('pumukit_notification.admin_email', Configuration::ADMIN_EMAIL);
+        } else {
+            $container->setParameter('pumukit_notification.template', $config['template']);
+            $container->setParameter('pumukit_notification.sender_email', $config['sender_email']);
+            $container->setParameter('pumukit_notification.admin_email', $config['admin_email']);
+        }
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
