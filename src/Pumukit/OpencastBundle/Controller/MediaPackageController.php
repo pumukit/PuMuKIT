72172638bae8d244b902a0d04b06c03830d4b7bf
pogando <pablo.ogando@teltek.es>
2017-04-18 09:47:26 +0200

    Hotfix on redirect import opencast, and use php-cs-fixer

--- src/Pumukit/OpencastBundle/Controller/MediaPackageController.php
+++ src/Pumukit/OpencastBundle/Controller/MediaPackageController.php
@@ -5,21 +5,13 @@ namespace Pumukit\OpencastBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Pagerfanta\Adapter\FixedAdapter;
 use Pagerfanta\Pagerfanta;
-
-use Pumukit\SchemaBundle\Document\Series;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
-use Pumukit\SchemaBundle\Document\Track;
-use Pumukit\SchemaBundle\Document\Pic;
-use Pumukit\OpencastBundle\Services\OpencastService;
 
 /**
  * @Route("/admin")
@@ -39,29 +31,29 @@ class MediaPackageController extends Controller
             throw new AccessDeniedException('Not allowed. Configure your OpencastBundle to show the Importer Tab.');
         }
 
-        if(!$this->has('pumukit_opencast.client')) {
-          throw $this->createNotFoundException('PumukitOpencastBundle not configured.');
+        if (!$this->has('pumukit_opencast.client')) {
+            throw $this->createNotFoundException('PumukitOpencastBundle not configured.');
         }
 
         $opencastClient = $this->get('pumukit_opencast.client');
         $repository_multimediaobjects = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
 
         $limit = 10;
-        $page =  $request->get("page", 1);
+        $page = $request->get('page', 1);
         $criteria = $this->getCriteria($request);
 
         try {
             list($total, $mediaPackages) = $opencastClient->getMediaPackages(
-                (isset($criteria["name"])) ? $criteria["name"]->regex : "",
+                (isset($criteria['name'])) ? $criteria['name']->regex : '',
                 $limit,
-                ($page -1) * $limit);
+                ($page - 1) * $limit);
         } catch (\Exception $e) {
-          return new Response($this->renderView('PumukitOpencastBundle:MediaPackage:error.html.twig', array('admin_url' => $opencastClient->getUrl() ,'message' => $e->getMessage())), 503);
+            return new Response($this->renderView('PumukitOpencastBundle:MediaPackage:error.html.twig', array('admin_url' => $opencastClient->getUrl(), 'message' => $e->getMessage())), 503);
         }
 
         $currentPageOpencastIds = array();
         foreach ($mediaPackages as $mediaPackage) {
-            $currentPageOpencastIds[] = $mediaPackage["id"];
+            $currentPageOpencastIds[] = $mediaPackage['id'];
         }
 
         $adapter = new FixedAdapter($total, $mediaPackages);
@@ -71,14 +63,13 @@ class MediaPackageController extends Controller
         $pagerfanta->setCurrentPage($page);
 
         $repo = $repository_multimediaobjects->createQueryBuilder()
-          ->field("properties.opencast")->exists(true)
-          ->field("properties.opencast")->in($currentPageOpencastIds)
+          ->field('properties.opencast')->exists(true)
+          ->field('properties.opencast')->in($currentPageOpencastIds)
           ->getQuery()
           ->execute();
 
         return array('mediaPackages' => $pagerfanta, 'multimediaObjects' => $repo, 'player' => $opencastClient->getPlayerUrl());
     }
-
 
     /**
      * @Route("/opencast/mediapackage/{id}", name="pumukitopencast_import")
@@ -91,16 +82,20 @@ class MediaPackageController extends Controller
 
         $opencastService = $this->get('pumukit_opencast.import');
         $opencastService->importRecording($id, $request->get('invert'), $this->getUser());
-        return $this->redirect($this->getRequest()->headers->get('referer'));
+
+        if ($this->getRequest()->headers->get('referer')) {
+            return $this->redirect($this->getRequest()->headers->get('referer'));
+        } else {
+            return $this->redirectToRoute('pumukitopencast');
+        }
     }
 
     /**
-     * Gets the criteria values
+     * Gets the criteria values.
      */
     public function getCriteria($request)
     {
         $criteria = $request->get('criteria', array());
-
 
         if (array_key_exists('reset', $criteria)) {
             $this->get('session')->remove('admin/opencast/criteria');
