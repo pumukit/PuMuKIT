5eb3a75640bed05f1ec932ebe7d8fb3c80a05d05
rubenrua <rubenrua@teltek.es>
2017-09-18 12:22:16 +0200

    TTK-15298 Add doc

--- src/Pumukit/WebTVBundle/Twig/MongoDateExtension.php
+++ src/Pumukit/WebTVBundle/Twig/MongoDateExtension.php
@@ -2,6 +2,11 @@
 
 namespace Pumukit\WebTVBundle\Twig;
 
+/**
+ * MongoDate::toDateTime is only avaliable in PECL mongo >= 1.6.0.
+ *
+ * PuMuKIT 2.3 must be compatible with PECL mongo 1.4.5 (Ubuntu 14.04).
+ */
 class MongoDateExtension extends \Twig_Extension
 {
     /**
@@ -21,6 +26,7 @@ class MongoDateExtension extends \Twig_Extension
 
     public function convertMongoDateFilter(\MongoDate $mongoDate)
     {
+        // return $mongoDate->toDateTime()
         return new \DateTime('@'.$mongoDate->sec);
     }
 }
