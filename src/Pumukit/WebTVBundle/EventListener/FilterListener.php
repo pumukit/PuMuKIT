pumukit/PuMuKIT

commit 5a32e2b21bd9652bd83e076158f282b9da071a1a

TTK-14890 Doctrine filter is not enable in the error page

src/Pumukit/WebTVBundle/EventListener/FilterListener.php
@@ -37,7 +37,6 @@ class FilterListener
         $deprecatedCheck = false && (false !== strpos($req->attributes->get('_controller'), 'WebTVBundle'));
 
         if (($controller[0] instanceof WebTVController /*deprecated*/ || $deprecatedCheck)
-            && $event->isMasterRequest()
                 && $isFilterActivated) {
             $filter = $this->dm->getFilterCollection()->enable('frontend');
 

commit 8ace301d4b21fcaf15c5b43fd87ee6aff9e73b4f

TTK-14648 Not re-enable frontend filter.
Fix bug introduced with commit
5a32e2b21bd9652bd83e076158f282b9da071a1a (TTK-14890) when any
sub-request can enable the filter and override any filter parameter
value.

src/Pumukit/WebTVBundle/EventListener/FilterListener.php
@@ -38,6 +38,10 @@ class FilterListener
 
         if (($controller[0] instanceof WebTVController /*deprecated*/ || $deprecatedCheck)
                 && $isFilterActivated) {
+            if ($this->dm->getFilterCollection()->isEnabled('frontend')) {
+                return;
+            }
+
             $filter = $this->dm->getFilterCollection()->enable('frontend');
 
             if (isset($routeParams['show_hide']) && $routeParams['show_hide']) {