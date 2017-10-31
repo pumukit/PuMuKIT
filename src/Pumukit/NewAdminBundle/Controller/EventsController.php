d8b4763207a73364d8d3a23b751f71b9981a6c2a
pogando <pablo.ogando@teltek.es>
2017-10-31 09:24:21 +0100

    TTK-15845 go to page 1 when created new event to edit and show new event

--- src/Pumukit/NewAdminBundle/Controller/EventsController.php
+++ src/Pumukit/NewAdminBundle/Controller/EventsController.php
@@ -130,6 +130,7 @@ class EventsController extends Controller
 
         $session = $this->get('session');
         $session->set('admin/live/event/id', $multimediaObject->getId());
+        $this->get('session')->set('admin/live/event/page', 1);
 
         return $this->redirectToRoute('pumukit_new_admin_live_event_index');
     }
