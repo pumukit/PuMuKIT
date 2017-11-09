b0837919b3c3d9b728ef94dbf88fafb729e7e0bc
pogando <pablo.ogando@teltek.es>
2017-11-09 10:54:29 +0100

    TTK-15557, TTK-15809, TTK-15978 sort live events webtv

--- src/Pumukit/WebTVBundle/Controller/EventController.php
+++ src/Pumukit/WebTVBundle/Controller/EventController.php
@@ -2,7 +2,6 @@
 
 namespace Pumukit\WebTVBundle\Controller;
 
-use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
