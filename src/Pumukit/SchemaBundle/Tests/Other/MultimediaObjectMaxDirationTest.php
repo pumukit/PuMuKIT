a9c508eff11627ce5bf5774afb9fa76ac2ef6ffa
rubenrua <rubenrua@teltek.es>
2017-11-28 09:47:21 +0100

    TTK-16120 Unit test with track.size > 2GB

---
+++ src/Pumukit/SchemaBundle/Tests/Other/MultimediaObjectMaxDirationTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Pumukit\SchemaBundle\Tests\Other;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\SchemaBundle\Document\Track;
+
+class MultimediaObjectMaxDirationTest extends WebTestCase
+{
+    private $dm;
+    private $repo;
+    private $qb;
+    private $factoryService;
+
+    public function setUp()
+    {
+        $options = array('environment' => 'test');
+        static::bootKernel($options);
+
+        $this->dm = static::$kernel->getContainer()->get('doctrine_mongodb')->getManager();
+        $this->repo = $this->dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $this->factoryService = static::$kernel->getContainer()->get('pumukitschema.factory');
+
+        //DELETE DATABASE
+        $this->dm->getDocumentCollection('PumukitSchemaBundle:MultimediaObject')
+            ->remove(array());
+        $this->dm->getDocumentCollection('PumukitSchemaBundle:Series')
+            ->remove(array());
+        $this->dm->flush();
+    }
+
+    public function tearDown()
+    {
+        $this->dm->close();
+        $this->dm = null;
+        $this->repo = null;
+        $this->factoryService = null;
+        gc_collect_cycles();
+        parent::tearDown();
+    }
+
+    public function testMaxDuration()
+    {
+        $size = 2446644584;
+        $duration = 608;
+
+        $mm = $this->createMultimediaObject();
+        $track = new Track();
+        $mm->addTrack($track);
+
+        $this->dm->persist($mm);
+        $this->dm->flush();
+
+        $track->setSize($size);
+        $track->setDuration($duration);
+
+        $this->dm->persist($mm);
+        $this->dm->flush();
+
+        $id = $mm->getId();
+        $trackId = $track->getId();
+
+        $this->dm->clear();
+
+        $mm = $this->repo->find($id);
+        $track = $mm->getTrackById($trackId);
+
+        $this->assertEquals($size, $track->getSize());
+        $this->assertEquals($duration, $track->getDuration());
+    }
+
+    private function createMultimediaObject()
+    {
+        $rank = 1;
+        $status = MultimediaObject::STATUS_NEW;
+        $record_date = new \DateTime();
+        $public_date = new \DateTime();
+        $title = 'Title';
+        $subtitle = 'Subtitle';
+        $description = 'Description';
+
+        $series = $this->factoryService->createSeries();
+        $mm = $this->factoryService->createMultimediaObject($series);
+
+        $mm->setStatus($status);
+        $mm->setRecordDate($record_date);
+        $mm->setPublicDate($public_date);
+        $mm->setTitle($title);
+        $mm->setSubtitle($subtitle);
+        $mm->setDescription($description);
+
+        $this->dm->persist($mm);
+        $this->dm->flush();
+
+        return $mm;
+    }
+}
