030166a3dcfd7b37c8eca94dddf485cd0779d0b3
mrey <mrey@teltek.es>
2018-02-09 17:23:37 +0100

    TTK-16830: SchemaBundle - Add functions service for event poster and text color

--- src/Pumukit/SchemaBundle/Services/EmbeddedEventSessionService.php
+++ src/Pumukit/SchemaBundle/Services/EmbeddedEventSessionService.php
@@ -3,6 +3,7 @@
 namespace Pumukit\SchemaBundle\Services;
 
 use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\EmbeddedEvent;
 use Doctrine\ODM\MongoDB\DocumentManager;
 
 class EmbeddedEventSessionService
@@ -10,6 +11,152 @@ class EmbeddedEventSessionService
     private $dm;
     private $repo;
     private $collection;
+
+    const DEFAULT_POSTER = '/bundles/pumukitwebtv/images/live_screen.jpg';
+    const DEFAULT_COLOR = 'white';
+
+    private $validColors = array(
+        'aliceblue',
+        'antiquewhite',
+        'aqua',
+        'aquamarine',
+        'azure',
+        'beige',
+        'bisque',
+        'black',
+        'blanchedalmond',
+        'blue',
+        'blueviolet',
+        'brown',
+        'burlywood',
+        'cadetblue',
+        'chartreuse',
+        'chocolate',
+        'coral',
+        'cornflowerblue',
+        'cornsilk',
+        'crimson',
+        'cyan',
+        'darkblue',
+        'darkcyan',
+        'darkgoldenrod',
+        'darkgray',
+        'darkgreen',
+        'darkkhaki',
+        'darkmagenta',
+        'darkolivegreen',
+        'darkorange',
+        'darkorchid',
+        'darkred',
+        'darksalmon',
+        'darkseagreen',
+        'darkslateblue',
+        'darkslategray',
+        'darkturquoise',
+        'darkviolet',
+        'deeppink',
+        'deepskyblue',
+        'dimgray',
+        'dodgerblue',
+        'firebrick',
+        'floralwhite',
+        'forestgreen',
+        'fuchsia',
+        'gainsboro',
+        'ghostwhite',
+        'gold',
+        'goldenrod',
+        'gray',
+        'green',
+        'greenyellow',
+        'honeydew',
+        'hotpink',
+        'indianred',
+        'indigo',
+        'ivory',
+        'khaki',
+        'lavender',
+        'lavenderblush',
+        'lawngreen',
+        'lemonchiffon',
+        'lightblue',
+        'lightcoral',
+        'lightcyan',
+        'lightgoldenrodyellow',
+        'lightgreen',
+        'lightgrey',
+        'lightpink',
+        'lightsalmon',
+        'lightseagreen',
+        'lightskyblue',
+        'lightslategray',
+        'lightsteelblue',
+        'lightyellow',
+        'lime',
+        'limegreen',
+        'linen',
+        'magenta',
+        'maroon',
+        'mediumaquamarine',
+        'mediumblue',
+        'mediumorchid',
+        'mediumpurple',
+        'mediumseagreen',
+        'mediumslateblue',
+        'mediumspringgreen',
+        'mediumturquoise',
+        'mediumvioletred',
+        'midnightblue',
+        'mintcream',
+        'mistyrose',
+        'moccasin',
+        'navajowhite',
+        'navy',
+        'oldlace',
+        'olive',
+        'olivedrab',
+        'orange',
+        'orangered',
+        'orchid',
+        'palegoldenrod',
+        'palegreen',
+        'paleturquoise',
+        'palevioletred',
+        'papayawhip',
+        'peachpuff',
+        'peru',
+        'pink',
+        'plum',
+        'powderblue',
+        'purple',
+        'red',
+        'rosybrown',
+        'royalblue',
+        'saddlebrown',
+        'salmon',
+        'sandybrown',
+        'seagreen',
+        'seashell',
+        'sienna',
+        'silver',
+        'skyblue',
+        'slateblue',
+        'slategray',
+        'snow',
+        'springgreen',
+        'steelblue',
+        'tan',
+        'teal',
+        'thistle',
+        'tomato',
+        'turquoise',
+        'violet',
+        'wheat',
+        'white',
+        'whitesmoke',
+        'yellow',
+        'yellowgreen',
+    );
 
     /**
      * Constructor.
@@ -102,6 +249,59 @@ class EmbeddedEventSessionService
         return $this->collection->aggregate($pipeline)->toArray();
     }
 
+    /**
+     * Get event poster.
+     *
+     * @param EmbeddedEvent $event
+     *
+     * @return string
+     */
+    public function getEventPoster(EmbeddedEvent $event)
+    {
+        $pics = $this->getMultimediaObjectPics($event->getId());
+
+        return $this->getPoster($pics);
+    }
+
+    /**
+     * Get poster text color.
+     *
+     * @param EmbeddedEvent $event
+     *
+     * @return string
+     */
+    public function getPosterTextColor(EmbeddedEvent $event)
+    {
+        $properties = $this->getMultimediaObjectProperties($event->getId());
+        if (isset($properties['postertextcolor'])) {
+            return $properties['postertextcolor'];
+        }
+
+        return self::DEFAULT_COLOR;
+    }
+
+    /**
+     * Validate HTML Color.
+     *
+     * @param string $color
+     *
+     * @return string
+     */
+    public function validateHtmlColor($color)
+    {
+        if (in_array(strtolower($color), $this->validColors) ||
+        preg_match('/^#[a-f0-9]{3}$/i', $color) ||
+        preg_match('/^#[a-f0-9]{6}$/i', $color)) {
+            return $color;
+        }
+        if (preg_match('/^[a-f0-9]{6}$/i', $color) ||
+        preg_match('/^[a-f0-9]{3}$/i', $color)) {
+            return '#'.$color;
+        }
+
+        throw new \Exception('Invalid text color: must be a hexadecimal number or a color name.');
+    }
+
     private function initPipeline()
     {
         $pipeline = array();
@@ -169,5 +369,94 @@ class EmbeddedEventSessionService
                 'data.session.start' => 1,
             ),
         );
+    }
+
+    private function getMultimediaObjectPics($eventId)
+    {
+        $pipeline = array();
+
+        $pipeline[] = array(
+            '$match' => array(
+                'embeddedEvent._id' => new \MongoId($eventId),
+            ),
+        );
+
+        $pipeline[] = array(
+            '$project' => array(
+                'multimediaObjectId' => '$_id',
+                'pics' => '$pics',
+            ),
+        );
+
+        $pipeline[] = array(
+            '$group' => array(
+                '_id' => '$multimediaObjectId',
+                'data' => array(
+                    '$first' => array(
+                        'multimediaObjectId' => '$multimediaObjectId',
+                        'pics' => '$pics',
+                    ),
+                ),
+            ),
+        );
+
+        $data = $this->collection->aggregate($pipeline)->toArray();
+
+        if (isset($data[0]['data']['pics'])) {
+            return $data[0]['data']['pics'];
+        }
+
+        return array();
+    }
+
+    private function getPoster($pics)
+    {
+        foreach ($pics as $pic) {
+            if (isset($pic['tags'])) {
+                if (in_array('poster', $pic['tags']) && isset($pic['url'])) {
+                    return $pic['url'];
+                }
+            }
+        }
+
+        return self::DEFAULT_POSTER;
+    }
+
+    private function getMultimediaObjectProperties($eventId)
+    {
+        $pipeline = array();
+
+        $pipeline[] = array(
+            '$match' => array(
+                'embeddedEvent._id' => new \MongoId($eventId),
+            ),
+        );
+
+        $pipeline[] = array(
+            '$project' => array(
+                'multimediaObjectId' => '$_id',
+                'properties' => '$properties',
+            ),
+        );
+
+        $pipeline[] = array(
+            '$group' => array(
+                '_id' => '$multimediaObjectId',
+                'data' => array(
+                    '$first' => array(
+                        'multimediaObjectId' => '$multimediaObjectId',
+                        'properties' => '$properties',
+                    ),
+                ),
+            ),
+        );
+
+        $data = $this->collection->aggregate($pipeline)->toArray();
+
+        if (isset($data[0]['data']['properties'])) {
+            return $data[0]['data']['properties'];
+        }
+
+        return array();
     }
 }
