02f0b9066f7cb0c3f3108c83c8b008a3e9eed515
rubenrua <rubenrua@teltek.es>
2018-02-22 13:22:52 +0100

    TTK-17143 Enable automatic Exceptions Logging

---
+++ src/Pumukit/CoreBundle/EventListener/ConsoleExceptionListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pumukit\CoreBundle\EventListener;
+
+use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use Psr\Log\LoggerInterface;
+
+/**
+ * From http://symfony.com/doc/2.8/cookbook/console/logging.html#enabling-automatic-exceptions-logging.
+ */
+class ConsoleExceptionListener
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onConsoleException(ConsoleExceptionEvent $event)
+    {
+        $command = $event->getCommand();
+        $exception = $event->getException();
+
+        $message = sprintf(
+            '%s: %s (uncaught exception) at %s line %s while running console command `%s`',
+            get_class($exception),
+            $exception->getMessage(),
+            $exception->getFile(),
+            $exception->getLine(),
+            $command->getName()
+        );
+
+        $this->logger->error($message, array('exception' => $exception));
+    }
+}
