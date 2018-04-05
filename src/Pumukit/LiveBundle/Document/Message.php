087bdfd6d0b83ad4a0cb52a351ba7ffce392c3ea
mrey <mrey@teltek.es>
2018-04-05 14:06:20 +0200

    TTK-16832: LiveBundle - Add Message Document and Repository

---
+++ src/Pumukit/LiveBundle/Document/Message.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Pumukit\LiveBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+/**
+ * @MongoDB\Document(repositoryClass="Pumukit\LiveBundle\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var integer id
+     *
+     * @MongoDB\Id
+     */
+    private $id;
+
+    /**
+     * @var string $author
+     *
+     * @MongoDB\String
+     */
+    private $author;
+
+    /**
+     * @var string multimediaObject
+     * @MongoDB\ReferenceOne(targetDocument="Pumukit\SchemaBundle\Document\MultimediaObject", inversedBy="multimedia_object", simple=true)
+     * @MongoDB\Index
+     */
+    private $multimediaObject;
+
+    /**
+     * @var string message
+     *
+     * @MongoDB\String
+     */
+    private $message;
+
+    /**
+     * @var \DateTime $insertDate
+     *
+     * @MongoDB\Date
+     */
+    private $insertDate;
+
+    /**
+     * Get id
+     *
+     * @return id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+      $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+      return $this->author;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setMessage($content)
+    {
+      $this->message = $content;
+    }
+
+    /**
+     * @param \DateTime $insertDate
+     */
+    public function setInsertDate($insertDate)
+    {
+      $this->insertDate = $insertDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getInsertDate()
+    {
+      return $this->insertDate;
+    }
+
+    /**
+     * @param string $multimediaObject
+     */
+    public function setMultimediaObject($multimediaObject)
+    {
+      $this->multimediaObject = $multimediaObject;
+    }
+}
