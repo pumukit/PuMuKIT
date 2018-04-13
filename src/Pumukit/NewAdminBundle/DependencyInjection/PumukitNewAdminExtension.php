b054be02ae2aec6441ff0b2e922a88ffa55d8a4f
mrey <mrey@teltek.es>
2018-04-13 13:09:17 +0200

    TTK-17588: Define default value for liveevent_contact_and_share

--- src/Pumukit/NewAdminBundle/DependencyInjection/PumukitNewAdminExtension.php
+++ src/Pumukit/NewAdminBundle/DependencyInjection/PumukitNewAdminExtension.php
@@ -29,6 +29,7 @@ class PumukitNewAdminExtension extends Extension
         $container->setParameter('pumukit_new_admin.advance_live_event', $config['advance_live_event']);
         $container->setParameter('pumukit_new_admin.advance_live_event_create_default_pic', $config['advance_live_event_create_default_pic']);
         $container->setParameter('pumukit_new_admin.advance_live_event_create_serie_pic', $config['advance_live_event_create_serie_pic']);
+        $container->setParameter('liveevent_contact_and_share', $config['liveevent_contact_and_share']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
