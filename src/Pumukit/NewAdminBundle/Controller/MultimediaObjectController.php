1b5731b8a96a075e99aaee6e5cb5520021c6c4b5
Daniel <daniel.peiteado@teltek.es>
2018-04-20 10:46:14 +0200

    TTK-17604 Added field to select paella layout in media edition tab
    (cherry picked from commit 4fd14edb601b5a0d26abbde7b5fb4902fb79b9fd)

--- src/Pumukit/NewAdminBundle/Controller/MultimediaObjectController.php
+++ src/Pumukit/NewAdminBundle/Controller/MultimediaObjectController.php
@@ -1471,4 +1471,17 @@ class MultimediaObjectController extends SortableAdminController implements NewA
 
         return (strpos($sRoute, 'all') === false) ? 'mms' : 'mmslist';
     }
+
+    public function updatePropertyAction(Request $request)
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $multimediaObject = $dm->getRepository('PumukitSchemaBundle:MultimediaObject')->findOneById(new \MongoId($request->get('id')));
+        $method = $request->getMethod();
+        if (in_array($method, array('POST'))) {
+            $multimediaObject->setProperty('paellalayout', $request->get('paellalayout'));
+            $dm->flush();
+        }
+
+        return new JsonResponse(array('paellalayout' => $multimediaObject->getProperty('paellalayout')));
+    }
 }
