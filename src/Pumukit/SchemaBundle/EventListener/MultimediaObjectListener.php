162342bc604bc6285e8080f751f9ae3b6a917192
Alfonso Rodríguez <arodriguez@teltek.es>
2018-07-18 16:31:07 +0200

    TTK-18571 SchemaBundle: Fixed tests. Added correct value. Fixed php-cs-fixer

--- src/Pumukit/SchemaBundle/EventListener/MultimediaObjectListener.php
+++ src/Pumukit/SchemaBundle/EventListener/MultimediaObjectListener.php
@@ -14,12 +14,14 @@ class MultimediaObjectListener
         $this->dm = $dm;
     }
 
-    private function getTracksType($tracks){
-        foreach($tracks as $track){
-            if(!$track->isOnlyAudio()){
+    private function getTracksType($tracks)
+    {
+        foreach ($tracks as $track) {
+            if (!$track->isOnlyAudio()) {
                 return MultimediaObject::TYPE_VIDEO;
             }
         }
+
         return MultimediaObject::TYPE_AUDIO;
     }
 
@@ -34,11 +36,11 @@ class MultimediaObjectListener
             $multimediaObject->setType(MultimediaObject::TYPE_VIDEO);
         } elseif ($multimediaObject->getProperty('externalplayer')) {
             $multimediaObject->setType(MultimediaObject::TYPE_EXTERNAL);
-        } elseif ($displayTracks = $multimediaObject->getTracksWithTag('display')){
+        } elseif ($displayTracks = $multimediaObject->getTracksWithTag('display')) {
             $multimediaObject->setType($this->getTracksType($displayTracks));
         } elseif ($masterTracks = $multimediaObject->getTracksWithTag('master')) {
             $multimediaObject->setType($this->getTracksType($masterTracks));
-        } elseif ($otherTracks = $multimediaObject->getTracks()){
+        } elseif ($otherTracks = $multimediaObject->getTracks()) {
             $multimediaObject->setType($this->getTracksType($otherTracks));
         } else {
             $multimediaObject->setType(MultimediaObject::TYPE_UNKNOWN);
