953e3b4c229c84bf8c263b4352d2243cda9b6566
Pablo Nieto <pnieto@teltek.es>
2018-10-29 16:35:07 +0100

    fix from reference

--- doc/docker/php/docker-entrypoint.sh
+++ doc/docker/php/docker-entrypoint.sh
@@ -11,6 +11,11 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
+	while ! exec 6<>/dev/tcp/db/27017; do
+	    echo "$(date) - still trying to connect to mongo"
+	    sleep 1
+        done
+
 	if [ "$APP_ENV" != 'prod' ]; then
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 	fi
