bc7bd7f64bd1d4f2cf64f46ae7b3b9ef79f777e1
Ruben Gonzalez <rubenrua@gmail.com>
2018-11-15 12:24:22 +0100

    TTK-19763 Not realpath in service constructors used by a twig extension
    No IO in main requests

--- src/Pumukit/EncoderBundle/DependencyInjection/PumukitEncoderExtension.php
+++ src/Pumukit/EncoderBundle/DependencyInjection/PumukitEncoderExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Pumukit\EncoderBundle\Services\ProfileService;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -21,6 +22,8 @@ class PumukitEncoderExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        ProfileService::validateProfilesDir($config['profiles']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
