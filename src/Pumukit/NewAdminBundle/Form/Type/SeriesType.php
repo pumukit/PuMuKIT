05f381d5c4127bc894309299364e518e793d1041
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2018-12-13 09:37:21 +0100

    TTK-19753 delete copyright and license form SeriesType.php

--- src/Pumukit/NewAdminBundle/Form/Type/SeriesType.php
+++ src/Pumukit/NewAdminBundle/Form/Type/SeriesType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
@@ -15,7 +14,6 @@ use Pumukit\NewAdminBundle\Form\Type\Other\Html5dateType;
 use Pumukit\NewAdminBundle\Form\Type\Base\TextI18nType;
 use Pumukit\NewAdminBundle\Form\Type\Base\TextareaI18nType;
 use Pumukit\NewAdminBundle\Form\Type\Base\TextI18nAdvanceType;
-use Pumukit\NewAdminBundle\Form\Type\Base\LicenseType;
 use Pumukit\SchemaBundle\Document\Series;
 
 class SeriesType extends AbstractType
@@ -99,24 +97,6 @@ class SeriesType extends AbstractType
                         'aria-label' => $this->translator->trans('Keywords', [], null, $this->locale),
                     ],
                     'label' => $this->translator->trans('Keywords', [], null, $this->locale),
-                ]
-            )
-            ->add(
-                'copyright',
-                TextType::class,
-                [
-                    'required' => false,
-                    'attr' => ['aria-label' => $this->translator->trans('Copyright', [], null, $this->locale)],
-                    'label' => $this->translator->trans('Copyright', [], null, $this->locale),
-                ]
-            )
-            ->add(
-                'license',
-                LicenseType::class,
-                [
-                    'required' => false,
-                    'attr' => ['aria-label' => $this->translator->trans('License', [], null, $this->locale)],
-                    'label' => $this->translator->trans('License', [], null, $this->locale),
                 ]
             )
             ->add(
