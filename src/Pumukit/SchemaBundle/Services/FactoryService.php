716e77b334b9a4a804a40bb1293fb5f048fe4266
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-01-24 09:36:08 +0100

    TTK-20545 set numview to 0 when cloned mmobj

--- src/Pumukit/SchemaBundle/Services/FactoryService.php
+++ src/Pumukit/SchemaBundle/Services/FactoryService.php
@@ -559,6 +559,7 @@ class FactoryService
         $new->setI18nKeyword($src->getI18nKeyword());
         $new->setCopyright($src->getCopyright());
         $new->setLicense($src->getLicense());
+        $new->setNumview(0);
         $new->setTextIndex($src->getTextIndex());
         $new->setSecondaryTextIndex($src->getSecondaryTextIndex());
         // NOTE: #7408 Specify which properties are clonable
@@ -590,6 +591,7 @@ class FactoryService
         }
         foreach ($src->getTracks() as $track) {
             $clonedTrack = clone $track;
+            $clonedTrack->setNumview(0);
             $this->dm->persist($clonedTrack);
             $new->addTrack($clonedTrack);
         }
