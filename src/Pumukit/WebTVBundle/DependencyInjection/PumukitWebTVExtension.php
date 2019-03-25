3fc8cdcbebc973b76ad2f06c6c5e076e75d521e0
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-03-25 13:56:02 +0100

    TTK-21289 use php cs fixer

--- src/Pumukit/WebTVBundle/DependencyInjection/PumukitWebTVExtension.php
+++ src/Pumukit/WebTVBundle/DependencyInjection/PumukitWebTVExtension.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -31,5 +30,4 @@ class PumukitWebTVExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
     }
-
 }
--- src/Pumukit/WebTVBundle/PumukitWebTVBundle.php
+++ src/Pumukit/WebTVBundle/PumukitWebTVBundle.php
@@ -5,7 +5,7 @@ namespace Pumukit\WebTVBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class PumukitWebTVBundle
+ * Class PumukitWebTVBundle.
  */
 class PumukitWebTVBundle extends Bundle
 {
