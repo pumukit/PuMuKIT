da5bfd1cfacabdaeb22612d21a9f094ae920df50
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-03-26 12:07:37 +0100

    TTK-21297 fix WebTVBundle ContactType

--- src/Pumukit/WebTVBundle/Form/Type/ContactType.php
+++ src/Pumukit/WebTVBundle/Form/Type/ContactType.php
@@ -7,72 +7,43 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-/**
- * Class ContactType.
- */
 class ContactType extends AbstractType
 {
-    /**
-     * @var TranslatorInterface
-     */
     private $translator;
-
     private $locale;
 
-    /**
-     * ContactType constructor.
-     *
-     * @param TranslatorInterface $translator
-     * @param string              $locale
-     */
-    public function __construct(TranslatorInterface $translator, $locale = 'en')
-    {
-        $this->translator = $translator;
-        $this->locale = $locale;
-    }
-
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->translator = $options['translator'];
+        $this->locale = $options['locale'];
+
         $builder
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => $this->translator->trans('Name', [], null, $this->locale),
-                    'attr' => ['class' => 'form-control'],
-                    'required' => true,
-                ]
-            )
-            ->add(
-                'email',
-                EmailType::class,
-                [
-                    'label' => $this->translator->trans('Email', [], null, $this->locale),
-                    'attr' => ['class' => 'form-control'],
-                    'required' => true,
-                ]
-            )
-            ->add(
-                'content',
-                TextareaType::class,
-                [
-                    'label' => $this->translator->trans('Content', [], null, $this->locale),
-                    'attr' => ['class' => 'form-control'],
-                    'required' => true,
-                ]
-            );
+            ->add('name', TextType::class, array(
+                'label' => $this->translator->trans('Name', array(), null, $this->locale),
+                'attr' => array('class' => 'form-control'),
+                'required' => true,
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => $this->translator->trans('Email', array(), null, $this->locale),
+                'attr' => array('class' => 'form-control'),
+                'required' => true,
+            ))
+            ->add('content', TextareaType::class, array(
+                'label' => $this->translator->trans('Content', array(), null, $this->locale),
+                'attr' => array('class' => 'form-control'),
+                'required' => true,
+            ));
     }
 
-    /**
-     * @return string
-     */
-    public function getName()
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('translator');
+        $resolver->setRequired('locale');
+    }
+
+    public function getBlockPrefix()
     {
         return 'pumukit_multimedia_object_contact';
     }
