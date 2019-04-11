b1204246f97615b431c75f6d933e38e9dd92679d
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-04-11 10:32:32 +0200

    Hotfix delete inheritance with base player

--- src/Pumukit/JWPlayerBundle/PumukitJWPlayerBundle.php
+++ src/Pumukit/JWPlayerBundle/PumukitJWPlayerBundle.php
@@ -6,8 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PumukitJWPlayerBundle extends Bundle
 {
-    public function getParent()
-    {
-        return 'PumukitBasePlayerBundle';
-    }
 }
