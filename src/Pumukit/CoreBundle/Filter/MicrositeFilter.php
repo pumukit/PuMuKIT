c8f753ff2581505e32e2753c0194572fe738c91a
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-06-11 09:27:05 +0200

    TTK-21793 recover microsite filter

---
+++ src/Pumukit/CoreBundle/Filter/MicrositeFilter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pumukit\CoreBundle\Filter;
+
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use Doctrine\ODM\MongoDB\Query\Filter\BsonFilter;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+class MicrositeFilter extends BsonFilter
+{
+    public function addFilterCriteria(ClassMetadata $targetDocument)
+    {
+        if (MultimediaObject::class === $targetDocument->reflClass->name) {
+            return ['tags.cod' => $this->getParameter('microsite_tag')];
+        }
+
+        return [];
+    }
+}
