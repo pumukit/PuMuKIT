527d12d562c01a6a14f83b8d350accd7e7d02c5b
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-07-17 15:08:45 +0200

    Twig_SimpleFilter is deprecated since Twig 2.7, use Twig\TwigFilter instead

--- src/Pumukit/WebTVBundle/Twig/MongoDateExtension.php
+++ src/Pumukit/WebTVBundle/Twig/MongoDateExtension.php
@@ -2,20 +2,23 @@
 
 namespace Pumukit\WebTVBundle\Twig;
 
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
 /**
  * MongoDate::toDateTime is only avaliable in PECL mongo >= 1.6.0.
  * PuMuKIT 2.3 must be compatible with PECL mongo 1.4.5 (Ubuntu 14.04).
  * Class MongoDateExtension.
  */
-class MongoDateExtension extends \Twig_Extension
+class MongoDateExtension extends AbstractExtension
 {
     /**
-     * @return array|\Twig_SimpleFilter[]
+     * @return array|TwigFilter[]
      */
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('convertMongoDate', [$this, 'convertMongoDateFilter']),
+            new TwigFilter('convertMongoDate', [$this, 'convertMongoDateFilter']),
         ];
     }
 
