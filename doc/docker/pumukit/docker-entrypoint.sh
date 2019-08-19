ae71cb37ba20c9718f1d5772f50b5f2836b30a5d
Javier Lorenzo Pouso <16794234+j-lorenzo@users.noreply.github.com>
2019-08-19 12:39:50 +0200

    Update docker-entrypoint.sh

--- doc/docker/pumukit/docker-entrypoint.sh
+++ doc/docker/pumukit/docker-entrypoint.sh
@@ -16,12 +16,12 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
     if [ "$APP_ENV" != 'prod' ]; then
         composer install --prefer-dist --no-progress --no-suggest --no-interaction
         bin/console doctrine:mongodb:schema:create
-		bin/console pumukit:init:repo all --force
+	bin/console pumukit:init:repo all --force
     	if [ "$AUTOCREATE_PUMUKIT_USER" == 'true']; then
-			set +e
-			php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_USER_MAIL $PUMUKIT_PASS --super-admin || true
-			set -e
-		fi
+	    set +e
+	    php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_USER_MAIL $PUMUKIT_PASS --super-admin || true
+	    set -e
+	fi
     fi
 fi
 
