9abac088743772b46beb274db2cf2a97c0c9941b
jlorenzo <16794234+j-lorenzo@users.noreply.github.com>
2019-08-19 12:11:02 +0200

    Changed PUMUKIT_EMAIL var

--- doc/docker/pumukit/docker-entrypoint.sh
+++ doc/docker/pumukit/docker-entrypoint.sh
@@ -16,9 +16,9 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
     if [ "$APP_ENV" != 'prod' ]; then
         composer install --prefer-dist --no-progress --no-suggest --no-interaction
         bin/console doctrine:mongodb:schema:create
-	bin/console pumukit:init:repo all --force
+      	bin/console pumukit:init:repo all --force
+        php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_EMAIL $PUMUKIT_PASS || true
     fi
 fi
-php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_MAIL $PUMUKIT_PASS
 
 exec docker-php-entrypoint "$@"
