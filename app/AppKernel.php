65ac8bfadbdf3a5fa319ad169a8b1f9218d05502
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-08-21 14:04:25 +0200

    Update appKernel.php bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,13 +1,13 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -34,19 +34,16 @@ class AppKernel extends Kernel
             new Pumukit\JWPlayerBundle\PumukitJWPlayerBundle(),
             new Pumukit\StatsUIBundle\PumukitStatsUIBundle(),
             new Vipx\BotDetectBundle\VipxBotDetectBundle(),
-        );
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Pumukit\InstallBundleBundle\PumukitInstallBundleBundle();
-            $bundles[] = new Pumukit\ExampleDataBundle\PumukitExampleDataBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-
             }
         }
 
