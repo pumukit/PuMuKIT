d8691890daf45dfba9385564b0379ece6be9f7fb
rubenrua <rubenrua@gmail.com>
2019-09-10 17:58:34 +0200

    TTK-22396 Update track duration when syncing

--- src/Pumukit/OpencastBundle/Services/OpencastImportService.php
+++ src/Pumukit/OpencastBundle/Services/OpencastImportService.php
@@ -445,6 +445,10 @@ class OpencastImportService
         $track->setUrl($url);
         $track->setPath($this->opencastService->getPath($url));
 
+        if ($track->getPath()) {
+            $this->inspectionService->autocompleteTrack($track);
+        }
+
         return true;
     }
 
