a8ab2c08c54f2a765e97e9d91177c0415868335c
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-09-10 09:39:50 +0200

    TTK-22385 allow activate/deactivate intro by request parameter

--- src/Pumukit/BasePlayerBundle/Services/IntroService.php
+++ src/Pumukit/BasePlayerBundle/Services/IntroService.php
@@ -6,28 +6,33 @@ use Pumukit\SchemaBundle\Document\MultimediaObject;
 
 class IntroService
 {
-    private $intro;
+    private $globalUrlIntroduction;
 
-    public function __construct($intro)
+    public function __construct($globalUrlIntroduction)
     {
-        $this->intro = $intro;
+        $this->globalUrlIntroduction = $globalUrlIntroduction;
     }
 
-    public function getVideoIntroduction(MultimediaObject $multimediaObject, bool $activateIntro = true): ?string
+    public function getVideoIntroduction(MultimediaObject $multimediaObject, bool $activateIntroFromRequest = true): ?string
     {
-        if (!$activateIntro) {
+        if (!$activateIntroFromRequest) {
             return null;
         }
 
         $urlIntroduction = $multimediaObject->getIntroductionVideo();
+
+        if($activateIntroFromRequest && $this->globalUrlIntroduction && !$urlIntroduction) {
+            return $this->globalUrlIntroduction;
+        }
+
         if ($urlIntroduction && filter_var($urlIntroduction, FILTER_VALIDATE_URL)) {
             return $urlIntroduction;
         }
 
-        if (!$this->intro) {
+        if (!$this->globalUrlIntroduction) {
             return null;
         }
 
-        return $this->intro;
+        return $this->globalUrlIntroduction;
     }
 }
