4ab07df046dceb1a0dbf855ac008241e8c321175
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2019-10-30 14:06:04 +0100

    WIP: Fix travis

--- doc/object-manager.php
+++ doc/object-manager.php
@@ -1,8 +1,6 @@
 <?php
 
-$loader = require __DIR__.'/../app/autoload.php';
-
-$kernel = new AppKernel('dev', true);
+$kernel = new \App\Kernel('dev', true);
 $kernel->boot();
 
 return $kernel->getContainer()->get('doctrine')->getManager();
