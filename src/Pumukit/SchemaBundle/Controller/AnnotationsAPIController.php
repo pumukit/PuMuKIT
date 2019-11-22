f67507c3619102476d2578bd7f656f1bb4122c20
Alfro <alfonsorguezp@gmail.com>
2019-11-22 11:39:03 +0100

    Hotfix: Removed old FrameworkExtraBundle Route class

--- src/Pumukit/SchemaBundle/Controller/AnnotationsAPIController.php
+++ src/Pumukit/SchemaBundle/Controller/AnnotationsAPIController.php
@@ -8,7 +8,6 @@ use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Event\AnnotationsEvents;
 use Pumukit\SchemaBundle\Event\AnnotationsUpdateEvent;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
