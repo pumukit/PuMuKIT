0053582847135b12ead15335027617d9ae33b1ec
Alfonso Rodríguez <arodriguez@teltek.es>
2020-01-28 12:29:06 +0100

    TTK-22691 BaseLivePlayerBundle: Removed unused imports

--- src/Pumukit/BaseLivePlayerBundle/DependencyInjection/PumukitBaseLivePlayerExtension.php
+++ src/Pumukit/BaseLivePlayerBundle/DependencyInjection/PumukitBaseLivePlayerExtension.php
@@ -2,9 +2,7 @@
 
 namespace Pumukit\BaseLivePlayerBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
