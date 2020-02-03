c720feb88408091a36142ef7d5155b6ff7f2bd3d
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-03 08:43:31 +0100

    TTK-22691 access to method root on TreeBuilder is deprecated

--- src/Pumukit/BaseLivePlayerBundle/DependencyInjection/Configuration.php
+++ src/Pumukit/BaseLivePlayerBundle/DependencyInjection/Configuration.php
@@ -5,20 +5,12 @@ namespace Pumukit\BaseLivePlayerBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pumukit_base_live_player');
+        $treeBuilder = new TreeBuilder('pumukit_base_live_player');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
