ecde877e7509a2e38e5cf9fb97c8449d1d356a78
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-05 16:48:33 +0100

    TTK-22691 BasePlayerBundle: Controllers as services

--- src/Pumukit/BasePlayerBundle/Controller/BasePlaylistController.php
+++ src/Pumukit/BasePlayerBundle/Controller/BasePlaylistController.php
@@ -4,11 +4,11 @@ namespace Pumukit\BasePlayerBundle\Controller;
 
 use Pumukit\CoreBundle\Controller\WebTVControllerInterface;
 use Pumukit\SchemaBundle\Document\Series;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-abstract class BasePlaylistController extends Controller implements WebTVControllerInterface
+abstract class BasePlaylistController extends AbstractController implements WebTVControllerInterface
 {
     /**
      * @Route("/playlist/{id}", name="pumukit_playlistplayer_index", defaults={"no_channels": true} )
