9108a5910edf21042cfa4a2dc32aa580f1dab0f4
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-05 13:48:36 +0100

    TTK-22691 add symfony serializer, create service to use it and defined controllers from BaseLivePlayer as services

---
+++ src/Pumukit/CoreBundle/Services/SerializerService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pumukit\CoreBundle\Services;
+
+use Symfony\Component\Serializer\SerializerInterface;
+
+class SerializerService
+{
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function dataSerialize(array $data, string $format = 'html'): string
+    {
+        return $this->serializer->serialize($data, $format);
+    }
+}
