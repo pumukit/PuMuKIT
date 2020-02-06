66459a5ce1881faece637c8d7227a71259f79099
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-06 13:07:38 +0100

    TTK-22691 WizardBundle: Controllers and Commands as services

--- src/Pumukit/WizardBundle/Controller/SimpleController.php
+++ src/Pumukit/WizardBundle/Controller/SimpleController.php
@@ -2,32 +2,37 @@
 
 namespace Pumukit\WizardBundle\Controller;
 
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Pumukit\EncoderBundle\Services\JobService;
+use Pumukit\EncoderBundle\Services\ProfileService;
+use Pumukit\InspectionBundle\Services\InspectionFfprobeService;
 use Pumukit\NewAdminBundle\Form\Type\Base\CustomLanguageType;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\SchemaBundle\Services\FactoryService;
+use Pumukit\SchemaBundle\Services\SortedMultimediaObjectsService;
+use Pumukit\WizardBundle\Services\FormEventDispatcherService;
+use Pumukit\WizardBundle\Services\LicenseService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Security("is_granted('ROLE_ACCESS_WIZARD_UPLOAD')")
  */
-class SimpleController extends Controller
+class SimpleController extends AbstractController
 {
     /**
-     * @return array
-     *
      * @Template("PumukitWizardBundle:Simple:index.html.twig")
      */
-    public function indexAction(Series $series, Request $request)
+    public function indexAction(Request $request, LicenseService $licenseService, TranslatorInterface $translator, Series $series, array $pumukitCustomLanguages): array
     {
-        $licenseService = $this->get('pumukit_wizard.license');
         $licenseContent = $licenseService->getLicenseContent($request->getLocale());
 
-        $languages = CustomLanguageType::getLanguageNames($this->container->getParameter('pumukit.customlanguages'), $this->get('translator'));
+        $languages = CustomLanguageType::getLanguageNames($pumukitCustomLanguages, $translator);
 
         return [
             'series' => $series,
@@ -37,18 +42,10 @@ class SimpleController extends Controller
         ];
     }
 
-    /**
-     * @throws \Exception
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function uploadAction(Series $series, Request $request)
+    public function uploadAction(Request $request, JobService $jobService, ProfileService $profileService, FactoryService $factoryService, array $locales, InspectionFfprobeService $inspectionFfprobeService, SortedMultimediaObjectsService $sortedMultimediaObjectsService, Series $series, $pumukitWizardSimpleDefaultMasterProfile)
     {
-        $jobService = $this->get('pumukitencoder.job');
-        $inspectionService = $this->get('pumukit.inspection');
-
         $priority = 2;
-        $profile = $this->getDefaultMasterProfile();
+        $profile = $this->getDefaultMasterProfile($profileService, $pumukitWizardSimpleDefaultMasterProfile);
         $description = [];
         $language = $request->request->get('language', $request->getLocale());
         $file = $request->files->get('resource');
@@ -66,17 +63,17 @@ class SimpleController extends Controller
 
             try {
                 //exception if is not a mediafile (video or audio)
-                $duration = $inspectionService->getDuration($filePath);
+                $duration = $inspectionFfprobeService->getDuration($filePath);
             } catch (\Exception $e) {
                 throw new \Exception('The file is not a valid video or audio file');
             }
 
-            if (0 == $duration) {
+            if (0 === $duration) {
                 throw new \Exception('The file is not a valid video or audio file (duration is zero)');
             }
 
             $title = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-            $multimediaObject = $this->createMultimediaObject($title, $series);
+            $multimediaObject = $this->createMultimediaObject($factoryService, $locales, $title, $series);
             $multimediaObject->setDuration($duration);
 
             $jobService->createTrackFromLocalHardDrive(
@@ -94,32 +91,25 @@ class SimpleController extends Controller
             throw $e;
         }
 
-        $this->get('pumukitschema.sorted_multimedia_object')->reorder($series);
+        $sortedMultimediaObjectsService->reorder($series);
 
         return $this->redirect($this->generateUrl('pumukitnewadmin_mms_shortener', ['id' => $multimediaObject->getId()]));
     }
 
     /**
-     * @return array
-     *
      * @Template("PumukitWizardBundle:Simple:embedindex.html.twig")
      */
-    public function embedindexAction(Request $request)
+    public function embedindexAction(Request $request, DocumentManager $documentManager, LicenseService $licenseService, TranslatorInterface $translator, array $pumukitCustomLocales, $pumukitWizardShowSimpleMmTitle, $pumukitWizardShowSimpleSeriesTitle)
     {
         $seriesId = $request->get('series');
         $externalData = $request->get('externalData');
-        $series = $this->getSeries($seriesId);
+        $series = $documentManager->getRepository(Series::class)->find($seriesId);
         if (!$series) {
-            $series = $this->getSeriesByExternalData($externalData);
+            $series = $this->getSeriesByExternalData($documentManager, $externalData);
         }
 
-        $licenseService = $this->get('pumukit_wizard.license');
         $licenseContent = $licenseService->getLicenseContent($request->getLocale());
-
-        $languages = CustomLanguageType::getLanguageNames($this->container->getParameter('pumukit.customlanguages'), $this->get('translator'));
-
-        $showMmTitle = $this->getParameter('pumukit_wizard.show_simple_mm_title');
-        $showSeriesTitle = $this->getParameter('pumukit_wizard.show_simple_series_title');
+        $languages = CustomLanguageType::getLanguageNames($pumukitCustomLocales, $translator);
 
         $seriesI18nTitle = [];
         if ($series) {
@@ -134,31 +124,23 @@ class SimpleController extends Controller
             'show_license' => $licenseService->isEnabled(),
             'license_text' => $licenseContent,
             'externalData' => $externalData,
-            'show_simple_mm_title' => $showMmTitle,
-            'show_simple_series_title' => $showSeriesTitle,
+            'show_simple_mm_title' => $pumukitWizardShowSimpleMmTitle,
+            'show_simple_series_title' => $pumukitWizardShowSimpleSeriesTitle,
             'series_i18n_title' => $seriesI18nTitle,
         ];
     }
 
-    /**
-     * @throws \Exception
-     *
-     * @return JsonResponse
-     */
-    public function embeduploadAction(Request $request)
+    public function embeduploadAction(Request $request, DocumentManager $documentManager, ProfileService $profileService, JobService $jobService, FactoryService $factoryService, InspectionFfprobeService $inspectionFfprobeService, SortedMultimediaObjectsService $sortedMultimediaObjectsService, FormEventDispatcherService $formDispatcher, array $locales, $pumukitWizardSimpleDefaultMasterProfile)
     {
         $seriesId = $request->get('id');
-        $series = $this->getSeries($seriesId);
+        $series = $documentManager->getRepository(Series::class)->find($seriesId);
         $externalData = $request->get('externalData');
         if (!$series) {
-            $series = $this->getSeriesByExternalData($externalData);
+            $series = $this->getSeriesByExternalData($documentManager, $externalData);
         }
 
-        $jobService = $this->get('pumukitencoder.job');
-        $inspectionService = $this->get('pumukit.inspection');
-
         $priority = 2;
-        $profile = $this->getDefaultMasterProfile();
+        $profile = $this->getDefaultMasterProfile($profileService, $pumukitWizardSimpleDefaultMasterProfile);
         $description = [];
         $language = $request->request->get('language', $request->getLocale());
         $file = $request->files->get('resource');
@@ -176,17 +158,17 @@ class SimpleController extends Controller
 
             try {
                 //exception if is not a mediafile (video or audio)
-                $duration = $inspectionService->getDuration($filePath);
+                $duration = $inspectionFfprobeService->getDuration($filePath);
             } catch (\Exception $e) {
                 throw new \Exception('The file is not a valid video or audio file');
             }
 
-            if (0 == $duration) {
+            if (0 === $duration) {
                 throw new \Exception('The file is not a valid video or audio file (duration is zero)');
             }
 
             if (!$series) {
-                $series = $this->createSeries($externalData);
+                $series = $this->createSeries($factoryService, $externalData);
             }
 
             $showMmTitle = $this->getParameter('pumukit_wizard.show_simple_mm_title');
@@ -194,16 +176,16 @@ class SimpleController extends Controller
                 $i18nTitle = $request->request->get('multimediaobject_i18n_title', []);
                 if (!array_filter($i18nTitle)) {
                     $title = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                    $i18nTitle = $this->createI18nTitleFromFile($title);
+                    $i18nTitle = $this->createI18nTitleFromFile($locales, $title);
                 }
             } else {
                 $title = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-                $i18nTitle = $this->createI18nTitleFromFile($title);
+                $i18nTitle = $this->createI18nTitleFromFile($locales, $title);
             }
-            $multimediaObject = $this->createMultimediaObjectWithI18nTitle($i18nTitle, $series);
+            $multimediaObject = $this->createMultimediaObjectWithI18nTitle($factoryService, $i18nTitle, $series);
             $multimediaObject->setDuration($duration);
 
-            $multimediaObject = $this->setExternalProperties($multimediaObject, $externalData);
+            $multimediaObject = $this->setExternalProperties($documentManager, $multimediaObject, $externalData);
 
             $jobService->createTrackFromLocalHardDrive(
                 $multimediaObject,
@@ -217,13 +199,12 @@ class SimpleController extends Controller
                 JobService::ADD_JOB_NOT_CHECKS
             );
 
-            $formDispatcher = $this->get('pumukit_wizard.form_dispatcher');
             $formDispatcher->dispatchSubmit($this->getUser(), $multimediaObject, ['simple' => true, 'externalData' => $externalData]);
         } catch (\Exception $e) {
             throw $e;
         }
 
-        $this->get('pumukitschema.sorted_multimedia_object')->reorder($series);
+        $sortedMultimediaObjectsService->reorder($series);
 
         $response = [
             'url' => $this->generateUrl('pumukitnewadmin_mms_shortener', ['id' => $multimediaObject->getId()]),
@@ -233,35 +214,19 @@ class SimpleController extends Controller
         return new JsonResponse($response);
     }
 
-    /**
-     * Create Multimedia Object.
-     *
-     * @param string $title
-     *
-     * @return MultimediaObject
-     */
-    private function createMultimediaObject($title, Series $series)
+    private function createMultimediaObject(FactoryService $factoryService, array $locales, string $title, Series $series): MultimediaObject
     {
-        $factoryService = $this->get('pumukitschema.factory');
         $multimediaObject = $factoryService->createMultimediaObject($series, true, $this->getUser());
 
-        foreach ($this->container->getParameter('pumukit.locales') as $locale) {
+        foreach ($locales as $locale) {
             $multimediaObject->setTitle($title, $locale);
         }
 
         return $multimediaObject;
     }
 
-    /**
-     * Create Multimedia Object.
-     *
-     * @param array $i18nTitle
-     *
-     * @return MultimediaObject
-     */
-    private function createMultimediaObjectWithI18nTitle($i18nTitle, Series $series)
+    private function createMultimediaObjectWithI18nTitle(FactoryService $factoryService, $i18nTitle, Series $series): MultimediaObject
     {
-        $factoryService = $this->get('pumukitschema.factory');
         $multimediaObject = $factoryService->createMultimediaObject($series, true, $this->getUser());
         if (!array_filter($i18nTitle)) {
             $i18nTitle = $factoryService->getDefaultMultimediaObjectI18nTitle();
@@ -271,100 +236,57 @@ class SimpleController extends Controller
         return $multimediaObject;
     }
 
-    /**
-     * Get Series from id.
-     *
-     * @param \MongoId|string $seriesId
-     *
-     * @return mixed
-     */
-    private function getSeries($seriesId)
+    private function getSeriesByExternalData(DocumentManager $documentManager, array $externalData)
     {
-        $dm = $this->get('doctrine_mongodb.odm.document_manager');
-        $repo = $dm->getRepository(Series::class);
-
-        return $repo->find($seriesId);
-    }
-
-    /**
-     * Get Series by external data.
-     *
-     * @param array $externalData
-     *
-     * @return object|null
-     */
-    private function getSeriesByExternalData($externalData)
-    {
-        $dm = $this->get('doctrine_mongodb.odm.document_manager');
-        $repo = $dm->getRepository(Series::class);
-
         if (isset($externalData['seriesData']['title'])) {
-            return $repo->findOneBy(['title' => $externalData['seriesData']['title'], 'properties.owners' => $this->getUser()->getId()]);
+            return $documentManager->getRepository(Series::class)->findOneBy([
+                'title' => $externalData['seriesData']['title'],
+                'properties.owners' => $this->getUser()->getId(),
+            ]);
         }
 
         return null;
     }
 
-    /**
-     * @param array $externalData
-     *
-     * @return Series
-     */
-    private function createSeries($externalData)
+    private function createSeries(FactoryService $factoryService, array $externalData): Series
     {
-        $factoryService = $this->get('pumukitschema.factory');
         if (isset($externalData['seriesData']['title'])) {
-            $series = $factoryService->createSeries($this->getUser(), $externalData['seriesData']['title']);
-        } else {
-            $series = $factoryService->createSeries($this->getUser());
+            return $factoryService->createSeries($this->getUser(), $externalData['seriesData']['title']);
         }
 
-        return $series;
+        return $factoryService->createSeries($this->getUser());
     }
 
-    /**
-     * @param string $title
-     *
-     * @return array
-     */
-    private function createI18nTitleFromFile($title)
+    private function createI18nTitleFromFile(array $locales, string $title): array
     {
         $i18nTitle = [];
-        foreach ($this->container->getParameter('pumukit.locales') as $locale) {
+        foreach ($locales as $locale) {
             $i18nTitle[$locale] = $title;
         }
 
         return $i18nTitle;
     }
 
-    /**
-     * @param array $externalData
-     *
-     * @return MultimediaObject
-     */
-    private function setExternalProperties(MultimediaObject $multimediaObject, $externalData)
+    private function setExternalProperties(DocumentManager $documentManager, MultimediaObject $multimediaObject, array $externalData): MultimediaObject
     {
         if (isset($externalData['mmobjData']['properties'])) {
             foreach ($externalData['mmobjData']['properties'] as $key => $value) {
                 $multimediaObject->setProperty($key, $value);
             }
-            $dm = $this->get('doctrine_mongodb.odm.document_manager');
-            $dm->persist($multimediaObject);
-            $dm->flush();
+
+            $documentManager->persist($multimediaObject);
+            $documentManager->flush();
         }
 
         return $multimediaObject;
     }
 
-    /**
-     * @return mixed|null
-     */
-    private function getDefaultMasterProfile()
+    private function getDefaultMasterProfile(ProfileService $profileService, $pumukitWizardSimpleDefaultMasterProfile = null)
     {
-        if ($this->container->hasParameter('pumukit_wizard.simple_default_master_profile')) {
-            return $this->container->getParameter('pumukit_wizard.simple_default_master_profile');
+        if ($pumukitWizardSimpleDefaultMasterProfile) {
+            return $pumukitWizardSimpleDefaultMasterProfile;
         }
 
-        return $this->get('pumukitencoder.profile')->getDefaultMasterProfile();
+        return $profileService->getDefaultMasterProfile();
     }
 }
