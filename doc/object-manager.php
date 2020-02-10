35fc638828f26d8d146174396144d5f95e696898
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-10 09:24:17 +0100

    TTK-22691 udpate composer and travis

--- doc/object-manager.php
+++ doc/object-manager.php
@@ -1,8 +1,9 @@
 <?php
 
-$loader = require __DIR__.'/../app/autoload.php';
+use App\Kernel;
 
-$kernel = new AppKernel('dev', true);
+require dirname(__DIR__) . '/config/bootstrap.php';
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 
-return $kernel->getContainer()->get('doctrine')->getManager();
+return $kernel->getContainer()->get('doctrine_mongodb.odm.document_manager');
