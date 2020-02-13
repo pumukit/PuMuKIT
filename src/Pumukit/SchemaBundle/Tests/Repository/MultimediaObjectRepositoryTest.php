21016335938b0da7d2cd11c028f817cef8f53c34
Alfonso Rodríguez <arodriguez@teltek.es>
2020-02-13 20:02:20 +0100

    TTK-22691  Tests: Corrected previous errored assertCount

--- src/Pumukit/SchemaBundle/Tests/Repository/MultimediaObjectRepositoryTest.php
+++ src/Pumukit/SchemaBundle/Tests/Repository/MultimediaObjectRepositoryTest.php
@@ -1832,7 +1832,7 @@ class MultimediaObjectRepositoryTest extends PumukitTestCase
         $mm3 = $this->createMultimediaObjectAssignedToSeries('mm3', $series2);
         $mm4 = $this->createMultimediaObjectAssignedToSeries('mm4', $series3);
 
-        static::assertCount(4, $this->repo);
+        static::assertEquals(4, $this->repo->count());
         static::assertEquals(492, $this->repo->countDuration());
     }
 
