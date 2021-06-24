63698db8f54d498f878284049901815f4a06887e
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-06-24 09:53:50 +0200

    [Composer] Update versions (#372)
    * [Composer] Update versions
    
    * [PHPCsFixer] Remove deprecated to use v3

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+$config =  new PhpCsFixer\Config();
+return $config->setRules([
+    '@Symfony' => true,
+    '@PhpCsFixer' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'is_null' => true,
+    'list_syntax' => ['syntax' => 'short'],
+    'modernize_types_casting' => true,
+    'ternary_to_null_coalescing' => true,
+    'combine_nested_dirname' => true,
+    'phpdoc_types_order' => [
+        'null_adjustment' => 'always_last',
+        'sort_algorithm' => 'none',
+    ],
+])
+->setFinder($finder)
+->setRiskyAllowed(true)
+;
