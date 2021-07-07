7833ec873609afef7868e165aecb65d65ed61aa7
acodina <alba.codina@teltek.es>
2021-07-07 16:54:00 +0200

    TTK-25113: Remove oms and series without visualizations

--- src/Pumukit/StatsBundle/Services/StatsService.php
+++ src/Pumukit/StatsBundle/Services/StatsService.php
@@ -124,39 +124,7 @@ class StatsService
             }
         }
 
-        //Add mmobj with zero views
-        if (count($aggregation) < $options['limit']) {
-            if (0 == count($aggregation)) {
-                $max = min((1 + $options['page']) * $options['limit'], $total);
-                for ($i = ($options['page'] * $options['limit']); $i < $max; ++$i) {
-                    $multimediaObject = $this->repo->find($mmobjIds[$i]);
-                    if ($multimediaObject) {
-                        $mostViewed[] = ['mmobj' => $multimediaObject,
-                            'num_viewed' => 0,
-                        ];
-                    }
-                }
-            } else {
-                foreach ($aggregationTotal as $element) {
-                    $idsWithVis[] = $element['_id'];
-                }
-                foreach ($mmobjIds as $element) {
-                    if (!in_array($element, $idsWithVis)) {
-                        $multimediaObject = $this->repo->find($element);
-                        if ($multimediaObject) {
-                            $mostViewed[] = ['mmobj' => $multimediaObject,
-                                'num_viewed' => 0,
-                            ];
-                            if (count($mostViewed) == $options['limit']) {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return [$mostViewed, $total];
+        return [$mostViewed, $totalInAggegation];
     }
 
     /**
@@ -196,39 +164,7 @@ class StatsService
             }
         }
 
-        //Add series with zero views
-        if (count($aggregation) < $options['limit']) {
-            if (0 == count($aggregation)) {
-                $max = min((1 + $options['page']) * $options['limit'], $total);
-                for ($i = ($options['page'] * $options['limit']); $i < $max; ++$i) {
-                    $series = $this->repoSeries->find($seriesIds[$i]);
-                    if ($series) {
-                        $mostViewed[] = ['series' => $series,
-                            'num_viewed' => 0,
-                        ];
-                    }
-                }
-            } else {
-                foreach ($aggregationTotal as $element) {
-                    $idsWithVis[] = $element['_id'];
-                }
-                foreach ($seriesIds as $element) {
-                    if (!in_array($element, $idsWithVis)) {
-                        $series = $this->repoSeries->find($element);
-                        if ($series) {
-                            $mostViewed[] = ['series' => $series,
-                                'num_viewed' => 0,
-                            ];
-                            if (count($mostViewed) == $options['limit']) {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return [$mostViewed, $total];
+        return [$mostViewed, $totalInAggegation];
     }
 
     /**
