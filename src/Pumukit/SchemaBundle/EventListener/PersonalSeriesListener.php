77b2fc9d9976342ae33a5b1014c01eeaf62a134c
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-07-08 09:29:53 +0200

    TTK-25184 check prototype owner on personal series

--- src/Pumukit/SchemaBundle/EventListener/PersonalSeriesListener.php
+++ src/Pumukit/SchemaBundle/EventListener/PersonalSeriesListener.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Pumukit\SchemaBundle\EventListener;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Role;
 use Pumukit\SchemaBundle\Document\Series;
 use Pumukit\SchemaBundle\Security\Permission;
 use Pumukit\SchemaBundle\Services\FactoryService;
@@ -20,26 +22,29 @@ class PersonalSeriesListener
     private $authorizationChecker;
     private $translator;
     private $locales;
+    private $personalScopeRoleCode;
 
     public function __construct(
         DocumentManager $documentManager,
         FactoryService $factoryService,
         AuthorizationCheckerInterface $authorizationChecker,
         TranslatorInterface $translator,
-        array $locales
+        array $locales,
+        string $personalScopeRoleCode
     ) {
         $this->documentManager = $documentManager;
         $this->factoryService = $factoryService;
         $this->authorizationChecker = $authorizationChecker;
         $this->translator = $translator;
         $this->locales = $locales;
+        $this->personalScopeRoleCode = $personalScopeRoleCode;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
     {
         $user = $event->getAuthenticationToken()->getUser();
         if ($this->hasUserAutoCreateSeriesRole()) {
-            if (!$user->getPersonalSeries() || !$this->seriesExists($user->getPersonalSeries())) {
+            if (!$user->getPersonalSeries() || !$this->seriesExists($user, $user->getPersonalSeries())) {
                 $series = $this->factoryService->createSeries($user, $this->generateDefaultTitle($user));
                 $series->setProperty('personal_series', true);
                 $user->setPersonalSeries($series->getId());
@@ -53,7 +58,7 @@ class PersonalSeriesListener
         return $this->authorizationChecker->isGranted(Permission::AUTO_CREATE_PERSONAL_SERIES);
     }
 
-    public function seriesExists(?string $series): bool
+    public function seriesExists(UserInterface $user, ?string $series): bool
     {
         if (!$series) {
             return false;
@@ -61,7 +66,11 @@ class PersonalSeriesListener
 
         $series = $this->documentManager->getRepository(Series::class)->findOneBy(['_id' => $series]);
         if ($series instanceof Series) {
-            return true;
+            if ($this->isOwnerOnSeriesPrototype($user, $series)) {
+                return true;
+            }
+
+            return false;
         }
 
         return false;
@@ -75,5 +84,22 @@ class PersonalSeriesListener
         }
 
         return $i18nTitle;
+    }
+
+    public function isOwnerOnSeriesPrototype(UserInterface $user, Series $series): bool
+    {
+        $prototype = $this->documentManager->getRepository(MultimediaObject::class)->findPrototype($series);
+        if ($prototype instanceof MultimediaObject) {
+            if (!$user->getPerson()) {
+                return false;
+            }
+
+            $role = $this->documentManager->getRepository(Role::class)->findOneBy(['cod' => $this->personalScopeRoleCode]);
+            if ($prototype->containsPersonWithRole($user->getPerson(), $role)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
