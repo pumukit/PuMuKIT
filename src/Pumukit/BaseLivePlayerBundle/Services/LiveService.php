58b51f4b5b23335c679e49fbaa51ec6383d97548
belenchu29 <belengomc@gmail.com>
2021-10-25 19:11:58 +0200

    create URL with HLS and add video screen for Stream 2

--- src/Pumukit/BaseLivePlayerBundle/Services/LiveService.php
+++ src/Pumukit/BaseLivePlayerBundle/Services/LiveService.php
@@ -12,9 +12,21 @@ class LiveService
      * Generate HLS URL from RTMP url.
      * Original twig template: {{ live.url|replace({'rtmp://':'http://', 'rtmpt://': 'http://'}) }}/{{ live.sourcename }}/playlist.m3u8.
      */
-    public function generateHlsUrl(Live $live): string
+    public function generateHlsUrl(Live $live, int $numStream = null): string
     {
         $sourceName = $live->getSourceName();
+        $sourceName2 = $live->getSourceName2();
+
+        if (null !== $numStream and 2 == $numStream) {
+            if (Live::LIVE_TYPE_AMS === $live->getLiveType()) {
+                $hls2 = sprintf('%s/%s/%s.m3u8', str_replace(['rtmp://', 'rtmpt://'], '//', $live->getUrl()), $sourceName2, $sourceName2);
+            } else {
+                $hls2 = sprintf('%s/%s/playlist.m3u8', str_replace(['rtmp://', 'rtmpt://'], '//', $live->getUrl()), $sourceName2);
+            }
+
+            return $hls2;
+        }
+
         if (Live::LIVE_TYPE_AMS === $live->getLiveType()) {
             $hls = sprintf('%s/%s/%s.m3u8', str_replace(['rtmp://', 'rtmpt://'], '//', $live->getUrl()), $sourceName, $sourceName);
         } else {
