126ed1d7b0b01e9d66a12aec7c64757b600d8d56
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2022-01-12 11:49:40 +0100

    TTK-25428 allow upload duplicated videos on inbox removed redis key w… (#495)

---
+++ src/Pumukit/CoreBundle/Utils/RedisUtils.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pumukit\CoreBundle\Utils;
+
+use Predis\Client;
+
+final class RedisUtils
+{
+    public static function createConnection(): Client
+    {
+        return new Client((new self())->options());
+    }
+
+    public static function elementsByKeyPattern(string $pattern): ?array
+    {
+        return self::createConnection()->keys($pattern);
+    }
+
+    public static function valuesFromKey(string $key): array
+    {
+        $result = (new self())->findValuesFromKey($key);
+
+        return json_decode($result, true);
+    }
+
+    public static function removeKey(string $key): int
+    {
+        return self::createConnection()->del($key);
+    }
+
+    private function findValuesFromKey(string $key): ?string
+    {
+        return self::createConnection()->get($key);
+    }
+
+    private function options(): array
+    {
+        return [
+            'scheme' => getenv('REDIS_SCHEME'),
+            'host' => getenv('REDIS_HOST'),
+            'port' => getenv('REDIS_PORT'),
+        ];
+    }
+}
