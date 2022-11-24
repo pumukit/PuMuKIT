3a8f9de30d6f43ba74276f3e8f9868f9a2d46b8f
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2022-11-24 10:21:26 +0100

    Use PHPCsFixer

--- rector.php
+++ rector.php
@@ -6,10 +6,10 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/config',
-        __DIR__ . '/doc',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
+        __DIR__.'/config',
+        __DIR__.'/doc',
+        __DIR__.'/public',
+        __DIR__.'/src',
     ]);
 
     $rectorConfig->import(\Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_73);
