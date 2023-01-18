85c6ec18293b51b1f39d63db03c3e1b21266340b
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2023-01-18 10:11:31 +0100

    Replace kernel.root_dir by kernel.project_dir

--- src/Pumukit/SchemaBundle/Tests/Services/PicServiceTest.php
+++ src/Pumukit/SchemaBundle/Tests/Services/PicServiceTest.php
@@ -41,6 +41,12 @@ class PicServiceTest extends PumukitTestCase
         parent::setUp();
         $this->factoryService = static::$kernel->getContainer()->get('pumukitschema.factory');
 
+        $this->context = $this->getMockBuilder(RequestContext::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $this->rootDir = static::$kernel->getContainer()->getParameter('kernel.root_dir');
         $publicDir = static::$kernel->getContainer()->getParameter('pumukit.public_dir');
         $scheme = static::$kernel->getContainer()->getParameter('router.request_context.scheme');
         $host = static::$kernel->getContainer()->getParameter('router.request_context.host');
