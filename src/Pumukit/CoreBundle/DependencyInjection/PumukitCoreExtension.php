c933715f0a226b9026f9e6537f7cdb421bef2831
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2023-03-08 12:58:52 +0100

    TTK-26217 fix variable name

--- src/Pumukit/CoreBundle/DependencyInjection/PumukitCoreExtension.php
+++ src/Pumukit/CoreBundle/DependencyInjection/PumukitCoreExtension.php
@@ -37,7 +37,7 @@ class PumukitCoreExtension extends Extension
         $container->setParameter('pumukit.maxFileSize', $config['maxFileSize']);
         $container->setParameter('pumukit.minFileSize', $config['minFileSize']);
         $container->setParameter('pumukit.maxNumberOfFiles', $config['maxNumberOfFiles']);
-        $container->setParameter('pumukit.overridePatchMethod', $config['overridePatchMethod']);
+        $container->setParameter('pumukit.overridePatchMethod', $config['inboxOverridePatchMethod']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
