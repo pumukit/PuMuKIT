04d6a1810f4c60f4b0d3ce00b2a7311fc4af9ec2
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2023-12-11 16:26:10 +0100

    [WIP] Add methods

--- src/Pumukit/SchemaBundle/Document/MediaType/Document.php
+++ src/Pumukit/SchemaBundle/Document/MediaType/Document.php
@@ -4,52 +4,116 @@ declare(strict_types=1);
 
 namespace Pumukit\SchemaBundle\Document\MediaType;
 
-use Pumukit\SchemaBundle\Document\Element;
+use MongoDB\BSON\ObjectId;
+use Pumukit\SchemaBundle\Document\ValueObject\i18nText;
+use Pumukit\SchemaBundle\Document\ValueObject\Tags;
 
-final class Document extends Element implements Media
+final class Document implements Media
 {
+    private const TYPE = 3;
+
+    private $id;
     private $originalName;
+    private $type;
+
+    private $description;
+    private $hide;
+    private $tags;
     private $download;
     private $views;
+    private $storage;
     private $metadata;
 
-    private function __construct(string $originalName, bool $isDownloadable, int $views, MediaMetadata $mediaMetadata)
-    {
+    private function __construct(
+        string        $originalName,
+        i18nText      $description,
+        Tags          $tags,
+        bool          $hide,
+        bool          $isDownloadable,
+        int           $views,
+        Storage       $storage,
+        MediaMetadata $mediaMetadata
+    ) {
+        $this->id = new ObjectId();
         $this->originalName = $originalName;
+        $this->type = self::TYPE;
+        $this->description = $description;
+        $this->tags = $tags;
+        $this->hide = $hide;
         $this->download = $isDownloadable;
         $this->views = $views;
+        $this->storage = $storage;
         $this->metadata = $mediaMetadata;
-        parent::__construct();
     }
 
-    public static function create(string $originalName, bool $isDownloadable, int $views, MediaMetadata $mediaMetadata): Media
+    public static function create(
+        string        $originalName,
+        i18nText      $description,
+        Tags          $tags,
+        bool          $hide,
+        bool          $isDownloadable,
+        int           $views,
+        Storage       $storage,
+        MediaMetadata $mediaMetadata
+    ): Media
     {
-        return new self($originalName, $isDownloadable, $views, $mediaMetadata);
+        return new self($originalName, $description, $tags, $hide, $isDownloadable, $views, $storage, $mediaMetadata);
     }
 
     public function __toString(): string
     {
-        // TODO: Implement __toString() method.
+        return (string) $this->id;
     }
 
-    public function originalName(): void
+    public function id(): ObjectId
     {
-        // TODO: Implement originalName() method.
+        return $this->id;
+    }
+
+    public function originalName(): string
+    {
+        return $this->originalName;
+    }
+
+    public function type(): int
+    {
+        return $this->type;
+    }
+
+    public function description(): i18nText
+    {
+        return $this->description;
+    }
+
+    public function tags(): Tags
+    {
+        return $this->tags;
+    }
+
+    public function isHide(): bool
+    {
+        return $this->hide;
     }
 
     public function isDownloadable(): bool
     {
-        // TODO: Implement download() method.
+        return $this->download;
     }
+
 
     public function views(): int
     {
-        // TODO: Implement views() method.
+        return $this->views;
+    }
+
+    public function storage(): Storage
+    {
+        return $this->storage;
     }
 
     public function metadata(): MediaMetadata
     {
-        // TODO: Implement fileInfo() method.
+        return $this->metadata;
     }
 
 }
