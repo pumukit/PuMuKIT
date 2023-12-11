c93e5c6b61b439451f8821e11b45ae7a529cf7fd
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2023-12-11 15:33:21 +0100

    [WIP] Add new system of generate media for multimedia objects

---
+++ src/Pumukit/SchemaBundle/Document/MediaType/Metadata/Generic.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pumukit\SchemaBundle\Document\MediaType\Metadata;
+
+use Pumukit\SchemaBundle\Document\MediaType\MediaMetadata;
+
+final class Generic implements MediaMetadata
+{
+
+}
