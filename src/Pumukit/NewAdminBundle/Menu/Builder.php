b884404d5a4c18c57ac4b66df7aa52356e037588
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2024-03-07 11:19:18 +0100

    WIP: Remove old wizard logic

--- src/Pumukit/NewAdminBundle/Menu/Builder.php
+++ src/Pumukit/NewAdminBundle/Menu/Builder.php
@@ -23,7 +23,8 @@ class Builder implements ContainerAwareInterface
         $this->authorizationChecker = $this->container->get('security.authorization_checker');
 
         $this->addDashboardMenu($menu);
-        $this->addWizardMenu($menu);
+//        TODO: DIGIREPO REMOVE
+//        $this->addWizardMenu($menu);
         $this->addMediaManagerMenu($menu);
         $this->addStatsMenu($menu);
         $this->addLiveMenu($menu);
@@ -47,18 +48,19 @@ class Builder implements ContainerAwareInterface
         }
     }
 
-    protected function addWizardMenu(KnpItemInterface $menu): void
-    {
-        if (!$this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
-            if ($this->authorizationChecker->isGranted(Permission::ACCESS_WIZARD_UPLOAD) && $this->authorizationChecker->isGranted(Permission::SHOW_WIZARD_MENU)) {
-                $masterRequest = $this->container->get('request_stack')->getMasterRequest();
-                $class = ($masterRequest && (0 === strpos($masterRequest->attributes->get('_route'), 'pumukitwizard_default_'))) ? 'active' : '';
-                $class .= ' menu_wizard_upload_new_videos';
-                $options = ['route' => 'pumukitwizard_default_series', 'attributes' => ['class' => $class]];
-                $menu->addChild('Upload new videos', $options);
-            }
-        }
-    }
+    // TODO: DIGIREPO REMOVE
+//    protected function addWizardMenu(KnpItemInterface $menu): void
+//    {
+//        if (!$this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
+//            if ($this->authorizationChecker->isGranted(Permission::ACCESS_WIZARD_UPLOAD) && $this->authorizationChecker->isGranted(Permission::SHOW_WIZARD_MENU)) {
+//                $masterRequest = $this->container->get('request_stack')->getMasterRequest();
+//                $class = ($masterRequest && (0 === strpos($masterRequest->attributes->get('_route'), 'pumukitwizard_default_'))) ? 'active' : '';
+//                $class .= ' menu_wizard_upload_new_videos';
+//                $options = ['route' => 'pumukitwizard_default_series', 'attributes' => ['class' => $class]];
+//                $menu->addChild('Upload new videos', $options);
+//            }
+//        }
+//    }
 
     protected function addMediaManagerMenu(KnpItemInterface $menu): void
     {
--- src/Pumukit/WizardBundle/DependencyInjection/PumukitWizardExtension.php
+++ src/Pumukit/WizardBundle/DependencyInjection/PumukitWizardExtension.php
@@ -4,35 +4,15 @@ declare(strict_types=1);
 
 namespace Pumukit\WizardBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PumukitWizardExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration(array_keys($container->getParameter('pumukitencode.profilelist')));
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $container->setParameter('pumukit_wizard.show_license', $config['show_license']);
-        $container->setParameter('pumukit_wizard.license_dir', $config['license_dir']);
-        $container->setParameter('pumukit_wizard.show_tags', $config['show_tags']);
-        $container->setParameter('pumukit_wizard.tag_parent_code', $config['tag_parent_code']);
-        $container->setParameter('pumukit_wizard.show_object_license', $config['show_object_license']);
-        $container->setParameter('pumukit_wizard.mandatory_title', $config['mandatory_title']);
-        $container->setParameter('pumukit_wizard.reuse_series', $config['reuse_series']);
-        $container->setParameter('pumukit_wizard.reuse_admin_series', $config['reuse_admin_series']);
-        $container->setParameter('pumukit_wizard.show_simple_mm_title', $config['show_simple_mm_title']);
-        $container->setParameter('pumukit_wizard.show_simple_series_title', $config['show_simple_series_title']);
-
-        if (isset($config['simple_default_master_profile'])) {
-            $container->setParameter('pumukit_wizard.simple_default_master_profile', $config['simple_default_master_profile']);
-        }
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
     }
 
-    public function getConfiguration(array $config, ContainerBuilder $container): ?ConfigurationInterface
-    {
-        return new Configuration(array_keys($container->getParameter('pumukitencode.profilelist')));
-    }
 }
