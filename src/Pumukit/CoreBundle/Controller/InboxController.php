5497397aa617745adefc3e4fec94339bc1082ec9
albacodina <48245111+albacodina@users.noreply.github.com>
2024-04-29 13:45:02 +0200

    TTK-26927: Exclude special characters when creating folders in the inbox (#617)
    * TTK-26927: Exclude special characters when creating folders in the inbox
    
    * TTK-26927: Change pattern to inbox folder
    
    * TTK-26927: Change error message when folder name is not valid
    
    ---------
    
    Co-authored-by: Pablo Ogando Ferreira <pablo.ogando@teltek.es>

--- src/Pumukit/CoreBundle/Controller/InboxController.php
+++ src/Pumukit/CoreBundle/Controller/InboxController.php
@@ -88,7 +88,7 @@ class InboxController extends AbstractController
     {
         $folderName = $request->get('folder');
 
-        if (false !== strpos($folderName, '#')) {
+        if (0 === preg_match('/^[[:alnum:]\s_]+$/', $folderName)) {
             return new JsonResponse(false);
         }
 
