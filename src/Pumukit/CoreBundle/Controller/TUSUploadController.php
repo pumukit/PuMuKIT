e8345c5d0b5526c9cdc53dd6c726c8db68403066
acodina <alba.codina@teltek.es>
2024-06-10 11:45:09 +0200

    TTK-26996: Fix saving files in folder and force creating folder

--- src/Pumukit/CoreBundle/Controller/TUSUploadController.php
+++ src/Pumukit/CoreBundle/Controller/TUSUploadController.php
@@ -2,6 +2,7 @@
 
 namespace Pumukit\CoreBundle\Controller;
 
+use Pumukit\CoreBundle\Services\InboxService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +10,13 @@ use TusPhp\Tus\Server;
 
 class TUSUploadController extends AbstractController
 {
+    private $inboxService;
+
+    public function __construct(InboxService $inboxService)
+    {
+        $this->inboxService = $inboxService;
+    }
+
     /**
      * @Route("/tus", name="tus_post")
      * @Route("/tus/{token}", name="tus_post_token", requirements={"token"=".+"})
@@ -16,8 +24,9 @@ class TUSUploadController extends AbstractController
      */
     public function server(Request $request, Server $server)
     {
-        if ('tus_post' === $request->attributes->get('_route') && !empty($request->get('folder_path'))) {
-            $server->setUploadDir($request->get('folder_path'));
+        if ('tus_post' === $request->attributes->get('_route') && !empty($request->get('series'))) {
+            $path = $this->inboxService->inboxPath().'/'.$request->get('series');
+            $server->setUploadDir($path);
         }
 
         return $server->serve();
