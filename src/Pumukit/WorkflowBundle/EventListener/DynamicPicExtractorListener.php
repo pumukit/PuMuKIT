f04ad033f60a31c066df10b22aa5154458593e6f
albacodina <48245111+albacodina@users.noreply.github.com>
2024-10-17 11:45:15 +0200

    TTK-26846: Fix number of dynamic images extracted

--- src/Pumukit/WorkflowBundle/EventListener/DynamicPicExtractorListener.php
+++ src/Pumukit/WorkflowBundle/EventListener/DynamicPicExtractorListener.php
@@ -7,6 +7,7 @@ namespace Pumukit\WorkflowBundle\EventListener;
 use Psr\Log\LoggerInterface;
 use Pumukit\EncoderBundle\Event\JobEvent;
 use Pumukit\EncoderBundle\Services\DynamicPicExtractorService;
+use Pumukit\EncoderBundle\Services\ProfileService;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Document\Track;
 
@@ -19,18 +20,24 @@ class DynamicPicExtractorListener
     private $dynamicPicExtractorService;
     private $enableDynamicPicExtract;
     private $trackTagAllowed;
+    private $profileService;
 
-    public function __construct(DynamicPicExtractorService $dynamicPicExtractorService, LoggerInterface $logger, bool $enableDynamicPicExtract = true, string $trackTagAllowed = 'master')
+    public function __construct(DynamicPicExtractorService $dynamicPicExtractorService, LoggerInterface $logger, ProfileService $profileService, bool $enableDynamicPicExtract = true, string $trackTagAllowed = 'master')
     {
         $this->dynamicPicExtractorService = $dynamicPicExtractorService;
         $this->logger = $logger;
         $this->enableDynamicPicExtract = $enableDynamicPicExtract;
         $this->trackTagAllowed = $trackTagAllowed;
+        $this->profileService = $profileService;
     }
 
     public function onJobSuccess(JobEvent $event): void
     {
-        if ($this->enableDynamicPicExtract) {
+        $profileName = $event->getJob()->getProfile();
+        $profile = $this->profileService->getProfile($profileName);
+        $generatePic = $profile['generate_pic'];
+
+        if ($this->enableDynamicPicExtract && $generatePic) {
             $this->generateDynamicPic($event->getMultimediaObject(), $event->getTrack());
         }
     }
@@ -38,6 +45,17 @@ class DynamicPicExtractorListener
     public function generateDynamicPic(MultimediaObject $multimediaObject, Track $track): bool
     {
         if (!$track->containsTag($this->trackTagAllowed) || $track->isOnlyAudio()) {
+            return false;
+        }
+
+        $jobs = [$multimediaObject->getProperty('executing_jobs'), $multimediaObject->getProperty('pending_jobs')];
+        $jobsToMerge = array_filter($jobs, function ($arr) {
+            return isset($arr) && !empty($arr);
+        });
+
+        $allJobs = array_merge(...$jobsToMerge);
+
+        if (count($allJobs) > 2 && $track->containsTag('presentation/delivery')) {
             return false;
         }
 
