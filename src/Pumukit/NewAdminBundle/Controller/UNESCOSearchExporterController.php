a4a053c95ceb201d407c5c6b0ea43f4e48e20282
Alba <albalago.18@gmail.com>
2024-10-24 13:21:26 +0200

    TTK-27300: Create new service to include duplicated code

--- src/Pumukit/NewAdminBundle/Controller/UNESCOSearchExporterController.php
+++ src/Pumukit/NewAdminBundle/Controller/UNESCOSearchExporterController.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Pumukit\NewAdminBundle\Controller;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
-use MongoDB\BSON\UTCDateTime;
-use Pumukit\NewAdminBundle\Services\MultimediaObjectSearchService;
 use Pumukit\NewAdminBundle\Services\TagCatalogueService;
+use Pumukit\NewAdminBundle\Services\UNESCOService;
 use Pumukit\SchemaBundle\Document\EmbeddedPerson;
 use Pumukit\SchemaBundle\Document\EmbeddedRole;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
@@ -38,21 +37,21 @@ class UNESCOSearchExporterController extends AbstractController implements NewAd
     /** @var RequestStack */
     private $requestStack;
 
-    /** @var MultimediaObjectSearchService */
-    private $multimediaObjectSearchService;
+    /** @var UNESCOService */
+    private $UNESCOService;
 
     public function __construct(
         DocumentManager $documentManager,
         SessionInterface $session,
         TagCatalogueService $tagCatalogueService,
         RequestStack $requestStack,
-        MultimediaObjectSearchService $multimediaObjectSearchService
+        UNESCOService $UNESCOService
     ) {
         $this->documentManager = $documentManager;
         $this->session = $session;
         $this->tagCatalogueService = $tagCatalogueService;
         $this->requestStack = $requestStack;
-        $this->multimediaObjectSearchService = $multimediaObjectSearchService;
+        $this->UNESCOService = $UNESCOService;
     }
 
     /**
@@ -102,7 +101,8 @@ class UNESCOSearchExporterController extends AbstractController implements NewAd
 
         $criteria = $session->get('UNESCO/criteria');
         if (isset($criteria) && !empty($criteria)) {
-            $qb = $this->addCriteria($qb, $criteria);
+            $request = $this->requestStack->getMainRequest();
+            $qb = $this->UNESCOService->addCriteria($qb, $criteria, $request->getLocale());
         }
 
         $results = $qb->getQuery()->execute()->toArray();
@@ -226,114 +226,5 @@ class UNESCOSearchExporterController extends AbstractController implements NewAd
         $response->headers->set('Content-Disposition', 'attachment; filename="resultados.csv"');
 
         return $response;
-    }
-
-    private function addCriteria($query, $criteria)
-    {
-        $request = $this->requestStack->getMainRequest();
-
-        foreach ($criteria as $key => $field) {
-            if ('roles' === $key && (is_countable($field) ? count($field) : 0) >= 1) {
-                foreach ($field as $key2 => $value) {
-                    $query->field('people')->elemMatch($query->expr()->field('cod')->equals($key2)->field('people.name')->equals($value));
-                }
-            } elseif ('public_date_init' === $key && !empty($field)) {
-                $public_date_init = $field;
-            } elseif ('public_date_finish' === $key && !empty($field)) {
-                $public_date_finish = $field;
-            } elseif ('record_date_init' === $key && !empty($field)) {
-                $record_date_init = $field;
-            } elseif ('record_date_finish' === $key && !empty($field)) {
-                $record_date_finish = $field;
-            } elseif ('$text' === $key && !empty($field)) {
-                if (preg_match('/^[0-9a-z]{24}$/', $field)) {
-                    $query->field('_id')->equals($field);
-                } else {
-                    $this->multimediaObjectSearchService->completeSearchQueryBuilder(
-                        $field,
-                        $query,
-                        $request->getLocale()
-                    );
-                }
-            } elseif ('type' === $key && !empty($field)) {
-                if ('all' !== $field) {
-                    $query->field('type')->equals($field);
-                }
-            } elseif ('tracks.duration' == $key && !empty($field)) {
-                $query = $this->findDuration($query, $key, $field);
-            } elseif ('year' === $key && !empty($field)) {
-                $query = $this->findDuration($query, 'year', $field);
-            } else {
-                $query->field($key)->equals($field);
-            }
-        }
-
-        if (isset($public_date_init, $public_date_finish)) {
-            $query->field('public_date')->range(
-                new UTCDateTime(strtotime($public_date_init) * 1000),
-                new UTCDateTime(strtotime($public_date_finish) * 1000)
-            );
-        } elseif (isset($public_date_init)) {
-            $date = date($public_date_init.'T23:59:59');
-            $query->field('public_date')->range(
-                new UTCDateTime(strtotime($public_date_init) * 1000),
-                new UTCDateTime(strtotime($date) * 1000)
-            );
-        } elseif (isset($public_date_finish)) {
-            $date = date($public_date_finish.'T23:59:59');
-            $query->field('public_date')->range(
-                new UTCDateTime(strtotime($public_date_finish) * 1000),
-                new UTCDateTime(strtotime($date) * 1000)
-            );
-        }
-
-        if (isset($record_date_init, $record_date_finish)) {
-            $query->field('record_date')->range(
-                new UTCDateTime(strtotime($record_date_init) * 1000),
-                new UTCDateTime(strtotime($record_date_finish) * 1000)
-            );
-        } elseif (isset($record_date_init)) {
-            $date = date($record_date_init.'T23:59:59');
-            $query->field('record_date')->range(
-                new UTCDateTime(strtotime($record_date_init) * 1000),
-                new UTCDateTime(strtotime($date) * 1000)
-            );
-        } elseif (isset($record_date_finish)) {
-            $date = date($record_date_finish.'T23:59:59');
-            $query->field('record_date')->range(
-                new UTCDateTime(strtotime($record_date_finish) * 1000),
-                new UTCDateTime(strtotime($date) * 1000)
-            );
-        }
-
-        return $query;
-    }
-
-    private function findDuration($query, $key, $field)
-    {
-        if ('tracks.duration' === $key) {
-            if ('-5' == $field) {
-                $query->field($key)->lte(300);
-            }
-            if ('-10' == $field) {
-                $query->field($key)->lte(600);
-            }
-            if ('-30' == $field) {
-                $query->field($key)->lte(1800);
-            }
-            if ('-60' == $field) {
-                $query->field($key)->lte(3600);
-            }
-            if ('+60' == $field) {
-                $query->field($key)->gt(3600);
-            }
-        } elseif ('year' === $key) {
-            $start = \DateTime::createFromFormat('d/m/Y:H:i:s', sprintf('01/01/%s:00:00:01', $field));
-            $end = \DateTime::createFromFormat('d/m/Y:H:i:s', sprintf('01/01/%s:00:00:01', ((int) $field) + 1));
-            $query->field('record_date')->gte($start);
-            $query->field('record_date')->lt($end);
-        }
-
-        return $query;
     }
 }
