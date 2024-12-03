0dc8f2ad2368e20c5ab978af881e740c7e8148e0
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2024-12-03 12:18:30 +0100

    Update JobRender.php

--- src/Pumukit/EncoderBundle/Services/JobRender.php
+++ src/Pumukit/EncoderBundle/Services/JobRender.php
@@ -52,7 +52,6 @@ final class JobRender
         $vars['output_dirname'] = dirname($vars['output']);
         $vars['output_basename'] = basename($vars['output']);
 
-
         foreach (range(1, 9) as $identifier) {
             $vars['tmpfile'.$identifier] = $this->tmpPath.'/'.random_int(0, mt_getrandmax());
         }
