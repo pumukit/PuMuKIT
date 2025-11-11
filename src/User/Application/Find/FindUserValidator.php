1a66e541a20e2ed0650f8507cf24465d15924d60
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2025-11-11 12:13:13 +0100

    feat: add user list table with new design

--- src/User/Application/Find/FindUserValidator.php
+++ src/User/Application/Find/FindUserValidator.php
@@ -12,7 +12,6 @@ final class FindUserValidator
             throw new \InvalidArgumentException('User ID cannot be empty');
         }
 
-        // Validate MongoDB ObjectId format (24 hex characters)
         if (!preg_match('/^[a-f0-9]{24}$/i', $request->id)) {
             throw new \InvalidArgumentException(
                 sprintf('Invalid User ID format: %s', $request->id)
---
+++ src/User/UI/Backend/Controller/ListUserDataController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\User\UI\Backend\Controller;
+
+use App\User\Application\List\ListUserRequest;
+use App\User\Application\List\ListUserService;
+use App\User\Domain\Repository\UserRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+
+class ListUserDataController extends AbstractController
+{
+    public function __construct(
+        private ListUserService $listUserService,
+        private UserRepositoryInterface $userRepository
+    ) {}
+
+    public function __invoke(
+        Request $request,
+        RouterInterface $router
+    ): JsonResponse {
+        $offset = (int) $request->query->get('offset', 0);
+        $limit = (int) $request->query->get('limit', 10);
+        $sort = $request->query->get('sort', 'fullName');
+        $order = $request->query->get('order', 'asc');
+
+        $page = (int) floor($offset / $limit) + 1;
+
+        $filters = [];
+
+        if(str_starts_with($sort, 'user.')) {
+            $sort = substr($sort, 5);
+        }
+
+        $dto = new ListUserRequest(
+            page: $page,
+            limit: $limit,
+            filters: $filters,
+            sort: $sort,
+            order: $order
+        );
+
+        $userResponse = ($this->listUserService)($dto);
+
+        $rows = [];
+        foreach ($userResponse->users as $item) {
+            $actionsHtml = sprintf(
+                '<div class="d-flex gap-1 justify-content-end">
+        <a href="%s" class="btn btn-sm"><i class="fa fa-eye"></i></a>
+        <form action="%s" method="POST" style="display:inline;" onsubmit="return confirm(\'Are you sure you want to clone this series?\');">
+            <button type="submit" class="btn btn-sm">
+                <i class="fa fa-copy"></i>
+            </button>
+        </form>
+        <form action="%s" method="POST" style="display:inline;" onsubmit="return confirm(\'Are you sure you want to delete this series?\');">
+            <button type="submit" class="btn btn-sm">
+                <i class="fa fa-trash"></i>
+            </button>
+        </form>
+    </div>',
+                $router->generate('series_view', ['id' => $item->getId()]),
+                $router->generate('series_clone', ['id' => $item->getId()]),
+                $router->generate('series_delete', ['id' => $item->getId()])
+            );
+
+            $rows[] = [
+                'user' => $item,
+                'actions' => $actionsHtml,
+            ];
+        }
+
+        return $this->json([
+            'total' => $userResponse->total,
+            'rows' => $rows,
+        ]);
+    }
+}
