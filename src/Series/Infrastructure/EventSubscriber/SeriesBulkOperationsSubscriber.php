fd54e186f41e218889223479944565e6f8c1cec8
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2025-11-26 13:18:45 +0100

    feat: add event and subscriber to series _bulk_operations

---
+++ src/Series/Infrastructure/EventSubscriber/SeriesBulkOperationsSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Series\Infrastructure\EventSubscriber;
+
+use App\Series\Domain\Event\SeriesBulkOperationsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class SeriesBulkOperationsSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SeriesBulkOperationsEvent::NAME => 'onBulkOperations',
+        ];
+    }
+
+    public function onBulkOperations(SeriesBulkOperationsEvent $event): void
+    {
+        $event->addOperation(
+            key: 'announce',
+            label: 'Announce | Not announce',
+            handler: '#',
+            type: 'route',
+            icon: 'fa-download',
+            confirmMessage: 'Do you want announce all series selected?',
+            priority: 1
+        );
+
+        $event->addOperation(
+            key: 'delete',
+            label: 'Delete',
+            handler: '#',
+            type: 'route',
+            icon: 'fa-trash',
+            confirmMessage: 'Do you want delete all series selected?',
+            priority: 99
+        );
+    }
+}
+
--- src/Series/UI/Backend/Controller/ListSeriesController.php
+++ src/Series/UI/Backend/Controller/ListSeriesController.php
@@ -2,13 +2,39 @@
 
 namespace App\Series\UI\Backend\Controller;
 
+use App\Series\Domain\Event\SeriesBulkOperationsEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 final class ListSeriesController extends AbstractController
 {
+    public function __construct(
+        private EventDispatcherInterface $eventDispatcher
+    ) {}
+
     public function __invoke(): Response
     {
-        return $this->render('@Series/UI/Backend/Pages/list.html.twig');
+        $bulkOperationsEvent = new SeriesBulkOperationsEvent();
+        $this->eventDispatcher->dispatch($bulkOperationsEvent, SeriesBulkOperationsEvent::NAME);
+
+        $operations = $bulkOperationsEvent->getOperations();
+        foreach ($operations as &$operation) {
+            if ($operation['type'] === 'route') {
+                if($operation['handler'] === '#') {
+                    continue;
+                }
+                $operation['handler'] = $this->generateUrl(
+                    $operation['handler'],
+                    $operation['route_params'] ?? []
+                );
+                $operation['type'] = 'url';
+            }
+        }
+
+        return $this->render('@Series/UI/Backend/Pages/list.html.twig', [
+            'bulkOperationsEvent' => $bulkOperationsEvent,
+            'bulkOperations' => $operations,
+        ]);
     }
 }
