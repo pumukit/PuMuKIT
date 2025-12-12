2d626fcf88c6c7f20ba0c7de6514981a53ee9917
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2025-12-12 14:31:33 +0100

    fix: add method to ViewSeriesValidator

--- src/ContentManagement/Series/Application/View/ViewSeriesValidator.php
+++ src/ContentManagement/Series/Application/View/ViewSeriesValidator.php
@@ -2,6 +2,12 @@
 
 namespace App\ContentManagement\Series\Application\View;
 
+use App\Shared\Domain\Validator\UuidValidator;
+
 final class ViewSeriesValidator
 {
+    public static function validate(ViewSeriesRequest $request): void
+    {
+        UuidValidator::validate($request->id, 'Series ID');
+    }
 }
