128e90e44fe77f2cc40dc8b81201f13d61c70915
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2025-12-15 12:11:06 +0100

    feat: (Analytics) move all configuration and UI of BC inside BC

---
+++ src/Analytics/Infrastructure/Ui/Backoffice/Http/EventSubscriber/AnalyticsMenuSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Analytics\Infrastructure\Ui\Backoffice\Http\EventSubscriber;
+
+use App\Shared\Infrastructure\Ui\Backoffice\Menu\Event\MenuBuildEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class AnalyticsMenuSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MenuBuildEvent::NAME => 'onMenuBuild',
+        ];
+    }
+
+    public function onMenuBuild(MenuBuildEvent $event): void
+    {
+        $event->addItem(
+            key: 'analytics',
+            label: 'Stats',
+            route: 'backoffice_analytics_index',
+            parent: 'home',
+            icon: 'fa-chart-bar',
+            priority: 200,
+        );
+    }
+}
