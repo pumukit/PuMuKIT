6050c71713a603aa8d2ba55803b995428b86110e
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2025-12-16 13:03:45 +0100

    feat: renamed module Permission to Authorization

--- src/IdentityAndAccess/Authorization/Application/Command/DebugPermissionsCommand.php
+++ src/IdentityAndAccess/Authorization/Application/Command/DebugPermissionsCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\IdentityAndAccess\Permission\Application\Command;
+namespace App\IdentityAndAccess\Authorization\Application\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
