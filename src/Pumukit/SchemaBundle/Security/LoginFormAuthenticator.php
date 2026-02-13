13d8a9ac87a6403528d8fbcae3ee81d605913e62
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2026-02-13 09:45:22 +0100

    fix: replace google recaptcha by login attempts feature

--- src/Pumukit/SchemaBundle/Security/LoginFormAuthenticator.php
+++ src/Pumukit/SchemaBundle/Security/LoginFormAuthenticator.php
@@ -14,7 +14,6 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -72,14 +71,19 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         $user = $this->objectManager->getRepository(User::class)->findOneBy(['username' => $credentials['username']]);
-
         if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            throw new CustomUserMessageAuthenticationException('Invalid credentials or account temporarily blocked.');
         }
 
-        if (!$user->isEnabled() && !$user->isResetLoginAttemptsAllowed()) {
-            throw new CustomUserMessageAuthenticationException('User deactivated, please wait at least '.User::RESET_LOGIN_ATTEMPTS_INTERVAL.' to retry');
+        if (!$user->isEnabled() && $user->isResetLoginAttemptsAllowed()) {
+            $user->resetLoginAttempts();
+            $this->objectManager->flush();
+        }
+
+        if (!$user->canLogin() || !$user->isEnabled()) {
+            throw new CustomUserMessageAuthenticationException(
+                'Invalid credentials or account temporarily blocked.'
+            );
         }
 
         return $user;
@@ -90,11 +94,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
     }
 
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     *
-     * @param mixed $credentials
-     */
     public function getPassword($credentials): ?string
     {
         return $credentials['password'];
@@ -117,22 +116,23 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): RedirectResponse
     {
-        $username = $request->request->get('username');
-        if (!$username) {
-            throw new UsernameNotFoundException(self::EXCEPTION_MESSAGE);
-        }
-
-        $user = $this->objectManager->getRepository(User::class)->findOneBy(['username' => $username]);
-        if (!$user) {
-            throw new UsernameNotFoundException(self::EXCEPTION_MESSAGE);
-        }
-
-        $this->updateUser($user);
-
-        if ($user->canLogin()) {
-            $request->getSession()->getFlashBag()->add('error', 'Username or password invalid');
+        if ($exception instanceof CustomUserMessageAuthenticationException) {
+            $request->getSession()->getFlashBag()->add('error', $exception->getMessage());
         } else {
-            $request->getSession()->getFlashBag()->add('error_max_attempt', 'Please, wait '.User::RESET_LOGIN_ATTEMPTS_INTERVAL.' before trying again.');
+            $username = $request->request->get('username');
+            $user = $this->objectManager->getRepository(User::class)->findOneBy(['username' => $username]);
+
+            if ($user) {
+                $this->updateUser($user);
+
+                if (!$user->canLogin()) {
+                    $request->getSession()->getFlashBag()->add('error', 'Invalid credentials or account temporarily blocked.');
+                } else {
+                    $request->getSession()->getFlashBag()->add('error', 'Invalid credentials or account temporarily blocked.');
+                }
+            } else {
+                $request->getSession()->getFlashBag()->add('error', 'Invalid credentials or account temporarily blocked.');
+            }
         }
 
         return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
