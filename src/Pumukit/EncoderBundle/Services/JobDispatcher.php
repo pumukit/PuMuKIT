ea16dae1bfd6a15166946224cb924b0ab0c4684f
albacodina <48245111+albacodina@users.noreply.github.com>
2026-05-05 10:11:20 +0200

    Fix 28053 job dispatch eventname (#758)
    * fix(28053): Use event name instead of string flag in job dispatcher
    
    * fix(28053): Fix phpcsfixer error

--- src/Pumukit/EncoderBundle/Services/JobDispatcher.php
+++ src/Pumukit/EncoderBundle/Services/JobDispatcher.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pumukit\EncoderBundle\Services;
 
 use Pumukit\EncoderBundle\Document\Job;
-use Pumukit\EncoderBundle\Event\EncoderEvents;
 use Pumukit\EncoderBundle\Event\JobEvent;
 use Pumukit\SchemaBundle\Document\MediaType\MediaInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -21,11 +20,11 @@ class JobDispatcher
         $this->jobValidator = $jobValidator;
     }
 
-    public function dispatch(string $success, Job $job, MediaInterface $media = null): void
+    public function dispatch(string $eventName, Job $job, MediaInterface $media = null): void
     {
         $multimediaObject = $this->jobValidator->ensureMultimediaObjectExists($job);
 
         $event = new JobEvent($job, $media, $multimediaObject);
-        $this->eventDispatcher->dispatch($event, $success ? EncoderEvents::JOB_SUCCESS : EncoderEvents::JOB_ERROR);
+        $this->eventDispatcher->dispatch($event, $eventName);
     }
 }
