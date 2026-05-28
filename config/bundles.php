886f2127993ac41239d29a1ad34b1d27df697407
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2026-05-28 15:39:50 +0200

    chore: remove unused sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -27,6 +27,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
