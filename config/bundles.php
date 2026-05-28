36f0307d10d06420e2a02610281c5eefb27f7ef2
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2026-05-28 16:53:39 +0200

    chore: remove unused maker-bundle and stopwatch dev dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -25,7 +25,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
